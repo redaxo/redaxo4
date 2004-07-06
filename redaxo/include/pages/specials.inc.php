b8552a9d819e15d2f4a830b36ce7c4d1d018e432
vscope <vscope>
2004-07-06 10:48:22 +0000

    Bug #1999 solved

--- redaxo/include/pages/specials.inc.php
+++ redaxo/include/pages/specials.inc.php
@@ -1,5 +1,5 @@
 <?
-
+print_r($_POST);
 // ---------------------------------------------------------- func
 
 // ------------- generiere statische inhalte
@@ -7,15 +7,15 @@ $ERRMSG = "";
 
 if ($func == "copyCategory")
 {
-	
+
 	// noch nicht fertig
-	
-	
+
+
 	// $which,$to_cat
 	// copyCategory(10,0);
-	
-	
-	
+
+
+
 }elseif ($func == "generate")
 {
 
@@ -31,7 +31,7 @@ if ($func == "copyCategory")
 		fclose($fp);
 		$gt->next();
 	}
-	
+
 	// ----------------------------------------------------------- generiere artikel
 	deleteDir($REX[INCLUDE_PATH]."/generated/articles",0);
 	// mkdir($REX[INCLUDE_PATH]."/generated/articles",0664);
@@ -56,83 +56,83 @@ if ($func == "copyCategory")
 	// generateCategories();
 
 	$MSG = $I18N->msg('articles_generated')." ".$I18N->msg('old_articles_deleted');
-	
+
 }elseif($func == "linkchecker")
 {
 	unset($LART);
-	
+
 	for ($j=1;$j<11;$j++)
 	{
 		$LC = new sql;
 		// $LC->debugsql = 1;
-		$LC->setQuery("select rex_article_slice.article_id,rex_article_slice.id from rex_article_slice 
-				left join rex_article on rex_article_slice.link$j=rex_article.id 
-				where 
+		$LC->setQuery("select rex_article_slice.article_id,rex_article_slice.id from rex_article_slice
+				left join rex_article on rex_article_slice.link$j=rex_article.id
+				where
 				rex_article_slice.link$j>0 and rex_article.id IS NULL");
 		for ($i=0;$i<$LC->getRows();$i++)
 		{
 			$LART[$LC->getValue("rex_article_slice.article_id")]=1;
 			$LSLI[$LC->getValue("rex_article_slice.article_id")]=$LC->getValue("rex_article_slice.id");
-			$LC->next();	
+			$LC->next();
 		}
 	}
-	
+
 	if (count($LART)>0) reset($LART);
-	
+
 	for ($i=0;$i<count($LART);$i++)
 	{
 		$MSG .= " | <a href=index.php?page=content&article_id=".key($LART)."&mode=edit&slice_id=".$LSLI[key($LART)]."&function=edit#editslice>".key($LART)."</a>";
 		next($LART);
 	}
-	
+
 	if (count($LART)==0) $MSG = $I18N->msg("links_ok");
 	else $MSG = "<b>".$I18N->msg("links_not_ok")."</b> ". $MSG. " |";
-	
+
 }elseif($func == 'updateinfos')
 {
 
 	$h = fopen("include/master.inc.php","r");
 	$cont = fread($h,filesize("include/master.inc.php"));
-	
-	$cont = ereg_replace("(REX\[BARRIEREFREI\].?\=.?)[^;]*","\\1".strtolower($neu_barriere),$cont); 
-	// $cont = ereg_replace("(REX\[COMMUNITY\].?\=.?)[^;]*","\\1".strtolower($neu_community),$cont); 
-	$cont = ereg_replace("(REX\[STARTARTIKEL_ID\].?\=.?)[^;]*","\\1".strtolower($neu_startartikel),$cont); 
+
+	$cont = ereg_replace("(REX\[BARRIEREFREI\].?\=.?)[^;]*","\\1".strtolower($neu_barriere),$cont);
+	// $cont = ereg_replace("(REX\[COMMUNITY\].?\=.?)[^;]*","\\1".strtolower($neu_community),$cont);
+	$cont = ereg_replace("(REX\[STARTARTIKEL_ID\].?\=.?)[^;]*","\\1".strtolower($neu_startartikel),$cont);
 	$cont = ereg_replace("(REX\[error_emailaddress\].?\=.?)[^;]*","\\1\"".strtolower($neu_error_emailaddress)."\"",$cont);
-	$cont = ereg_replace("(REX\[LANG\].?\=.?)[^;]*","\\1".$neu_lang,$cont); 
-	
+	$cont = ereg_replace("(REX\[LANG\].?\=.?)[^;]*","\\1".$neu_lang,$cont);
+
 	$cont = ereg_replace("(REX\[SERVER\].?\=.?)[^;]*","\\1\"".($neu_SERVER)."\"",$cont);
-	$cont = ereg_replace("(REX\[SERVERNAME\].?\=.?)[^;]*","\\1\"".($neu_SERVERNAME)."\"",$cont);	
-	
+	$cont = ereg_replace("(REX\[SERVERNAME\].?\=.?)[^;]*","\\1\"".($neu_SERVERNAME)."\"",$cont);
+
 	$cont = ereg_replace("(DB\[2\]\[HOST\].?\=.?)[^;]*","\\1\"".($neu_db2_host)."\"",$cont);
 	$cont = ereg_replace("(DB\[2\]\[LOGIN\].?\=.?)[^;]*","\\1\"".($neu_db2_login)."\"",$cont);
 	$cont = ereg_replace("(DB\[2\]\[PSW\].?\=.?)[^;]*","\\1\"".($neu_db2_psw)."\"",$cont);
-	$cont = ereg_replace("(DB\[2\]\[NAME\].?\=.?)[^;]*","\\1\"".($neu_db2_name)."\"",$cont);	
-	
-	
-	
+	$cont = ereg_replace("(DB\[2\]\[NAME\].?\=.?)[^;]*","\\1\"".($neu_db2_name)."\"",$cont);
+
+
+
 	fclose($h);
 	$h = fopen("include/master.inc.php","w+");
 	fwrite($h,$cont,strlen($cont));
 	fclose($h);
-	
+
 	if ($neu_barriere == "TRUE") $REX[BARRIEREFREI] = TRUE;
 	else $REX[BARRIEREFREI] = FALSE;
-	
+
 	/*
 	if ($neu_community == "TRUE") $REX[COMMUNITY] = TRUE;
 	else $REX[COMMUNITY] = FALSE;
 	*/
-	
+
 	$REX[STARTARTIKEL_ID] = $neu_startartikel;
 	$REX[error_emailaddress] = $neu_error_emailaddress;
 	$REX[SERVER] = $neu_SERVER;
 	$REX[SERVERNAME] = $neu_SERVERNAME;
-	
+
 	$DB[2][HOST] = $neu_db2_host;
 	$DB[2][LOGIN] = $neu_db2_login;
 	$DB[2][PSW] = $neu_db2_psw;
-	$DB[2][NAME] = $neu_db2_name;	
-	
+	$DB[2][NAME] = $neu_db2_name;
+
 	$MSG = $I18N->msg("info_updated");
 
 }
@@ -145,14 +145,14 @@ echo "<table border=0 cellpadding=5 cellspacing=1 width=770>
 	</tr>";
 
 if ($MSG != "") echo "<tr><td class=warning colspan=2><b>$MSG</b></td></tr>";
-	
+
 echo "<tr><td class=grey width=50% valign=top><br>";
 
 echo "<b><a href=index.php?page=specials&func=generate>".$I18N->msg("regenerate_article")."</a></b><br>".$I18N->msg("regeneration_message")."<br><br>";
 echo "<b><a href=index.php?page=specials&func=linkchecker>".$I18N->msg("link_checker")."</a></b><br>".$I18N->msg("check_links_text")."<br>";
 
 echo "<br></td><td class=grey valign=top><br>";
-		
+
 echo "<table width=100% cellpadding=0 cellspacing=1 border=0>";
 echo "<form action=index.php method=post>";
 echo "<input type=hidden name=page value=specials>";
@@ -180,31 +180,31 @@ echo "<tr><td colspan=3><br><b>".$I18N->msg("specials_others")."</b></td></tr>";
 
 echo "<tr><td>\$REX[WWW_PATH]:</td><td><img src=pics/leer.gif width=10 height=20></td><td>\"".$REX[WWW_PATH]."\"</td></tr>";
 echo "<tr><td>\$REX[INCLUDE_PATH]:</td><td><img src=pics/leer.gif width=10 height=20></td><td>\"".$REX[INCLUDE_PATH]."\"</td></tr>";
-	
+
 if($REX[BARRIEREFREI]) $barricheck = "selected"; else $barricheck_false = "selected";
 // if($REX[COMMUNITY]) $communitycheck = "selected"; else $communitycheck_false = "selected";
-	
+
 echo "<tr><td>\$REX[error_emailaddress]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_error_emailaddress value=\"".$REX[error_emailaddress]."\" class=inp100></td></tr>";
 echo "<tr><td>\$REX[BARRIEREFREI]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><select name=neu_barriere size=1><option $barricheck>TRUE</option><option $barricheck_false>FALSE</option></select></td></tr>";
 // echo "<tr><td>\$REX[COMMUNITY]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><select name=neu_community size=1><option $communitycheck>TRUE</option><option $communitycheck_false>FALSE</option></select></td></tr>";
 echo "<tr><td>\$REX[STARTARTIKEL_ID]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_startartikel value=\"".$REX[STARTARTIKEL_ID]."\"></td></tr>";
 echo "<tr><td>\$REX[LANG]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><select name=neu_lang size=1>";
 foreach ($REX[LOCALES] as $l) {
-	$selected = ($l == $REX[LANG] ? "selected" : ""); 
+	$selected = ($l == $REX[LANG] ? "selected" : "");
 	echo "<option value='$l' $selected>$l</option>";
 }
 echo "</select></td></tr>";
 echo "<tr><td></td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=submit name=sendit value=".$I18N->msg("specials_update")."></td></tr>";
 echo "</form>";
 echo "</table>";
-	
+
 echo "<br></td>
 	</tr>
 	</table>";
 
 echo "<br>";
 
-if ($function == "update")
+if ($function == "Update" or $function == "Ändern")
 {
 	$update = new sql;
 	$update->setTable("rex_article_type");
@@ -213,9 +213,9 @@ if ($function == "update")
 	$update->setValue("description",$description);
 	$update->update();
 	$type_id = 0;
-	$function = "";	
+	$function = "";
 	$message = $I18N->msg("article_type_updated");
-}elseif($function == "delete")
+}elseif($function == "Delete" or $function == "Löschen")
 {
 	if ($type_id!=1)
 	{
@@ -236,7 +236,7 @@ if ($function == "update")
 	$add->setValue("description",$description);
 	$add->insert();
 	$type_id = 0;
-	$function = "";	
+	$function = "";
 	$message = $I18N->msg("article_type_added");
 }
 
@@ -271,7 +271,7 @@ if ($function == "add")
 		<td class=dgrey><input style='width:100%' type=text size=20 name=description value=\"".htmlentities($description)."\"></td>
 		<td class=dgrey valign=top><input type=submit name=function value=add></td>
 		</form>
-		</tr>";	
+		</tr>";
 }
 
 
@@ -290,7 +290,7 @@ for($i=0;$i<$sql->getRows();$i++)
 			<td class=dgrey><input style='width:100%' type=text size=20 name=description value=\"".htmlentities($sql->getValue("description"))."\"></td>
 			<td class=dgrey valign=top><input type=submit name=function value=".$I18N->msg("update_button")."><input type=submit name=function value=".$I18N->msg("delete_button")."></td>
 			</form>
-			</tr>";		
+			</tr>";
 	}else
 	{
 		echo "	<tr>
