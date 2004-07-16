3e06716779bd6e9b179850ee09e3bd177cdfeb4d
vscope <vscope>
2004-07-16 16:49:06 +0000

    replaceLinks funktion aus class.article

--- redaxo/include/functions/function_rex_modrewrite.inc.php
+++ redaxo/include/functions/function_rex_modrewrite.inc.php
@@ -3,6 +3,7 @@
 ##########################################################
 # Generate Mod Rewrite Name for Article
 # Get Url for an Article ID
+# Replace redaxo:// REX_LINK_INTERN[] in content
 ##########################################################
 
 function ModRewriteName($article_name) {
@@ -30,6 +31,29 @@ function getURLbyID($ArticleID){
 		$url = '?article_id='.$ArticleID;
 	}
 	return $url;
+}
+
+function replaceLinks($content){
+
+        // -- preg match REX_LINK_INTERN[ARTICLEID] --
+        preg_match_all("/REX_LINK_INTERN\[([0-9]*)\]/im",$content,$matches);
+        if($matches[0][0]!=''){
+            for($m=0;$m<count($matches[0]);$m++){
+                $url = getURLbyID($matches[1][$m]);
+                $content = str_replace($matches[0][$m],$url,$content);
+            }
+        }
+
+        // -- preg match redaxo://[ARTICLEID] --
+        preg_match_all("/redaxo:\/\/([0-9]*)/im",$content,$matches);
+        if($matches[0][0]!=''){
+            for($m=0;$m<count($matches[0]);$m++){
+                $url = getURLbyID($matches[1][$m]);
+                $content = str_replace($matches[0][$m],$url,$content);
+            }
+        }
+
+        return $content;
 }
 
 ?>
