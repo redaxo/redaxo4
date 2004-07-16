dd80167f139b7f3677f8a7be4c02672211a3d6b5
vscope <vscope>
2004-07-16 10:53:45 +0000

    new function getURLbyID

--- redaxo/include/functions/function_rex_modrewrite.inc.php
+++ redaxo/include/functions/function_rex_modrewrite.inc.php
@@ -2,6 +2,7 @@
 
 ##########################################################
 # Generate Mod Rewrite Name for Article
+# Get Url for an Article ID
 ##########################################################
 
 function ModRewriteName($article_name) {
@@ -16,6 +17,19 @@ function ModRewriteName($article_name) {
     $url = str_replace("&","-",$url);
     $url = urlencode($url);
     return $url;
+}
+
+function getURLbyID($ArticleID){
+	global $REX;
+	if($REX[MOD_REWRITE]){
+		$db = new sql;
+		$sql = "SELECT name FROM rex_article WHERE id='$ArticleID'";
+		$res = $db->get_array($sql);
+		$url = $ArticleID."-".ModRewriteName($res[0][name]);
+	} else {
+		$url = '/?article_id='.$ArticleID;
+	}
+	return $url;
 }
 
 ?>
