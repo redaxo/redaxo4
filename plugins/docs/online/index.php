e59a4f3e57f0511911dd5a970752c3476819b547
vscope <vscope>
2004-09-13 16:37:51 +0000

    scrolling menu disabled

--- plugins/docs/online/index.php
+++ plugins/docs/online/index.php
@@ -9,7 +9,7 @@ include('../classes/config.inc.php');
 <title>redaxo - documentation</title>
 </head>
 <frameset cols="200,*">
-	<frame src="code/menu.php?lang=<?=$_GET[lang]?>&doc=<?=$_GET[doc]?>&chapter=<?=$_GET[chapter]?>&title=<?=$_GET[title]?>" name="menu" scrolling="yes" frameborder="0" scrolling="no">
+	<frame src="code/menu.php?lang=<?=$_GET[lang]?>&doc=<?=$_GET[doc]?>&chapter=<?=$_GET[chapter]?>&title=<?=$_GET[title]?>" name="menu" frameborder="0" scrolling="no">
 	<frame src="code/main.php?lang=<?=$_GET[lang]?>&doc=<?=$_GET[doc]?>&chapter=<?=$_GET[chapter]?>&title=<?=$_GET[title]?>#title<?=$_GET[title]?>" name="main" scrolling="yes" frameborder="0">
 </frameset>
 </html>
