81ccb6f09ed05d9054be30a29fd26712c1c61ff8
dergel <jan.kristinus@yakamara.de>
2004-10-29 19:01:00 +0000

    addon: import files

---
+++ redaxo/include/addons/import_export/config.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+$mypage = "import_export"; 				// only for this file
+
+$I18N_ADDON = new i18n($REX[LANG],$REX[INCLUDE_PATH]."/addons/$mypage/lang/"); 	// CREATE LANG OBJ FOR THIS ADDON
+
+$REX[ADDON][rxid][$mypage] = "REX_001";			// unique id /
+// $REX[ADDON][nsid][$mypage] = "REX002,REX003";	// necessary rxid; - not yet included
+$REX[ADDON][page][$mypage] = "$mypage";			// pagename/foldername
+$REX[ADDON][name][$mypage] = "Import/Export";		// name
+$REX[ADDON][perm][$mypage] = "import[]"; 		// permission
+
+// IF NECESSARY INCLUDE FUNC/CLASSES ETC
+// INCLUDE IN FRONTEND --- if ($REX[GG]) 
+// INCLUDE IN BACKEND --- if (!$REX[GG]) 
+
+?>
---
+++ redaxo/include/addons/import_export/install.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+// CREATE/UPDATE DATABASE
+
+
+// CREATE/UPDATE MODULES
+
+
+// CREATE/UPDATE PAGES
+
+
+// CREATE/UPDATE FILES
+
+
+// REGENERATE SITE
+
+
+$REX[ADDON][install]["import_export"] = 1;
+// ERRMSG IN CASE: $REX[ADDON][installmsg]["import_export"] = "Leider konnte nichts installiert werden da.";
+
+
+?>
