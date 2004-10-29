06649e86cb50ab68f85903f9edcb8bc1f870bb46
dergel <jan.kristinus@yakamara.de>
2004-10-29 18:55:46 +0000

    ordentlich gemacht

--- redaxo/include/functions.inc.php
+++ redaxo/include/functions.inc.php
@@ -1,5 +1,9 @@
 <?php
 
+// ----------------- TIMER
+include_once $REX[INCLUDE_PATH]."/functions/function_showmicrotime.inc.php";
+
+// ----------------- REGISTER GLOBALS CHECK
 if (!ini_get('register_globals'))
 {
 	// register_globals = off;
@@ -15,54 +19,49 @@ if (!ini_get('register_globals'))
 	// register_globals = on;	
 }
 
-
-include_once $REX[INCLUDE_PATH]."/functions/function_showmicrotime.inc.php";
-
-// ------------------------------------------------------------ wenn magic quotes off
-
+// ----------------- MAGIC QUOTES CHECK
 if (!get_magic_quotes_gpc()) include $REX[INCLUDE_PATH]."/functions/function_rex_mquotes.inc.php";
 
-// ------------------------------------------------------------ includes
+// ----------------- REDAXO INCLUDES
+include_once $REX[INCLUDE_PATH]."/classes/class.i18n.inc.php"; // LANGUAGE
+include_once $REX[INCLUDE_PATH]."/classes/class.sql.inc.php";
+include_once $REX[INCLUDE_PATH]."/classes/class.select.inc.php";
+include_once $REX[INCLUDE_PATH]."/classes/class.article.inc.php";
+include_once $REX[INCLUDE_PATH]."/classes/class.login.inc.php";
+include_once $REX[INCLUDE_PATH]."/classes/class.stat.inc.php";
+include_once $REX[INCLUDE_PATH]."/classes/class.cache.inc.php"; // Advanced Caching class
+include_once $REX[INCLUDE_PATH]."/functions/function_datefrommydate.inc.php";
+include_once $REX[INCLUDE_PATH]."/functions/function_selectdate.inc.php";
+include_once $REX[INCLUDE_PATH]."/functions/function_mail.inc.php";
+include_once $REX[INCLUDE_PATH]."/functions/function_rex_mediapool.inc.php";
+include_once $REX[INCLUDE_PATH]."/functions/function_rex_modrewrite.inc.php";
+include_once $REX[INCLUDE_PATH]."/functions/function_rex_title.inc.php";
+include_once $REX[INCLUDE_PATH]."/functions/function_rex_generate.inc.php";
+include_once $REX[INCLUDE_PATH]."/addons.inc.php";
 
-// hier wird die i18n sprachklasse erzeugt
-include_once $REX[INCLUDE_PATH]."/classes/class.i18n.inc.php";
+// ----------------- REDAXO COMMUNITY
+include_once $REX[INCLUDE_PATH]."/classes/class.board.inc.php";
+include_once $REX[INCLUDE_PATH]."/classes/class.mime_mail.inc.php";
+
+// ----------------- REDAXO IM/EXPORT
+include_once $REX[INCLUDE_PATH]."/classes/class.tar.inc.php";
+
+// ----------------- EXTRAS
+include_once $REX[INCLUDE_PATH]."/classes/class.mail_decode.inc.php";
+include_once $REX[INCLUDE_PATH]."/classes/class.form.inc.php";
+include_once $REX[INCLUDE_PATH]."/classes/class.list.inc.php";
+include_once $REX[INCLUDE_PATH]."/classes/class.oocategory.inc.php"; // OO Classes
+include_once $REX[INCLUDE_PATH]."/classes/class.textile.inc.php"; // Textile class
+include_once $REX[INCLUDE_PATH]."/functions/function_createimage.inc.php";
+
+// ----------------- CREATE LANG OBJ
+if (!$REX[GG] && $lang == "de_DE") $REX[LANG] = $lang;
+elseif (!$REX[GG] && $lang == "en_GB") $REX[LANG] = $lang;
+
 $I18N = new i18n($REX[LANG],$REX[INCLUDE_PATH]."/lang/");
 $REX[LOCALES] = i18n::getLocales($REX[INCLUDE_PATH]."/lang/");
 
-// klassen
-// include $REX[INCLUDE_PATH]."/classes/class.ftp.inc.php";
-include $REX[INCLUDE_PATH]."/classes/class.sql.inc.php";
-include $REX[INCLUDE_PATH]."/classes/class.select.inc.php";
-include $REX[INCLUDE_PATH]."/classes/class.article.inc.php";
-include $REX[INCLUDE_PATH]."/classes/class.login.inc.php";
-include $REX[INCLUDE_PATH]."/classes/class.form.inc.php";
-include $REX[INCLUDE_PATH]."/classes/class.list.inc.php";
-include $REX[INCLUDE_PATH]."/classes/class.board.inc.php";
-include $REX[INCLUDE_PATH]."/classes/class.mime_mail.inc.php";
-include $REX[INCLUDE_PATH]."/classes/class.mail_decode.inc.php";
-include $REX[INCLUDE_PATH]."/classes/class.tar.inc.php";
-include $REX[INCLUDE_PATH]."/classes/class.stat.inc.php";
-
-// OO Classes
-include_once $REX[INCLUDE_PATH]."/classes/class.oocategory.inc.php";
-
-// Textile class
-include_once $REX[INCLUDE_PATH]."/classes/class.textile.inc.php";
-
-// Advanced Caching class
-include_once $REX[INCLUDE_PATH]."/classes/class.cache.inc.php";
-
-// functions
-include $REX[INCLUDE_PATH]."/functions/function_datefrommydate.inc.php";
-include $REX[INCLUDE_PATH]."/functions/function_selectdate.inc.php";
-include $REX[INCLUDE_PATH]."/functions/function_mail.inc.php";
-include $REX[INCLUDE_PATH]."/functions/function_createimage.inc.php";
-include $REX[INCLUDE_PATH]."/functions/function_rex_mediapool.inc.php";
-include $REX[INCLUDE_PATH]."/functions/function_rex_modrewrite.inc.php";
-
-// ------------------------------------------------------------ redaxo includes
-
-include $REX[INCLUDE_PATH]."/functions/function_rex_title.inc.php";
-include $REX[INCLUDE_PATH]."/functions/function_rex_generate.inc.php";
+// ----------------- 
+setlocale(LC_ALL,trim($I18N->msg("setlocale")));
 
 ?>
