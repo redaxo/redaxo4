ae16cd8660bcf165519920e440c3825b3b5f39ff
dergel <jan.kristinus@yakamara.de>
2004-10-29 19:19:22 +0000

    cache angepasst.

--- index.php
+++ index.php
@@ -1,43 +1,5 @@
 <?
 
-//////////////////////////////////////////////
-// advanced caching
-//////////////////////////////////////////////
-
-session_start();
-
-$REX[NOFUNCTIONS] = true;
-include('redaxo/include/master.inc.php');
-include('redaxo/include/functions/function_showmicrotime.inc.php');
-
-// If Caching is true start engine
-if($REX[CACHING]){
-
-    include('redaxo/include/classes/class.cache.inc.php');
-    $Cache = new Cache($article_id);
-    if($Cache->isCacheConf()){
-        if($Cache->isCacheFile()){
-            // show cache content
-            $Cache->printCacheFile();
-            if ($REX[STATS]==1)
-            {
-                include $REX[INCLUDE_PATH]."/classes/class.stat.inc.php";
-                $log = new stat;
-                $log->writeLog($Cache->article_id);
-            }
-            if($REX[CACHING_DEBUG]) print "<br>CachedVersion<br>";
-            if($REX[CACHING_DEBUG]) print "Script time: ".showScripttime();
-            exit;
-        } else {
-            // start caching
-            $Cache->startCacheFile();
-        }
-    }
-
-}
-
-//////////////////////////////////////////////
-
 // --------------------------- globals
 
 unset($REX);
@@ -54,13 +16,13 @@ $REX[y] = $y;
 
 $REX[REDAXO] = false;
 
-// Barrierefreie Seite wenn $REX[BF] = true;
+// B CONTENT Seiten wenn $REX[BC] = true;
 // Weiterhin um diese Seiten zu erstellen muss
 // in der redaxo/include/master.inc.php die
-// $REX[BARRIEREFREI] = true; gesetzt werden.
+// $REX[BCONTENT] = true; gesetzt werden.
 //
 
-$REX[BF] = false;
+$REX[BC] = false;
 
 // Wenn $REX[GG] = true; dann wird der
 // Content aus den redaxo/include/generated/
@@ -92,21 +54,66 @@ if ($category_id != "")
 
 if ($article_id == "") $article_id = $REX[STARTARTIKEL_ID];
 
-$REX_ARTICLE = new article;
-if ($REX_ARTICLE->setArticleId($article_id))
-{
-	$REX_ARTICLE->getArticleTemplate();
-}elseif($REX_ARTICLE->setArticleId($REX[STARTARTIKEL_ID]))
-{
-	$REX_ARTICLE->getArticleTemplate();
-}else
-{
-	echo "Kein Startartikel selektiert";
-	#$REX[STATS] = 0;
+$SHOWARTICLE = true;
+
+// If Caching is true start engine
+if($REX[CACHING]){
+
+	
+	//////////////////////////////////////////////
+	// advanced caching
+	//////////////////////////////////////////////
+	
+	$Cache = new Cache($article_id);
+	if($Cache->isCacheConf())
+	{
+		if($Cache->isCacheFile())
+		{
+			$Cache->printCacheFile();
+			if($REX[CACHING_DEBUG]) print "<br>CachedVersion<br>";
+			if($REX[CACHING_DEBUG]) print "Script time: ".showScripttime();
+			$SHOWARTICLE = false;
+		} else {
+			// start caching
+			$Cache->startCacheFile();
+		}
+	}
+
+	//////////////////////////////////////////////
+
 }
 
-// ------------------------------------------------------------ scriptzeit
-// echo showmicrotime();
+if ($SHOWARTICLE)
+{
+	$REX_ARTICLE = new article;
+	if ($REX_ARTICLE->setArticleId($article_id))
+	{
+		$REX_ARTICLE->getArticleTemplate();
+	}elseif($REX_ARTICLE->setArticleId($REX[STARTARTIKEL_ID]))
+	{
+		$REX_ARTICLE->getArticleTemplate();
+	}else
+	{
+		echo "Kein Startartikel selektiert";
+		#$REX[STATS] = 0;
+	}
+	//////////////////////////////////////////////
+	// advanced caching
+	//////////////////////////////////////////////
+	
+	if($Cache->makeCacheFile){
+		$Cache->writeCacheFile();
+		if($REX[CACHING_DEBUG]) print "<br>MadeCache<br>";
+	} else {
+		if($REX[CACHING_DEBUG]) print "<br>Live<br>";
+	}
+	
+	if($REX[CACHING_DEBUG]) print "Script time: ".showScripttime();
+	
+	//////////////////////////////////////////////	
+}
+
+// ------------------------------------------------------------ STATISTIK
 
 if ($REX[STATS]==1)
 {
@@ -114,19 +121,5 @@ if ($REX[STATS]==1)
 	$log->writeLog($article_id);
 }
 
-
-//////////////////////////////////////////////
-// advanced caching
-//////////////////////////////////////////////
-
-if($Cache->makeCacheFile){
-	$Cache->writeCacheFile();
-	if($REX[CACHING_DEBUG]) print "<br>MadeCache<br>";
-} else {
-	if($REX[CACHING_DEBUG]) print "<br>Live<br>";
-}
-
-if($REX[CACHING_DEBUG]) print "Script time: ".showScripttime();
-//////////////////////////////////////////////
 
 ?>
