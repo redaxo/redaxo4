2a936d3ec200e145a30b9e92a4c5e061f357839c
dergel <jan.kristinus@yakamara.de>
2004-10-29 19:15:17 +0000

    aktuelle konfig

--- redaxo/include/master.inc.php
+++ redaxo/include/master.inc.php
@@ -1,67 +1,49 @@
-<?
+<?php
 
-// ------------------------------------------------------------
+// -----------------
 
 if (!$REX[GG]) $REX[GG] = false;
 
-// ------------------------------------------------------------ globals
-
 // ----------------- SERVER VARS
 
+$REX[SETUP] = false; 			// Setupservicestatus - if everything ok -> false; if problem set to true;
 $REX[SERVER] = "redaxo.com";
 $REX[SERVERNAME] = "Redaxo-Demo";
-$REX[error_emailaddress] = "";
+$REX[error_emailaddress] = "jan.kristinus@pergopa.de";
 $REX[version] = "2.7";
-$REX[STARTARTIKEL_ID] = 1;
-$REX[STATS] = 1;
-
-// select default language
-$REX[LANG] = en_GB;
-
-// activate mod_rewrite support
-$REX[MOD_REWRITE] = false;
-
-// Windows:
-// Pfade absolut eintragen
-// Linux:
-// nicht ändern
-
+$REX[subversion] = "4";
+$REX[STARTARTIKEL_ID] = 1; // FIRTS ARTICLE
+$REX[STATS] = 1; // STATS
+$REX[LANG] = de_DE; // select default language
+$REX[MOD_REWRITE] = false; // activate mod_rewrite support
 $REX[WWW_PATH] = ""; //
-$REX[DOC_ROOT] = "";
-$REX[INCLUDE_PATH] = $REX[DOC_ROOT].$REX[HTDOCS_PATH]."redaxo/include";
-$REX[MEDIAFOLDER] = $REX[HTDOCS_PATH]."files";
-
-//
-
-$REX[BARRIEREFREI] = false;
+$REX[DOC_ROOT] = ""; // 
+$REX[INCLUDE_PATH] = $REX[DOC_ROOT].$REX[HTDOCS_PATH]."redaxo/include"; // 
+$REX[MEDIAFOLDER] = $REX[HTDOCS_PATH]."files"; //
+$REX[BCONTENT] = false; //
 $REX[COMMUNITY] = false;
+$REX[CACHING] = true; // Caching
+$REX[CACHING_DEBUG] = false; // Shows debugmessage
 
-// Advanced Caching
-$REX[CACHING] = false;
-$REX[CACHING_DEBUG] = false;
-
-// ----------------- DATENBANK
-
+// ----------------- DB1
 $DB[1][HOST] = "localhost";
-$DB[1][LOGIN] = "redaxo";
-$DB[1][PSW] = "redaxo";
-$DB[1][NAME] = "redaxo";
+$DB[1][LOGIN] = "admin";
+$DB[1][PSW] = "";
+$DB[1][NAME] = "redaxo2_7";
 
+// ----------------- DB2 - if necessary
 $DB[2][HOST] = "";
 $DB[2][LOGIN] = "";
 $DB[2][PSW] = "";
 $DB[2][NAME] = "";
 
-
 // ----------------- INCLUDE FUNCTIONS
-if(!$REX[NOFUNCTIONS]){
-	include($REX[INCLUDE_PATH].'/functions.inc.php');
-}
+if(!$REX[NOFUNCTIONS]) include_once ($REX[INCLUDE_PATH].'/functions.inc.php');
 
-
+// -----------------
 if (!isset($category_id) or $category_id == "") $category_id = 0;
 
-// set to default
+// ----------------- set to default
 $REX[NOFUNCTIONS] = false;
 
 ?>
