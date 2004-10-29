45eb34124f0ca65176ed50e748fa828688ee773f
dergel <jan.kristinus@yakamara.de>
2004-10-29 19:14:05 +0000

    anpassungen mit logout und add on.. sowie link zu redaxo forum

--- redaxo/include/layout_redaxo/bottom.php
+++ redaxo/include/layout_redaxo/bottom.php
@@ -2,10 +2,11 @@
 
 echo "<a name=bottom></a><br><table border=0 cellpadding=5 cellspacing=0 width=770>
 <tr>
-	<td class=greenwhite>&nbsp;</td>
+	<td class=greenwhite colspan=2>&nbsp;</td>
 </tr>
 <tr>
-	<td class=grey>&nbsp;&copy; <a href=http://www.pergopa.de target=_blank class=black>pergopa kristinus gbr</a> | <a href=http://www.redaxo.de target=_blank class=black>redaxo.de</a> | ".showScripttime()." sec</td>
+	<td class=grey ><a href=http://www.pergopa.de target=_blank class=black>pergopa kristinus gbr</a> | <a href=http://www.redaxo.de target=_blank class=black>redaxo.de</a> | <a href=http://forum.redaxo.de>?</a></td>
+	<td class=grey align=right>".showScripttime()." sec | ".strftime($I18N->msg("dateformat"))."</td>
 </tr>
 </table>";
 
--- redaxo/include/layout_redaxo/top.php
+++ redaxo/include/layout_redaxo/top.php
@@ -1,25 +1,23 @@
 <?
-// changed 02.04.04 Carsten Eckelman <careck@circle42.com>
-//   * i18n
 
 echo "<html>
 <head>
-<title>".$REX[SERVERNAME]." - $page_name</title>
-<link rel=stylesheet type=text/css href=css/style.css>
-<script language=Javascript src=js/standard.js></script>
-<script language=Javascript>
-<!--
-var redaxo = true;
-//-->
-</script>
+	<title>".$REX[SERVERNAME]." - $page_name</title>
+	<link rel=stylesheet type=text/css href=css/style.css>
+	<script language=Javascript src=js/standard.js></script>
+	<script language=Javascript>
+	<!--
+	var redaxo = true;
+	//-->
+	</script>
 </head>
 <body bgcolor=#ffffff onunload=closeAll();>
-<table border=0 cellpadding=5 cellspacing=0 width=770>
-<tr>
-	<td colspan=3 class=grey align=right>".$REX[SERVERNAME]."</td>
-</tr>
-<tr>
-	<td class=greenwhite><b>";
+	<table border=0 cellpadding=5 cellspacing=0 width=770>
+	<tr>
+		<td colspan=2 class=grey align=right>".$REX[SERVERNAME]."</td>
+	</tr>
+	<tr>
+		<td class=greenwhite width=550><b>";
 
 if ($LOGIN)
 {
@@ -28,17 +26,31 @@ if ($LOGIN)
 	if ($REX_USER->isValueOf("rights","template[]")) echo " | <a href=index.php?page=template class=white>".$I18N->msg("template")."</a>";
 	if ($REX_USER->isValueOf("rights","module[]")) echo " | <a href=index.php?page=module class=white>".$I18N->msg("module")."</a>"; 
 	if ($REX_USER->isValueOf("rights","user[]")) echo " | <a href=index.php?page=user class=white>".$I18N->msg("user")."</a>"; 
+	if ($REX_USER->isValueOf("rights","addon[]")) echo " | <a href=index.php?page=addon class=white>".$I18N->msg("addon")."</a>"; 
 	if ($REX_USER->isValueOf("rights","specials[]")) echo " | <a href=index.php?page=specials class=white>".$I18N->msg("specials")."</a>"; 
-	if ($REX_USER->isValueOf("rights","import[]")) echo " | <a href=index.php?page=import class=white>".$I18N->msg("import")."</a>"; 
+	// if ($REX_USER->isValueOf("rights","import[]")) echo " | <a href=index.php?page=import class=white>".$I18N->msg("import")."</a>"; 
 	if ($REX_USER->isValueOf("rights","community[]")) echo " | <a href=index.php?page=community class=white>".$I18N->msg("community")."</a>"; 
 	if ($REX_USER->isValueOf("rights","stats[]")) echo " | <a href=index.php?page=stats class=white>".$I18N->msg("statistics")."</a>"; 
-	echo " | <a href=index.php?FORM[logout]=1 class=white>".$I18N->msg("logout")."</a>";
+
+	reset($REX[ADDON][status]);
+	for($i=0;$i<count($REX[ADDON][status]);$i++)
+	{
+		$apage = key($REX[ADDON][status]);
+		$perm = $REX[ADDON][perm][$apage];
+		$name = $REX[ADDON][name][$apage];
+		if (current($REX[ADDON][status]) == 1 && ($REX_USER->isValueOf("rights",$perm) or $perm == "") )
+		{
+			echo " | <a href=index.php?page=$apage class=white>$name</a>";
+		}
+		next($REX[ADDON][status]);
+	}
+
 }
 
 echo "</b></td>";
 
-if ($LOGIN) echo "<td align=right class=greenblack>".$I18N->msg("name").": <b>".$REX_USER->getValue("name")."</b></td>";
-else echo "<td align=right class=greenblack>".$I18N->msg("logged_out")."</td>";
+if ($LOGIN) echo "<td align=right class=greenblack valign=top>".$I18N->msg("name").": <b>".$REX_USER->getValue("name")."</b> [<a href=index.php?FORM[logout]=1 class=white><b>".$I18N->msg("logout")."</b></a>]</td>";
+else echo "<td align=right class=greenblack valign=top><b>".$I18N->msg("logged_out")."</b></td>";
 
 echo "</tr>
 </table>";
