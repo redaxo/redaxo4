f454cec2f93dac445422b693ecd121e7e46e7a14
vscope <vscope>
2005-02-18 16:01:16 +0000

    *** empty log message ***

---
+++ redaxo/js/tiny_mce/plugins/redaxo/langs/de.js
@@ -0,0 +1,10 @@
+// DE lang variables
+
+
+tinyMCELang['lang_redaxo_pasteRichtext_title'] = 'Redaxo Formatierten Text einfügen';
+tinyMCELang['lang_redaxo_pasteRichtext_desc'] = 'Formatierten Text einfügen';
+tinyMCELang['lang_redaxo_pasteRichtext_save'] = 'Einfügen';
+tinyMCELang['lang_redaxo_pasteRichtext_cancel'] = 'Abbrechen';
+tinyMCELang['lang_redaxo_insertEmail_title'] = 'Redaxo Email Adresse einfügen';
+tinyMCELang['lang_redaxo_insertEmail_desc'] = 'Email Adresse einfügen';
+
---
+++ redaxo/js/tiny_mce/plugins/redaxo/langs/en.js
@@ -0,0 +1,8 @@
+// UK lang variables
+
+tinyMCELang['lang_redaxo_pasteRichtext_title'] = 'Redaxo Insert formatted text';
+tinyMCELang['lang_redaxo_pasteRichtext_desc'] = 'Insert formatted Text';
+tinyMCELang['lang_redaxo_pasteRichtext_save'] = 'Insert';
+tinyMCELang['lang_redaxo_pasteRichtext_cancel'] = 'Cancel';
+tinyMCELang['lang_redaxo_insertEmail_title'] = 'Redaxo Insert email adress';
+tinyMCELang['lang_redaxo_insertEmail_desc'] = 'Insert email adress';
