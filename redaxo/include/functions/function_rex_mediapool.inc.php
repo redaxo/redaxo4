50a2cb8811d928de6aa720fcb14e04c9fd1f1b80
vscope <vscope>
2005-02-18 15:27:01 +0000

    neuer editor tiny mce 1.4.2

--- redaxo/include/functions/function_rex_mediapool.inc.php
+++ redaxo/include/functions/function_rex_mediapool.inc.php
@@ -6,71 +6,82 @@
 #
 ##################################################################
 
+
 function MEDIA_HTMLAREA($VALUE_ID=1,$CONTENT,$WIDTH="100%",$HEIGHT='300px',$STYLE_SHEET='css/style.css',$STYLES='all',$LANG='de'){
 
-        // lang = de oder uk
+        // lang = de oder en
 
         global $TINYMCE;
 
         // tiny mce init
         if($TINYMCE!="done"){
 
-                if(is_array($STYLES)){
-                        $ADVANCED_STYLES = "theme_advanced_styles :\"";
-                        foreach($STYLES as $key => $var){
-                                $ADVANCED_STYLES.= "$key=$var;";
-                        }
-                        $ADVANCED_STYLES = substr($ADVANCED_STYLES,0,-1);
-                        $ADVANCED_STYLES.="\",";
-                }
+				print '
+	            <!-- tinyMCE -->
+	            <script language="javascript" type="text/javascript" src="js/tiny_mce/tiny_mce.js"></script>
+	            <script language="javascript" type="text/javascript">
+	                tinyMCE.init({
+	                    language : "'.$LANG.'",
+	                    mode : "specific_textareas",
+	                    theme : "advanced",
+	                    plugins : "redaxo,table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,zoom,flash,searchreplace,print",
+	                    theme_advanced_buttons1_add_before : "save,separator",
+	                    theme_advanced_buttons1_add : "fontselect,fontsizeselect",
+	                    theme_advanced_buttons2_add : "separator,insertdate,inserttime,preview,zoom,separator,forecolor,backcolor",
+	                    theme_advanced_buttons2_add_before: "cut,copy,paste,separator,search,replace,separator",
+	                    theme_advanced_buttons3_add_before : "tablecontrols,separator",
+	                    theme_advanced_buttons3_add : "emotions,iespell,flash,advhr,separator,print,separator,pasteRichtext,separator,insertEmail,separator,linkHack",
+	                    theme_advanced_toolbar_location : "top",
+	                    theme_advanced_toolbar_align : "left",
+	                    theme_advanced_path_location : "bottom",
+	                    content_css : "'.$STYLE_SHEET.'",
+	                    plugin_insertdate_dateFormat : "%d.%m.%Y",
+	                    plugin_insertdate_timeFormat : "%H:%M:%S",
+	                    extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
+	                    external_link_list_url : "example_link_list.js",
+	                    external_image_list_url : "example_image_list.js",
+	                    flash_external_list_url : "example_flash_list.js",
+	                    insertimage_callback : "insertMediaPool",
+	                    insertlink_callback : "insertIntLink"
+	                    //file_browser_callback : "fileBrowserCallBack"
+	                });
 
-                print '
+	                function fileBrowserCallBack(field_name, url, type) {
+	                    // This is where you insert your custom filebrowser logic
+	                    alert("Filebrowser callback: " + field_name + "," + url + "," + type);
+	                }
 
-                <script language="javascript" type="text/javascript" src="js/tiny_mce/tiny_mce_src.js"></script>
-                <script language="javascript" type="text/javascript">
-            tinyMCE.init({
-                theme : "advanced",
-                language : "'.$LANG.'",
-                mode : "specific_textareas",
-                    insertlink_callback : "insertIntLink",
-                insertimage_callback : "insertMediaPool",
-                theme_advanced_source_editor_width : 600,
-                theme_advanced_source_editor_height : 400,
-                relative_urls : false,
-                content_css : "'.$STYLE_SHEET.'",
-                //extended_valid_elements : "a[href|target|name]",
-                //invalid_elements : "a",
-                '.$ADVANCED_STYLES.'
-                debug : false
-            });
+					// custom redaxo callback functions
+	                function insertMediaPool(src, alt, border, hspace, vspace, width, height, align){
+	                	window.open("index.php?page=medienpool&HTMLArea=TINY","pool","width=660,height=500,status=yes,resizable=yes");
+	                }
 
-            function insertMediaPool(src, alt, border, hspace, vspace, width, height, align){
-                    window.open("index.php?page=medienpool&HTMLArea=TINY","pool","width=660,height=500,status=yes,resizable=yes");
-            }
+	                function insertIntLink(href, target){
+	                        window.open("index.php?page=linkmap&HTMLArea=TINY","link","width=660,height=500,status=yes,resizable=yes,scrollbars=yes");
+	                }
 
-            function insertIntLink(href, target){
-                    window.open("index.php?page=linkmap&HTMLArea=TINY","link","width=660,height=500,status=yes,resizable=yes");
-            }
+	                function tinyMCEEmail(){
+	                        var email = prompt("Geben Sie eine Emailadresse ein","");
+	                        alert(tinyMCE.getContent());
+	                        tinyMCE.execCommand("mceInsertContent", false, "<a href=mailto:"+email+">"+email+"</a>");
+	                }
 
-            function tinyMCEEmail(){
-                    var email = prompt("Geben Sie eine Emailadresse ein","");
-                    alert(tinyMCE.getContent());
-                    tinyMCE.execCommand("mceInsertContent", false, "<a href=mailto:"+email+">"+email+"</a>");
-            }
+	            </script>
+	            <!-- /tinyMCE -->
+	            ';
 
-                </script>
-            ';
+				$GLOBALS[TINYMCE] = 'done';
 
-            $GLOBALS[TINYMCE] = 'done';
         }
+
 
         print '
 
         <textarea id="VALUE['.$VALUE_ID.']" name="VALUE['.$VALUE_ID.']" style="width:'.$WIDTH.';height:'.$HEIGHT.'" rows="15" mce_editable="true">'.$CONTENT.'</textarea>
 
         ';
-}
 
+}
 
 function media_resize($FILE,$width,$height,$make_copy=false){
 
