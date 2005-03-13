054234e58ba30eadea5598e9e60bdb0a510b459d
staabm <markus.staab@redaxo.de>
2005-03-13 17:52:27 +0000

    added truncate function

--- redaxo/include/functions/function_string.inc.php
+++ redaxo/include/functions/function_string.inc.php
@@ -22,4 +22,18 @@ function endsWith( $string, $end ) {
    return ( substr( $string, strlen( $string ) - strlen( $end ) ) == $end );
 }
 
+function truncate($string, $length = 80, $etc = '...', $break_words = false)
+{
+    if ($length == 0)
+        return '';
+
+    if (strlen($string) > $length) {
+        $length -= strlen($etc);
+        if (!$break_words)
+            $string = preg_replace('/\s+?(\S+)?$/', '', substr($string, 0, $length+1));
+      
+        return substr($string, 0, $length).$etc;
+    } else
+        return $string;
+}
 ?>
