f2097b12f2f6f786a8ae8b2bf090c5a241c28908
dergel <jan.kristinus@yakamara.de>
2005-04-14 14:52:53 +0000

    *** empty log message ***

--- redaxo/js/tiny_mce/plugins/flash/langs/de.js
+++
@@ -1,4 +0,0 @@
-// DE lang variables
-tinyMCELang['lang_insert_flash']      = 'Flash Movie einf&uuml;gen / bearbeiten';
-tinyMCELang['lang_insert_flash_file'] = 'Flash-Datei';
-tinyMCELang['lang_insert_flash_size'] = 'Gr&ouml;&szlig;e';
