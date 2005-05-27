b800f5d99d83d5865da665940ea79c5d12cb0b5f
staabm <markus.staab@redaxo.de>
2005-05-27 15:42:14 +0000

    archive class for new mediapool - 1st commit

---
+++ redaxo/include/classes/class.archive.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+ini_set('include_path', ini_get('include_path') .PATH_SEPARATOR. $REX['INCLUDE_PATH'].'/classes/');
+
+include_once $REX['INCLUDE_PATH'].'/classes/MIME/Type.php';
+include_once $REX['INCLUDE_PATH'].'/classes/File/Archive.php';
+
+?>
