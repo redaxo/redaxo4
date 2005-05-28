e708aa963b72b87b7e4664b9cb8445208f837a03
staabm <markus.staab@redaxo.de>
2005-05-28 14:16:27 +0000

    comment

--- redaxo/include/classes/class.archive.inc.php
+++ redaxo/include/classes/class.archive.inc.php
@@ -1,5 +1,6 @@
 <?php
 
+// Anpassen des Include-Path, da innerhalt der Pear-Klassen mit relativen Pfaden zum Pear-Root gearbeitet wird
 ini_set('include_path', ini_get('include_path') .PATH_SEPARATOR. $REX['INCLUDE_PATH'].'/classes/');
 
 include_once $REX['INCLUDE_PATH'].'/classes/MIME/Type.php';
