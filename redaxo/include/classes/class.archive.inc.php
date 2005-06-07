49920b10d6ff6e0fb99cdbe684e694cc6734c3e9
staabm <markus.staab@redaxo.de>
2005-06-07 15:35:48 +0000

    no archive functions needed for default mediapool

--- redaxo/include/classes/class.archive.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Anpassen des Include-Path, da innerhalt der Pear-Klassen mit relativen Pfaden zum Pear-Root gearbeitet wird
-ini_set('include_path', ini_get('include_path') .PATH_SEPARATOR. $REX['INCLUDE_PATH'].'/classes/');
-
-include_once $REX['INCLUDE_PATH'].'/classes/MIME/Type.php';
-include_once $REX['INCLUDE_PATH'].'/classes/File/Archive.php';
-
-?>
