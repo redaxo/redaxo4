2e1b964886ca56f40511853f13df98368a932026
dergel <jan.kristinus@yakamara.de>
2005-07-03 18:41:02 +0000

    ootemplate entfernt

--- redaxo/include/functions.inc.php
+++ redaxo/include/functions.inc.php
@@ -43,7 +43,6 @@ include_once $REX[INCLUDE_PATH]."/classes/class.ooarticle.inc.php";
 include_once $REX[INCLUDE_PATH]."/classes/class.ooarticleslice.inc.php";
 include_once $REX[INCLUDE_PATH]."/classes/class.oomediacategory.inc.php";
 include_once $REX[INCLUDE_PATH]."/classes/class.oomedia.inc.php";
-include_once $REX[INCLUDE_PATH]."/classes/class.ootemplate.inc.php";
 include_once $REX[INCLUDE_PATH]."/classes/class.textile.inc.php";
 
 if (!$REX[GG])
