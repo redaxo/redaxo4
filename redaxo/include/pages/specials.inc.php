160d6b37805521c129a1ca637be29da80844276e
staabm <markus.staab@redaxo.de>
2005-09-01 08:53:21 +0000

    - $REX[VAR] -> $REX['VAR']

--- redaxo/include/pages/specials.inc.php
+++ redaxo/include/pages/specials.inc.php
@@ -23,12 +23,12 @@ if ($spage == "standard")
 	{
 		// REACTIVATE SETUP
 		
-		$h = @fopen($REX[INCLUDE_PATH]."/master.inc.php","r");
-		$cont = fread($h,filesize($REX[INCLUDE_PATH]."/master.inc.php"));
+		$h = @fopen($REX['INCLUDE_PATH']."/master.inc.php","r");
+		$cont = fread($h,filesize($REX['INCLUDE_PATH']."/master.inc.php"));
 		$cont = ereg_replace("(REX\['SETUP'\].?\=.?)[^;]*","\\1"."true",$cont);
 		fclose($h);
 		// echo nl2br(htmlentities($cont));
-		$h = @fopen($REX[INCLUDE_PATH]."/master.inc.php","w+");
+		$h = @fopen($REX['INCLUDE_PATH']."/master.inc.php","w+");
 		if(fwrite($h,$cont,strlen($cont)) > 0)
 		{
 			$MSG = $I18N->msg("setup_error1");
@@ -86,7 +86,7 @@ if ($spage == "standard")
 		$cont = ereg_replace("(REX\['EMAIL'\].?\=.?)[^;]*","\\1\"".strtolower($neu_error_emailaddress)."\"",$cont);
 		$cont = ereg_replace("(REX\['error_emailaddress'\].?\=.?)[^;]*","\\1\"".strtolower($neu_error_emailaddress)."\"",$cont);
 		$cont = ereg_replace("(REX\['LANG'\].?\=.?)[^;]*","\\1\"".$neu_lang."\"",$cont);
-		$REX[LANG] = $neu_lang;
+		$REX['LANG'] = $neu_lang;
 		$cont = ereg_replace("(REX\['SERVER'\].?\=.?)[^;]*","\\1\"".($neu_SERVER)."\"",$cont);
 		$cont = ereg_replace("(REX\['SERVERNAME'\].?\=.?)[^;]*","\\1\"".($neu_SERVERNAME)."\"",$cont);
 		$cont = ereg_replace("(DB\['2'\]\['HOST'\].?\=.?)[^;]*","\\1\"".($neu_db2_host)."\"",$cont);
@@ -109,26 +109,26 @@ if ($spage == "standard")
 		fwrite($h,$cont,strlen($cont));
 		fclose($h);
 	
-		if ($neu_caching != "TRUE") $REX[CACHING] = false;
-		else $REX[CACHING] = true;
+		if ($neu_caching != "TRUE") $REX['CACHING'] = false;
+		else $REX['CACHING'] = true;
 		
-		if ($neu_caching_debug != "TRUE") $REX[CACHING_DEBUG] = false;
-		else $REX[CACHING_DEBUG] = true;
+		if ($neu_caching_debug != "TRUE") $REX['CACHING_DEBUG'] = false;
+		else $REX['CACHING_DEBUG'] = true;
 		
-		if ($neu_modrewrite != "TRUE") $REX[MOD_REWRITE] = false;
-		else $REX[MOD_REWRITE] = true;
+		if ($neu_modrewrite != "TRUE") $REX['MOD_REWRITE'] = false;
+		else $REX['MOD_REWRITE'] = true;
 	
 	
-		$REX[STARTARTIKEL_ID] = $neu_startartikel;
-		$REX[EMAIL] = $neu_error_emailaddress;
-		$REX[error_emailaddress] = $neu_error_emailaddress;
-		$REX[SERVER] = $neu_SERVER;
-		$REX[SERVERNAME] = $neu_SERVERNAME;
+		$REX['STARTARTIKEL_ID'] = $neu_startartikel;
+		$REX['EMAIL'] = $neu_error_emailaddress;
+		$REX['error_emailaddress'] = $neu_error_emailaddress;
+		$REX['SERVER'] = $neu_SERVER;
+		$REX['SERVERNAME'] = $neu_SERVERNAME;
 	
-		$DB[2][HOST] = $neu_db2_host;
-		$DB[2][LOGIN] = $neu_db2_login;
-		$DB[2][PSW] = $neu_db2_psw;
-		$DB[2][NAME] = $neu_db2_name;
+		$DB['2']['HOST'] = $neu_db2_host;
+		$DB['2']['LOGIN'] = $neu_db2_login;
+		$DB['2']['PSW'] = $neu_db2_psw;
+		$DB['2']['NAME'] = $neu_db2_name;
 	
 		$MSG = $I18N->msg("info_updated");
 	
@@ -154,48 +154,48 @@ if ($spage == "standard")
 	echo "<input type=hidden name=page value=specials>";
 	echo "<input type=hidden name=func value=updateinfos>";
 	echo "<tr><td colspan=3><b>".$I18N->msg("general_info_header")."</b></td></tr>";
-	echo "<tr><td width=170>\$REX[version]:</td><td width=10><img src=pics/leer.gif width=10 height=20></td><td>\"".$REX[version]."\"</td></tr>";
-	echo "<tr><td>\$REX[SERVER]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_SERVER value=\"".$REX[SERVER]."\" class=inp100></td></tr>";
-	echo "<tr><td>\$REX[SERVERNAME]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_SERVERNAME value=\"".$REX[SERVERNAME]."\" class=inp100></td></tr>";
+	echo "<tr><td width=170>\$REX['version']:</td><td width=10><img src=pics/leer.gif width=10 height=20></td><td>\"".$REX['version']."\"</td></tr>";
+	echo "<tr><td>\$REX['SERVER']:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_SERVER value=\"".$REX['SERVER']."\" class=inp100></td></tr>";
+	echo "<tr><td>\$REX['SERVERNAME']:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_SERVERNAME value=\"".$REX['SERVERNAME']."\" class=inp100></td></tr>";
 	
 	echo "<tr><td colspan=3><br><b>".$I18N->msg("db1_can_only_be_changed_by_setup")."</b></td></tr>";
 	
-	echo "<tr><td>\$DB[1][HOST]:</td><td><img src=pics/leer.gif width=10 height=20></td><td>\"".$DB[1][HOST]."\"</td></tr>";
-	echo "<tr><td>\$DB[1][LOGIN]:</td><td><img src=pics/leer.gif width=10 height=20></td><td>\"".$DB[1][LOGIN]."\"</td></tr>";
-	echo "<tr><td>\$DB[1][PSW]:</td><td><img src=pics/leer.gif width=10 height=20></td><td>-</td></tr>";
-	echo "<tr><td>\$DB[1][NAME]:</td><td><img src=pics/leer.gif width=10 height=20></td><td>\"".$DB[1][NAME]."\"</td></tr>";
+	echo "<tr><td>\$DB['1']['HOST']:</td><td><img src=pics/leer.gif width=10 height=20></td><td>\"".$DB['1']['HOST']."\"</td></tr>";
+	echo "<tr><td>\$DB['1']['LOGIN']:</td><td><img src=pics/leer.gif width=10 height=20></td><td>\"".$DB['1']['LOGIN']."\"</td></tr>";
+	echo "<tr><td>\$DB['1']['PSW']:</td><td><img src=pics/leer.gif width=10 height=20></td><td>-</td></tr>";
+	echo "<tr><td>\$DB['1']['NAME']:</td><td><img src=pics/leer.gif width=10 height=20></td><td>\"".$DB['1']['NAME']."\"</td></tr>";
 	
 	/*
 	echo "<tr><td colspan=3><br><b>".$I18N->msg("db2_text")."</b></td></tr>";
-	echo "<tr><td>\$DB[2][HOST]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_db2_host value=\"".$DB[2][HOST]."\" class=inp100></td></tr>";
-	echo "<tr><td>\$DB[2][LOGIN]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_db2_login value=\"".$DB[2][LOGIN]."\" class=inp100></td></tr>";
-	echo "<tr><td>\$DB[2][PSW]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_db2_psw value=\"".$DB[2][PSW]."\" class=inp100></td></tr>";
-	echo "<tr><td>\$DB[2][NAME]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_db2_name value=\"".$DB[2][NAME]."\" class=inp100></td></tr>";
+	echo "<tr><td>\$DB['2']['HOST']:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_db2_host value=\"".$DB['2']['HOST']."\" class=inp100></td></tr>";
+	echo "<tr><td>\$DB['2']['LOGIN']:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_db2_login value=\"".$DB['2']['LOGIN']."\" class=inp100></td></tr>";
+	echo "<tr><td>\$DB['2']['PSW']:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_db2_psw value=\"".$DB['2']['PSW']."\" class=inp100></td></tr>";
+	echo "<tr><td>\$DB['2']['NAME']:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_db2_name value=\"".$DB['2']['NAME']."\" class=inp100></td></tr>";
 	*/
 	
 	echo "<tr><td colspan=3><br><b>".$I18N->msg("specials_others")."</b></td></tr>";
-	echo "<tr><td>\$REX[WWW_PATH]:</td><td><img src=pics/leer.gif width=10 height=20></td><td>\"".$REX[WWW_PATH]."\"</td></tr>";
-	echo "<tr><td>\$REX[INCLUDE_PATH]:</td><td><img src=pics/leer.gif width=10 height=20></td><td>\"".$REX[INCLUDE_PATH]."\"</td></tr>";
-	echo "<tr><td>\$REX[error_emailaddress]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_error_emailaddress value=\"".$REX[error_emailaddress]."\" class=inp100></td></tr>";
-	echo "<tr><td>\$REX[STARTARTIKEL_ID]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_startartikel value=\"".$REX[STARTARTIKEL_ID]."\"></td></tr>";
-	echo "<tr><td>\$REX[LANG]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><select name=neu_lang size=1>";
-	foreach ($REX[LOCALES] as $l) {
-		$selected = ($l == $REX[LANG] ? "selected" : "");
+	echo "<tr><td>\$REX['WWW_PATH']:</td><td><img src=pics/leer.gif width=10 height=20></td><td>\"".$REX['WWW_PATH']."\"</td></tr>";
+	echo "<tr><td>\$REX['INCLUDE_PATH']:</td><td><img src=pics/leer.gif width=10 height=20></td><td>\"".$REX['INCLUDE_PATH']."\"</td></tr>";
+	echo "<tr><td>\$REX['error_emailaddress']:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_error_emailaddress value=\"".$REX['error_emailaddress']."\" class=inp100></td></tr>";
+	echo "<tr><td>\$REX['STARTARTIKEL_ID']:</td><td><img src=pics/leer.gif width=10 height=20></td><td><input type=text size=5 name=neu_startartikel value=\"".$REX['STARTARTIKEL_ID']."\"></td></tr>";
+	echo "<tr><td>\$REX['LANG']:</td><td><img src=pics/leer.gif width=10 height=20></td><td><select name=neu_lang size=1>";
+	foreach ($REX['LOCALES'] as $l) {
+		$selected = ($l == $REX['LANG'] ? "selected" : "");
 		echo "<option value='$l' $selected>$l</option>";
 	}
 	echo "</select></td></tr>";
 
-	if($REX[MOD_REWRITE]) $modcheck = "selected"; else $modcheck_false = "selected";
-	echo "<tr><td>\$REX[MOD_REWRITE]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><select name=neu_modrewrite size=1><option $modcheck>TRUE</option><option $modcheck_false>FALSE</option></select></td></tr>";
+	if($REX['MOD_REWRITE']) $modcheck = "selected"; else $modcheck_false = "selected";
+	echo "<tr><td>\$REX['MOD_REWRITE']:</td><td><img src=pics/leer.gif width=10 height=20></td><td><select name=neu_modrewrite size=1><option $modcheck>TRUE</option><option $modcheck_false>FALSE</option></select></td></tr>";
 
 
 	if($REX_USER->isValueOf("rights","caching[]")){
 		
 		echo "<tr><td colspan=3><br><b>Caching</b></td></tr>";
-		if($REX[CACHING]) $cachingcheck = "selected"; else $cachingcheck_false = "selected";
-		echo "<tr><td>\$REX[CACHING]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><select name=neu_caching size=1><option $cachingcheck>TRUE</option><option $cachingcheck_false>FALSE</option></select></td></tr>";
-		if($REX[CACHING_DEBUG]) $cachingdebugcheck = "selected"; else $cachingdebugcheck_false = "selected";
-		echo "<tr><td>\$REX[CACHING_DEBUG]:</td><td><img src=pics/leer.gif width=10 height=20></td><td><select name=neu_caching_debug size=1><option $cachingdebugcheck>TRUE</option><option $cachingdebugcheck_false>FALSE</option></select></td></tr>";
+		if($REX['CACHING']) $cachingcheck = "selected"; else $cachingcheck_false = "selected";
+		echo "<tr><td>\$REX['CACHING']:</td><td><img src=pics/leer.gif width=10 height=20></td><td><select name=neu_caching size=1><option $cachingcheck>TRUE</option><option $cachingcheck_false>FALSE</option></select></td></tr>";
+		if($REX['CACHING_DEBUG']) $cachingdebugcheck = "selected"; else $cachingdebugcheck_false = "selected";
+		echo "<tr><td>\$REX['CACHING_DEBUG']:</td><td><img src=pics/leer.gif width=10 height=20></td><td><select name=neu_caching_debug size=1><option $cachingdebugcheck>TRUE</option><option $cachingdebugcheck_false>FALSE</option></select></td></tr>";
 	}
 	echo "</td></tr>";
 	
@@ -213,7 +213,7 @@ if ($spage == "standard")
 	
 	// ------------------------------ clang definieren (sprachen)
 	
-	echo "<a name=clang></a>";
+	echo "<a name='clang'></a>";
 	
 	// ----- delete clang
 	if($delclang != "")
@@ -301,15 +301,15 @@ if ($spage == "standard")
 		echo "</form></tr>";
 	}
 	
-	reset($REX[CLANG]);
-	for ($i=0;$i<count($REX[CLANG]);$i++)
+	reset($REX['CLANG']);
+	for ($i=0;$i<count($REX['CLANG']);$i++)
 	{
-		if ($clang_id==key($REX[CLANG]) and $clang_id!="" and $func == "editclang")
+		if ($clang_id==key($REX['CLANG']) and $clang_id!="" and $func == "editclang")
 		{
 			echo "<tr><form action=index.php#clang method=post><input type=hidden name=page value=specials><input type=hidden name=spage value=lang><input type=hidden name=clang_id value=$clang_id><input type=hidden name=func value=editclangsave>";
 			echo "<td>edit</td>";
-			echo "<td align=center class=grey>".key($REX[CLANG])."</td>";
-			echo "<td><input type=text size=10 class=inp100 name=clang_name value='".htmlentities(current($REX[CLANG]))."''></td>";
+			echo "<td align=center class=grey>".key($REX['CLANG'])."</td>";
+			echo "<td><input type=text size=10 class=inp100 name=clang_name value='".htmlentities(current($REX['CLANG']))."''></td>";
 			echo "<td><input type=submit name=edit value=".$I18N->msg('edit').">";
 			if ($clang_id>0) echo "<input type=submit name=delclang value=delete onclick='return confirm(\"".$I18N->msg('delete')." ?\")'>";
 			echo "</td>";
@@ -319,11 +319,11 @@ if ($spage == "standard")
 		{
 			echo "<tr>" .
 					"<td></td>" .
-					"<td align=center>".key($REX[CLANG])."</td>" .
-					"<td><a href=index.php?page=specials&spage=lang&func=editclang&clang_id=".key($REX[CLANG])."#clang>".htmlentities(current($REX[CLANG]))."</a></td>" .
+					"<td align=center>".key($REX['CLANG'])."</td>" .
+					"<td><a href=index.php?page=specials&spage=lang&func=editclang&clang_id=".key($REX['CLANG'])."#clang>".htmlentities(current($REX['CLANG']))."</a></td>" .
 					"<td></td></tr>";
 		}
-		next($REX[CLANG]);
+		next($REX['CLANG']);
 	}
 	echo "</table>";	
 	
@@ -354,7 +354,7 @@ if ($spage == "standard")
 			if(is_array($result)){
 				$message = $I18N->msg("article_type_still_used")."<br>";
 				foreach($result as $var){
-					$message.= "<br><a href=index.php?page=content&article_id=".$var[id]."&mode=meta target=_blank>".$var[name]."</a>";
+					$message.= "<br><a href=index.php?page=content&article_id=".$var['id']."&mode=meta target=_blank>".$var['name']."</a>";
 				}
 				$message.="<br><br>";
 			} else {
