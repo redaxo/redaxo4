4e4adb8163a1924f87bf30ada7c637acbf1c156b
staabm <markus.staab@redaxo.de>
2005-09-17 15:53:07 +0000

    - Setzten des arg_separators, falls Sessions verwendet werden, um XHTML valide links zu produzieren
    - $REX[var] ->  $REX['var']

--- index.php
+++ index.php
@@ -1,42 +1,50 @@
 <?php
 
+
+// --------------------------- ini settings
+
+// Setzten des arg_separators, falls Sessions verwendet werden,
+// um XHTML valide Links zu produzieren
+@ini_set( 'arg_separator.input', '&amp;');
+@ini_set( 'arg_separator.output', '&amp;');
+
 // --------------------------- globals
 
 unset($REX);
 
 // Übernahme von FormularImagePosition
 
-$REX[x] = $x;
-$REX[y] = $y;
+$REX['x'] = $x;
+$REX['y'] = $y;
 
 // Flag ob Inhalte mit Redaxo aufgerufen oder
 // von der Webseite aus
 // Kann wichtig für die Darstellung sein
 // Sollte immer false bleiben
 
-$REX[REDAXO] = false;
+$REX['REDAXO'] = false;
 
 // Wenn $REX[GG] = true; dann wird der
 // Content aus den redaxo/include/generated/
 // genommen
 
-$REX[GG] = true;
+$REX['GG'] = true;
 
 // setzte pfad und includiere klassen und funktionen
 
-$REX[HTDOCS_PATH] = "./";
+$REX['HTDOCS_PATH'] = "./";
 include "./redaxo/include/master.inc.php";
 
 // Starte einen neuen Artikel und setzte die aktuelle
 // artikel id. wenn nicht vorhanden, nimm einen
 // speziellen artikel. z.b. fehler seite oder home seite
 
-if ($article_id == "") $article_id = $REX[STARTARTIKEL_ID];
+if ($article_id == "") $article_id = $REX['STARTARTIKEL_ID'];
 
 $SHOWARTICLE = true;
 
 // If Caching is true start engine
-if($REX[CACHING])
+if($REX['CACHING'])
 {
 
 	$Cache = new Cache($article_id);
@@ -45,8 +53,8 @@ if($REX[CACHING])
 		if($Cache->isCacheFile())
 		{
 			$Cache->printCacheFile();
-			if($REX[CACHING_DEBUG]) print "<br>CachedVersion<br>";
-			if($REX[CACHING_DEBUG]) print "Script time: ".showScripttime();
+			if($REX['CACHING_DEBUG']) print "<br>CachedVersion<br>";
+			if($REX['CACHING_DEBUG']) print "Script time: ".showScripttime();
 			$SHOWARTICLE = false;
 		} else {
 			// start caching
@@ -63,13 +71,13 @@ if ($SHOWARTICLE)
 	if ($REX_ARTICLE->setArticleId($article_id))
 	{
 		$REX_ARTICLE->getArticleTemplate();
-	}elseif($REX_ARTICLE->setArticleId($REX[STARTARTIKEL_ID]))
+	}elseif($REX_ARTICLE->setArticleId($REX['STARTARTIKEL_ID']))
 	{		
 		$REX_ARTICLE->getArticleTemplate();
 	}else
 	{
 		echo "Kein Startartikel selektiert / No starting Article selected. Please click here to enter <a href=redaxo/index.php>redaxo</a>";
-		$REX[STATS] = 0;
+		$REX['STATS'] = 0;
 	}
 	//////////////////////////////////////////////
 	// advanced caching
@@ -77,12 +85,12 @@ if ($SHOWARTICLE)
 	
 	if($Cache->makeCacheFile){
 		$Cache->writeCacheFile();
-		if($REX[CACHING_DEBUG]) print "<br>MadeCache<br>";
+		if($REX['CACHING_DEBUG']) print "<br>MadeCache<br>";
 	} else {
-		if($REX[CACHING_DEBUG]) print "<br>Live<br>";
+		if($REX['CACHING_DEBUG']) print "<br>Live<br>";
 	}
 	
-	if($REX[CACHING_DEBUG]) print "Script time: ".showScripttime();
+	if($REX['CACHING_DEBUG']) print "Script time: ".showScripttime();
 	
 	//////////////////////////////////////////////	
 }
