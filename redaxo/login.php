779d5978fb3c45cadb8d379933e3947bee67b8b0
staabm <markus.staab@redaxo.de>
2005-09-29 17:43:44 +0000

    - I18N::getLocales -> $I18N->getLocales()

--- redaxo/login.php
+++ redaxo/login.php
@@ -2,27 +2,26 @@
 
 unset($REX);
 
-$REX[HTDOCS_PATH] = "../";
-$REX[GG] = false;
+$REX['HTDOCS_PATH'] = "../";
+$REX['GG'] = false;
 
 include "./include/master.inc.php";
 
 // ----------------- CREATE LANG OBJ
-$I18N = new i18n($REX[LANG],$REX[INCLUDE_PATH]."/lang/");
-$REX[LOCALES] = i18n::getLocales($REX[INCLUDE_PATH]."/lang/");
+rex_create_lang( $REX['LANG']);
 setlocale(LC_ALL,trim($I18N->msg("setlocale")));
 
 $LOGIN = FALSE;
 
-include $REX[INCLUDE_PATH]."/layout/top.php";
+include $REX['INCLUDE_PATH']."/layout/top.php";
 
 title("Login","");
 
-if ($FORM[loginmessage] != "")
+if ($FORM['loginmessage'] != "")
 {
 	echo "<table border=0 cellpadding=5 cellspacing=1 width=770>
 	<tr><td align=center class=warning width=40><img src=pics/warning.gif width=16 height=16></td>
-	<td class=warning>".$FORM[loginmessage]."</td></tr>
+	<td class=warning>".$FORM['loginmessage']."</td></tr>
 	</table>";
 }
 
@@ -46,6 +45,6 @@ echo "<br><table border=1 cellpadding=5 cellspacing=0 width=770>
 </table>
 ";
 
-include $REX[INCLUDE_PATH]."/layout/bottom.php";
+include $REX['INCLUDE_PATH']."/layout/bottom.php";
 
 ?>
