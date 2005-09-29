22f80cd56570f0ceee4857279216755f5eeadb1c
staabm <markus.staab@redaxo.de>
2005-09-29 18:18:23 +0000

    - Fehlerausgabe bei der Installation funktioniert jetzt auch bei der Reinstallation - $REX[var] ->  $REX['var']

--- redaxo/include/pages/addon.inc.php
+++ redaxo/include/pages/addon.inc.php
@@ -2,12 +2,12 @@
 
 title($I18N->msg("addon"),"");
 
-$dir = $REX[INCLUDE_PATH]."/addons/";
+$dir = $REX['INCLUDE_PATH']."/addons/";
 chdir($dir);
 $hdl = opendir(".");
 while (false !== ($file = readdir($hdl)))
 {
-	if($file != ".." AND $file != ".") if(is_dir($file))
+	if($file != ".." AND $file != "." && is_dir($file))
 	{
 		$ADDONS[] = $file;
 	}
@@ -23,7 +23,7 @@ if ($spage=="help" && array_search($addonname,$ADDONS) !== false)
 	echo "<table class=rex style=table-layout:auto; cellpadding=5 cellspacing=1>";
 	echo "<tr><th>".$I18N->msg("addon_help")." $addonname</th></tr>";
 	echo "<tr><td>";
-	if (!@include $REX[INCLUDE_PATH]."/addons/$addonname/help.inc.php") echo $I18N->msg("addon_no_help_file");
+	if (!@include $REX['INCLUDE_PATH']."/addons/$addonname/help.inc.php") echo $I18N->msg("addon_no_help_file");
 	echo "&nbsp;</td></tr>";
 	echo "<tr><td><a href=index.php?page=addon>".$I18N->msg("addon_back")."</a></td></tr>";
 	echo "</table>";
@@ -37,45 +37,45 @@ if (array_search($addonname,$ADDONS) !== false)
 	// $addonname ist vorhanden
 	if ($install == 1)
 	{
-		if (!@include $REX[INCLUDE_PATH]."/addons/$addonname/install.inc.php")
+		if (!@include $REX['INCLUDE_PATH']."/addons/$addonname/install.inc.php")
 		{
 			$errmsg = $I18N->msg("addon_install_not_found");
 		}else
 		{
-			if ($REX[ADDON][install][$addonname] != 1)
+			if ($REX['ADDON']['install'][$addonname] != 1 || $REX['ADDON']['installmsg'][$addonname] != "")
 			{
-				$errmsg = "'$addonname' ".$I18N->msg("addon_no_install")." ";
-				if ($REX[ADDON][installmsg][$addonname] == "") $errmsg .= $I18N->msg("addon_no_reason");
-				else $errmsg .= $REX[ADDON][installmsg][$addonname];
+				$errmsg = "'$addonname' ".$I18N->msg("addon_no_install")."<br/>";
+				if ($REX['ADDON']['installmsg'][$addonname] == "") $errmsg .= $I18N->msg("addon_no_reason");
+				else $errmsg .= $REX['ADDON']['installmsg'][$addonname];
 			}else
 			{
 				// include config.
 				// if config is broken installation prozess will be terminated -> no install -> no errors in redaxo
 				
 				// skip config if it is a reinstall !
-				if($REX[ADDON][status][$addonname]!=1){
-					include $REX[INCLUDE_PATH]."/addons/$addonname/config.inc.php";
+				if($REX['ADDON']['status'][$addonname]!=1){
+					include $REX['INCLUDE_PATH']."/addons/$addonname/config.inc.php";
 				}
 				$errmsg = $addonname." ".$I18N->msg("addon_installed");
-				$REX[ADDON][install][$addonname] = 1;
+				$REX['ADDON']['install'][$addonname] = 1;
 				$errmsg = $I18N->msg("addon_installed");
 				$WA = true;
 			}
 		}
 	}elseif($activate == 1)
 	{
-		if ($REX[ADDON][install][$addonname]!=1)
+		if ($REX['ADDON']['install'][$addonname]!=1)
 		{
 			$errmsg = $I18N->msg("addon_no_activation");
 		}else
 		{
-			$REX[ADDON][status][$addonname] = 1;
+			$REX['ADDON']['status'][$addonname] = 1;
 			$errmsg = $I18N->msg("addon_activated");
 			$WA = true;
 		}
 	}elseif($activate == "0")
 	{
-		$REX[ADDON][status][$addonname] = 0;
+		$REX['ADDON']['status'][$addonname] = 0;
 		$errmsg = $I18N->msg("addon_deactivated");
 		$WA = true;
 	}
@@ -101,7 +101,7 @@ if ($WA)
 	}
 	$content .= "\n\r// --- /DYN";
 
-	$file = $REX[INCLUDE_PATH]."/addons.inc.php";
+	$file = $REX['INCLUDE_PATH']."/addons.inc.php";
 	$h = fopen($file,"r");
 	$fcontent = fread($h,filesize($file));
 	$fcontent = ereg_replace("(\/\/.---.DYN.*\/\/.---.\/DYN)",$content,$fcontent);
@@ -135,9 +135,9 @@ if ($SP)
 	for ($i=0;$i<count($ADDONS);$i++)
 	{
 		$cur = current($ADDONS);
-		if ($REX[ADDON][install][$cur] == 1) $install = $I18N->msg("addon_yes")." - <a href=index.php?page=addon&addonname=$cur&install=1>".$I18N->msg("addon_reinstall")."</a>";
+		if ($REX['ADDON']['install'][$cur] == 1) $install = $I18N->msg("addon_yes")." - <a href=index.php?page=addon&addonname=$cur&install=1>".$I18N->msg("addon_reinstall")."</a>";
 		else $install = $I18N->msg("addon_no")." - <a href=index.php?page=addon&addonname=$cur&install=1>".$I18N->msg("addon_install")."</a>";
-		if ($REX[ADDON][status][$cur] == 1) $status = $I18N->msg("addon_yes")." - <a href=index.php?page=addon&addonname=$cur&activate=0>".$I18N->msg("addon_deactivate")."</a>";
+		if ($REX['ADDON']['status'][$cur] == 1) $status = $I18N->msg("addon_yes")." - <a href=index.php?page=addon&addonname=$cur&activate=0>".$I18N->msg("addon_deactivate")."</a>";
 		else $status = $I18N->msg("addon_no")." - <a href=index.php?page=addon&addonname=$cur&activate=1>".$I18N->msg("addon_activate")."</a>";
 	
 		echo "<tr>
