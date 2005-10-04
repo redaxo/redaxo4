f8688834fa1c881e5b9d8d52f7fee739f0bd4a53
Koala <Sven@koalashome.de>
2005-10-04 21:12:08 +0000

    Undefined variable: $x und $y; isset()-Abfrage eingebunden

--- index.php
+++ index.php
@@ -17,6 +17,9 @@ unset($REX);
 
 // Übernahme von FormularImagePosition
 
+if (!isset($x)) $x = '';
+if (!isset($y)) $y = '';
+
 $REX['x'] = $x;
 $REX['y'] = $y;
 
@@ -42,7 +45,7 @@ include "./redaxo/include/master.inc.php";
 // artikel id. wenn nicht vorhanden, nimm einen
 // speziellen artikel. z.b. fehler seite oder home seite
 
-if ($article_id == "") $article_id = $REX['STARTARTIKEL_ID'];
+if (!isset($article_id) or $article_id == '') $article_id = $REX['STARTARTIKEL_ID'];
 
 $SHOWARTICLE = true;
 
@@ -50,52 +53,52 @@ $SHOWARTICLE = true;
 if($REX['CACHING'])
 {
 
-	$Cache = new Cache($article_id);
-	if($Cache->isCacheConf())
-	{
-		if($Cache->isCacheFile())
-		{
-			$Cache->printCacheFile();
-			if($REX['CACHING_DEBUG']) print "<br>CachedVersion<br>";
-			if($REX['CACHING_DEBUG']) print "Script time: ".showScripttime();
-			$SHOWARTICLE = false;
-		} else {
-			// start caching
-			$Cache->startCacheFile();
-		}
-	}
+  $Cache = new Cache($article_id);
+  if($Cache->isCacheConf())
+  {
+    if($Cache->isCacheFile())
+    {
+      $Cache->printCacheFile();
+      if ($REX['CACHING_DEBUG']) print "<br>CachedVersion<br>";
+      if ($REX['CACHING_DEBUG']) print "Script time: ".showScripttime();
+      $SHOWARTICLE = false;
+    } else {
+      // start caching
+      $Cache->startCacheFile();
+    }
+  }
 
 }
 
 if ($SHOWARTICLE)
 {
-	$REX_ARTICLE = new article;
-	$REX_ARTICLE->setCLang($clang);
-	if ($REX_ARTICLE->setArticleId($article_id))
-	{
-		$REX_ARTICLE->getArticleTemplate();
-	}elseif($REX_ARTICLE->setArticleId($REX['STARTARTIKEL_ID']))
-	{		
-		$REX_ARTICLE->getArticleTemplate();
-	}else
-	{
-		echo "Kein Startartikel selektiert / No starting Article selected. Please click here to enter <a href=redaxo/index.php>redaxo</a>";
-		$REX['STATS'] = 0;
-	}
-	//////////////////////////////////////////////
-	// advanced caching
-	//////////////////////////////////////////////
-	
-	if($Cache->makeCacheFile){
-		$Cache->writeCacheFile();
-		if($REX['CACHING_DEBUG']) print "<br>MadeCache<br>";
-	} else {
-		if($REX['CACHING_DEBUG']) print "<br>Live<br>";
-	}
-	
-	if($REX['CACHING_DEBUG']) print "Script time: ".showScripttime();
-	
-	//////////////////////////////////////////////	
+  $REX_ARTICLE = new article;
+  $REX_ARTICLE->setCLang($clang);
+  if ($REX_ARTICLE->setArticleId($article_id))
+  {
+    $REX_ARTICLE->getArticleTemplate();
+  }elseif($REX_ARTICLE->setArticleId($REX['STARTARTIKEL_ID']))
+  {   
+    $REX_ARTICLE->getArticleTemplate();
+  }else
+  {
+    echo "Kein Startartikel selektiert / No starting Article selected. Please click here to enter <a href=redaxo/index.php>redaxo</a>";
+    $REX['STATS'] = 0;
+  }
+  //////////////////////////////////////////////
+  // advanced caching
+  //////////////////////////////////////////////
+  
+  if(isset($Cache->makeCacheFile) and $Cache->makeCacheFile){
+    $Cache->writeCacheFile();
+    if($REX['CACHING_DEBUG']) print "<br>MadeCache<br>";
+  } else {
+    if($REX['CACHING_DEBUG']) print "<br>Live<br>";
+  }
+  
+  if($REX['CACHING_DEBUG']) print "Script time: ".showScripttime();
+  
+  //////////////////////////////////////////////  
 }
 
 
@@ -107,7 +110,7 @@ ob_end_clean();
 
 // ---- user functions vorhanden ? wenn ja ausführen
 
-if (is_array($REX['OUTPUT_FILTER']))
+if (isset($REX['OUTPUT_FILTER']) and is_array($REX['OUTPUT_FILTER']))
 {
 	reset ($REX['OUTPUT_FILTER']);
 	for ($i=0;$i<count($REX['OUTPUT_FILTER']);$i++)
@@ -119,7 +122,7 @@ if (is_array($REX['OUTPUT_FILTER']))
 
 // ---- caching functions vorhanden ? wenn ja ausführen
 
-if (is_array($REX['OUTPUT_FILTER_CACHE']))
+if (isset($REX['OUTPUT_FILTER_CACHE']) and is_array($REX['OUTPUT_FILTER_CACHE']))
 {
 	reset ($REX['OUTPUT_FILTER_CACHE']);
 	for ($i=0;$i<count($REX['OUTPUT_FILTER_CACHE']);$i++)
