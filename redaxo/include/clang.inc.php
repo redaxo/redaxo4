9b1c8e59f8766946fcd8ebcbf16a08ec878ffe96
Koala <Sven@koalashome.de>
2005-10-09 10:27:04 +0000

    "Use of undefined constant" beseitigt

--- redaxo/include/clang.inc.php
+++ redaxo/include/clang.inc.php
@@ -2,9 +2,9 @@
 
 // ----------------- CLANG
 // --- DYN
-$REX[CLANG][0] = "deutsch";
-$REX[CLANG][1] = "english";
-$REX[CLANG][3] = "finnisch";
+$REX['CLANG']['0'] = "deutsch";
+$REX['CLANG']['1'] = "english";
+$REX['CLANG']['3'] = "finnisch";
 // --- /DYN
 
 ?>
