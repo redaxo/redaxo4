3a303bdfe0eae79f49411086bdd112f0d096ecc8
staabm <markus.staab@redaxo.de>
2005-10-20 13:33:02 +0000

    - $REX[var] -> $REX['var']

--- redaxo/include/addons/stats/config.inc.php
+++ redaxo/include/addons/stats/config.inc.php
@@ -2,20 +2,20 @@
 
 $mypage = "stats"; // only for this file
 
-include_once $REX[INCLUDE_PATH]."/addons/$mypage/classes/class.stats.inc.php";
+include_once $REX['INCLUDE_PATH']."/addons/$mypage/classes/class.stats.inc.php";
 
-if (!$REX[GG])
+if (!$REX['GG'])
 {
 	// only backend
 
  	// CREATE LANG OBJ FOR THIS ADDON
-	$I18N_STATS = new i18n($REX[LANG],$REX[INCLUDE_PATH]."/addons/$mypage/lang/");
+	$I18N_STATS = new i18n($REX['LANG'],$REX['INCLUDE_PATH']."/addons/$mypage/lang/");
 
-	$REX[ADDON][rxid][$mypage] = "7"; // unique redaxo addon id
-	$REX[ADDON][page][$mypage] = "$mypage";
-	$REX[ADDON][name][$mypage] = $I18N_STATS->msg("stats_title");
-	$REX[ADDON][perm][$mypage] = "stats[]";
-	$REX[PERM][] = "stats[]";
+	$REX['ADDON']['rxid'][$mypage] = "7"; // unique redaxo addon id
+	$REX['ADDON']['page'][$mypage] = $mypage;
+	$REX['ADDON']['name'][$mypage] = $I18N_STATS->msg("stats_title");
+	$REX['ADDON']['perm'][$mypage] = "stats[]";
+	$REX['PERM'][''] = "stats[]";
 
 }else
 {
@@ -25,6 +25,6 @@ if (!$REX[GG])
 
 // backend and frontend
 
-$REX[ADDON][tbl][log][$mypage] = "rex_7_log"; // wir noch nicht benutzt
+$REX['ADDON']['tbl']['log'][$mypage] = "rex_7_log"; // wir noch nicht benutzt
 
 ?>
