2fd59f189d3a338d3761473e84e20fdb956e6aa6
staabm <markus.staab@redaxo.de>
2005-10-20 13:30:09 +0000

    - Bug in der Installation

--- redaxo/include/addons/stats/install.inc.php
+++ redaxo/include/addons/stats/install.inc.php
@@ -1,8 +1,6 @@
 <?php
 
-$REX[ADDON][install]["stats"] = 1;
-// ERRMSG IN CASE: $REX[ADDON][installmsg]["stats"] = "Leider konnte nichts installiert werden da.";
-
-$REX[ADDON][installmsg]["stats"] = "Leider konnte nichts installiert werden da.";
+$REX['ADDON']['install']['stats'] = 1;
+// ERRMSG IN CASE: $REX['ADDON']['installmsg']['stats'] = "Leider konnte nichts installiert werden da.";
 
 ?>
