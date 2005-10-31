45af0dd6b383a2bc15492c1e1ec78ca65dd5bbc1
Koala <Sven@koalashome.de>
2005-10-31 19:03:21 +0000

    Use of undefined constant UID

--- redaxo/include/classes/class.login.inc.php
+++ redaxo/include/classes/class.login.inc.php
@@ -1,4 +1,9 @@
 <?php
+/** 
+ *  
+ * @package redaxo3
+ * @version $Id$
+ */ 
 
 // class login 1.0
 // 
@@ -11,124 +16,124 @@
 
 class login{
 
-	var $DB;
-	var $error_page;
-	var $session_duration;
-	var $login_query;
-	var $user_query;
-	var $system_id;
-	var $usr_login;
-	var $usr_psw;
-	var $logout;
-	var $message;
-	var $uid;
-	var $USER;
-	var $text;
-	
-	function login()
-	{
-		$this->DB = 1;
-		$this->logout = false;
-		$this->message = "";
-		$this->system_id = "default";
-		$this->setLanguage();
-	}
-	
-	function setLanguage($lang = "en")
-	{
-		if ($lang == "de")
-		{
-			$this->text[10] = "Session beendet.";
-			$this->text[20] = "ID nicht gefunden.";
-			$this->text[30] = "Fehler beim Login.";
-			$this->text[40] = "Bitte einloggen.";
-			$this->text[50] = "Ausgeloggt.";
-		}else
-		{
-			$this->text[10] = "your session is expired !";
-			$this->text[20] = "uid not found";
-			$this->text[30] = "login wrong";
-			$this->text[40] = "login please";
-			$this->text[50] = "You logged out.";
-		}		
-	}
-	
-	function setSqlDb($DB)
-	{
-		$this->DB = $DB;
-	}
-	
-	function setSysID($system_id)
-	{
-		$this->system_id = $system_id;
-	}
-	
-	function setSessiontime($session_duration)
-	{
-		$this->session_duration = $session_duration;	
-	}
-	
-	function setLogin($usr_login,$usr_psw)
-	{
-		$this->usr_login = $usr_login;
-		$this->usr_psw = $usr_psw;
-	}
-	
-	function setLogout($logout)
-	{
-		$this->logout = $logout;	
-	}
-	
-	function setUserquery($login_query)
-	{
-		$this->user_query = $login_query;
-	}	
-	
-	function setLoginquery($user_query)
-	{
-		$this->login_query = $user_query;
-	}
-	
-	function setUserID($uid)
-	{
-		$this->uid = $uid;
-	}
-	
-	function setMessage($message)
-	{
-		$this->message = $message;	
-	}
-	
-	function checkLogin()
-	{
-		
-		// wenn logout dann header schreiben und auf error seite verweisen
-		// message schreiben
-		
-		$ok = false;
-		
-		if (!$this->logout)
-		{
-			
-			if ($this->usr_login != "")
-			{
-				// wenn login daten eingegeben dann checken
-				// auf error seite verweisen und message schreiben
-				
-				$this->USER = new sql($this->DB);
-				$USR_LOGIN = $this->usr_login;
-				$USR_PSW = $this->usr_psw;
+  var $DB;
+  var $error_page;
+  var $session_duration;
+  var $login_query;
+  var $user_query;
+  var $system_id;
+  var $usr_login;
+  var $usr_psw;
+  var $logout;
+  var $message;
+  var $uid;
+  var $USER;
+  var $text;
+  
+  function login()
+  {
+    $this->DB = 1;
+    $this->logout = false;
+    $this->message = "";
+    $this->system_id = "default";
+    $this->setLanguage();
+  }
+  
+  function setLanguage($lang = "en")
+  {
+    if ($lang == "de")
+    {
+      $this->text[10] = "Session beendet.";
+      $this->text[20] = "ID nicht gefunden.";
+      $this->text[30] = "Fehler beim Login.";
+      $this->text[40] = "Bitte einloggen.";
+      $this->text[50] = "Ausgeloggt.";
+    }else
+    {
+      $this->text[10] = "your session is expired !";
+      $this->text[20] = "uid not found";
+      $this->text[30] = "login wrong";
+      $this->text[40] = "login please";
+      $this->text[50] = "You logged out.";
+    }   
+  }
+  
+  function setSqlDb($DB)
+  {
+    $this->DB = $DB;
+  }
+  
+  function setSysID($system_id)
+  {
+    $this->system_id = $system_id;
+  }
+  
+  function setSessiontime($session_duration)
+  {
+    $this->session_duration = $session_duration;  
+  }
+  
+  function setLogin($usr_login,$usr_psw)
+  {
+    $this->usr_login = $usr_login;
+    $this->usr_psw = $usr_psw;
+  }
+  
+  function setLogout($logout)
+  {
+    $this->logout = $logout;  
+  }
+  
+  function setUserquery($login_query)
+  {
+    $this->user_query = $login_query;
+  } 
+  
+  function setLoginquery($user_query)
+  {
+    $this->login_query = $user_query;
+  }
+  
+  function setUserID($uid)
+  {
+    $this->uid = $uid;
+  }
+  
+  function setMessage($message)
+  {
+    $this->message = $message;  
+  }
+  
+  function checkLogin()
+  {
+    
+    // wenn logout dann header schreiben und auf error seite verweisen
+    // message schreiben
+    
+    $ok = false;
+    
+    if (!$this->logout)
+    {
+      
+      if ($this->usr_login != "")
+      {
+        // wenn login daten eingegeben dann checken
+        // auf error seite verweisen und message schreiben
+        
+        $this->USER = new sql($this->DB);
+        $USR_LOGIN = $this->usr_login;
+        $USR_PSW = $this->usr_psw;
 
-				$query = str_replace("USR_LOGIN",$this->usr_login,$this->login_query);
-				$query = str_replace("USR_PSW",$this->usr_psw,$query);
-				
-//				 $this->USER->debugsql = 1;
-				
-				$this->USER->setQuery($query);
-				if ($this->USER->getRows() == 1)
-				{
-					$ok = true;
-					$_SESSION['UID'][$this->system_id] = $this->USER->getValue($this->uid);
+        $query = str_replace("USR_LOGIN",$this->usr_login,$this->login_query);
+        $query = str_replace("USR_PSW",$this->usr_psw,$query);
+        
+//         $this->USER->debugsql = 1;
+        
+        $this->USER->setQuery($query);
+        if ($this->USER->getRows() == 1)
+        {
+          $ok = true;
+          $_SESSION['UID'][$this->system_id] = $this->USER->getValue($this->uid);
                     
                     // Erfolgreicher Login in der DB vermerken
                     $sql = new sql();
@@ -140,7 +145,7 @@ class login{
           // session_unregister("REX_SESSION");
         }
                 
-      }elseif($_SESSION['UID'][$this->system_id]!="")
+      } elseif (isset($_SESSION['UID']) and $_SESSION['UID'][$this->system_id] != '')
       {
         // wenn kein login und kein logout dann nach sessiontime checken
         // message schreiben und falls falsch auf error verweisen
@@ -187,7 +192,7 @@ class login{
       
       //session_register("REX_SESSION");
 
-			// $_SESSION['REX_SESSION'] = $REX_SESSION;
+      // $_SESSION['REX_SESSION'] = $REX_SESSION;
 
     }else
     {
