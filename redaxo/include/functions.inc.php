89486b5148e181bcd79e85125e33f9011ecfb9c6
staabm <markus.staab@redaxo.de>
2005-11-04 02:59:18 +0000

    - rexExtensions included

--- redaxo/include/functions.inc.php
+++ redaxo/include/functions.inc.php
@@ -71,6 +71,7 @@ include_once $REX['INCLUDE_PATH']."/classes/class.wysiwyg.inc.php";
 
 // ----- FUNCTIONS
 include_once $REX['INCLUDE_PATH']."/functions/function_rex_modrewrite.inc.php";
+include_once $REX['INCLUDE_PATH']."/functions/function_rex_extension.inc.php";
 
 // ----- EXTRA FUNCTIONS
 include_once $REX['INCLUDE_PATH']."/functions/function_rex_wysiwyg.inc.php";
