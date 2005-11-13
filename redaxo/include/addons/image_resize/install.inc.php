3bec9065a51ee27968c71484e2970caece060b78
staabm <markus.staab@redaxo.de>
2005-11-13 15:41:35 +0000

    - GD Extension check eingebaut

--- redaxo/include/addons/image_resize/install.inc.php
+++ redaxo/include/addons/image_resize/install.inc.php
@@ -1,6 +1,20 @@
 <?php
 
-$REX['ADDON']['install']["image_resize"] = 1;
+$error = '';
 
+if (!extension_loaded('gd')) {
+   if (!dl('gd.so')) {
+       $error = 'GD-LIB-extension not available! See <a href="http://www.php.net/gd">http://www.php.net/gd</a>'; 
+   }
+}
+
+if ($error != '')
+{
+  $REX['ADDON']['installmsg']['image_resize'] = $error;
+}
+else
+{
+  $REX['ADDON']['install']['image_resize'] = true;
+}
 
 ?>
