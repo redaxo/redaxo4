a3ca1a1307f4aac7d8e5b6011f32237b4372db38
dergel <jan.kristinus@yakamara.de>
2005-11-25 00:40:58 +0000

    unnoetige REX raus.. fileperm eingebaut, version angepasst

--- redaxo/include/master.inc.php
+++ redaxo/include/master.inc.php
@@ -9,28 +9,22 @@ if (!isset($page)) $page = '';
 // ----------------- SERVER VARS
 
 $REX['SETUP'] = false; 			// Setupservicestatus - if everything ok -> false; if problem set to true;
-$REX['SERVER'] = "redaxo.com";
-$REX['SERVERNAME'] = "Redaxo-Demo";
+$REX['SERVER'] = "redaxo.de";
+$REX['SERVERNAME'] = "REDAXO";
 $REX['error_emailaddress'] = "jan.kristinus@pergopa.de";
-$REX['version'] = "3.0";
+$REX['version'] = "3.1";
 $REX['subversion'] = "1";
 $REX['MYSQL_VERSION'] = ""; // Is set first time SQL Object ist initialised
 $REX['STARTARTIKEL_ID'] = 1; // FIRST ARTICLE
-$REX['STATS'] = 1; // STATS
-$REX['LANG'] = "de_sn"; // select default language
+$REX['LANG'] = "de_de"; // select default language
 $REX['MOD_REWRITE'] = false; // activate mod_rewrite support
 $REX['WWW_PATH'] = ""; //
 $REX['DOC_ROOT'] = ""; // 
 $REX['INCLUDE_PATH'] = $REX['DOC_ROOT'].$REX['HTDOCS_PATH']."redaxo/include"; // 
 $REX['MEDIAFOLDER'] = $REX['HTDOCS_PATH']."files"; //
-$REX['COMMUNITY'] = false;
-$REX['CACHING'] = false; // Caching
-$REX['CACHING_DEBUG'] = false; // Shows debugmessage
 $REX['TABLE_PREFIX'] = "rex_";
-$REX['MEDIAFOLDERPERM'] = "0777"; // 774
-$REX['FILEPERM'] = "0777";
+$REX['FILEPERM'] = octdec(775); // oktaler wert
 $REX['INSTNAME'] = "rex20050619161609";
-
 
 // ----------------- DB1
 $DB['1']['HOST'] = "localhost";
@@ -50,7 +44,6 @@ if(!isset($REX['NOFUNCTIONS']) or !$REX['NOFUNCTIONS']) include_once ($REX['INCL
 // -----------------
 if (!isset($category_id) or $category_id == "") $category_id = 0;
 if (!isset($ctype) or $ctype == "") $ctype = 0;
-
 
 // ----------------- set to default
 $REX['NOFUNCTIONS'] = false;
