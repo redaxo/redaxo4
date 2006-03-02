8653c02a067e1eafb45ad3a93c11f68630eb1ac3
staabm <markus.staab@redaxo.de>
2006-03-02 14:10:39 +0000

    - ExtensionPoint erstellt   + CLANG_ADDED, CLANG_UPDATED, CLANG_DELETED

--- redaxo/include/functions/function_rex_generate.inc.php
+++ redaxo/include/functions/function_rex_generate.inc.php
@@ -932,6 +932,9 @@ function rex_deleteCLang($id)
   $del = new sql();
   $del->query("delete from rex_clang where id='$id'");
 
+  // ----- EXTENSION POINT
+  rex_register_extension_point('CLANG_DELETED','',array ('id' => $id));
+  
   rex_generateAll();
 }
 
@@ -1000,6 +1003,9 @@ function rex_addCLang($id, $name)
   $add = new sql();
   $add->query("insert into rex_clang set id='$id',name='$name'");
 
+  // ----- EXTENSION POINT
+  rex_register_extension_point('CLANG_ADDED','',array ('id' => $id, 'name' => $name));
+  
   rex_generateAll();
 }
 
@@ -1025,6 +1031,9 @@ function rex_editCLang($id, $name)
   @ chmod($REX['INCLUDE_PATH']."/clang.inc.php", 0777);
   $edit = new sql;
   $edit->query("update rex_clang set name='$name' where id='$id'");
+  
+  // ----- EXTENSION POINT
+  rex_register_extension_point('CLANG_UPDATED','',array ('id' => $id, 'name' => $name));
 }
 
 /**
