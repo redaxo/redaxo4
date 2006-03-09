1a9930deeceffb3a8a780f9e52d5da5ab0416f3e
staabm <markus.staab@redaxo.de>
2006-03-09 17:28:40 +0000

    - addon id eingebaut

--- redaxo/include/addons/import_export/config.inc.php
+++ redaxo/include/addons/import_export/config.inc.php
@@ -4,8 +4,7 @@ $mypage = "import_export";        // only for this file
 
 if (!$REX['GG']) $I18N_IM_EXPORT = new i18n($REX['LANG'],$REX['INCLUDE_PATH']."/addons/$mypage/lang");   // CREATE LANG OBJ FOR THIS ADDON
 
-$REX['ADDON']['rxid'][$mypage] = "REX_001";     // unique id /
-// $REX['ADDON'][nsid][$mypage] = "REX002,REX003";  // necessary rxid; - not yet included
+$REX['ADDON']['rxid'][$mypage] = "1";     // unique id /
 $REX['ADDON']['page'][$mypage] = "$mypage";     // pagename/foldername
 $REX['ADDON']['name'][$mypage] = "Import/Export";   // name
 $REX['ADDON']['perm'][$mypage] = "import[]";    // permission
