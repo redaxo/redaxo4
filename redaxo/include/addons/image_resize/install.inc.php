5ad54a9557c8a8e9913bfd3250a6d08b5c02dd97
staabm <markus.staab@redaxo.de>
2006-04-05 16:57:59 +0000

    - Betriebsystemweiche eingebaut

--- redaxo/include/addons/image_resize/install.inc.php
+++ redaxo/include/addons/image_resize/install.inc.php
@@ -1,11 +1,20 @@
 <?php
-
 $error = '';
 
-if (!extension_loaded('gd')) {
-   if (!dl('gd.so')) {
-       $error = 'GD-LIB-extension not available! See <a href="http://www.php.net/gd">http://www.php.net/gd</a>'; 
-   }
+if (!extension_loaded('gd'))
+{
+  if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
+  {
+    $ext_loaded = @ dl('gd.dll');
+  }
+  else
+  {
+    $ext_loaded = @ dl('gd.so');
+  }
+  if (!$ext_loaded)
+  {
+    $error = 'GD-LIB-extension not available! See <a href="http://www.php.net/gd">http://www.php.net/gd</a>';
+  }
 }
 
 if ($error != '')
@@ -16,5 +25,4 @@ else
 {
   $REX['ADDON']['install']['image_resize'] = true;
 }
-
 ?>
