5d39dd785b887b5db800c93bdbbcd62088d807c8
staabm <markus.staab@redaxo.de>
2006-04-13 22:57:03 +0000

    - dev[] entfernt

--- redaxo/include/pages/content.inc.php
+++ redaxo/include/pages/content.inc.php
@@ -105,7 +105,6 @@ if ($article->getRows() == 1)
 
         // ----- RECHTE AM MODUL ?
         if ( !($REX_USER->isValueOf("rights","admin[]") || 
-            $REX_USER->isValueOf("rights","dev[]") || 
             $REX_USER->isValueOf("rights","module[$module_id]") || 
             $REX_USER->isValueOf("rights","module[0]"))
           )
