bcef8d2be064713a39183fa8f09e0b9a56774723
staabm <markus.staab@redaxo.de>
2006-08-04 17:42:11 +0000

    - formatiert

--- redaxo/include/layout/bottom.php
+++ redaxo/include/layout/bottom.php
@@ -10,10 +10,9 @@ if (!(isset( $open_header_only) && $open_header_only == true)):
 	<div id="rex-ftr">
 		<ul>
 			<li><a href="http://www.pergopa.de" target="_blank" class="black">pergopa kristinus gbr</a> | </li>
-            <li><a href="http://www.redaxo.de" target="_blank" class="black">redaxo.de</a> | </li>
-            <li><a href="http://forum.redaxo.de">?</a></li>
+      <li><a href="http://www.redaxo.de" target="_blank" class="black">redaxo.de</a> | </li>
+      <li><a href="http://forum.redaxo.de">?</a></li>
 		</ul>
-		
 		<p><?php echo showScripttime() ?> sec | <?php echo strftime($I18N->msg("dateformat"))?></p>
 	</div>
 <?php
