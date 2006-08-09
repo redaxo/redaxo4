19698a16fa272934c5f083dbfec02478ec402e25
staabm <markus.staab@redaxo.de>
2006-08-09 12:13:34 +0000

    *** empty log message ***

--- redaxo/include/addons/tinymce/pages/index.inc.php
+++ redaxo/include/addons/tinymce/pages/index.inc.php
@@ -23,7 +23,7 @@ include_once $REX['INCLUDE_PATH'].'/addons/tinymce/classes/class.tiny.inc.php';
 $TINY2[0]=new tiny2editor();
 $TINY2[0]->content="REX_VALUE[1]";
 $TINY2[0]->show();
-<?php echo "?>";?>
+?&gt;
 </pre>
 
 <h2>Moduleingabe Erweitert</h2>
@@ -38,19 +38,17 @@ $TINY2[0]->disable="link,image,advimage,justifyleft,justifycenter,justifyright,j
 $TINY2[0]->plugins="emotions,iespell,table,insertdatetime,preview,zoom,flash,searchreplace,print,contextmenu,paste,directionality,fullscreen";
 $TINY2[0]->validhtml="marquee[border|class|style|width]b[border|class|style]img[class|style|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]";
 $TINY2[0]->show();
-<?php echo "?>";?>
+?&gt;
 </pre>
 
 <h2>Modulausgabe (Alle)</h2>
 <pre>
 &lt;div class="section"&gt;
 &lt;?php
-$data =&lt;&lt;&lt;EOD
+$content =&lt;&lt;&lt;EOD
 REX_HTML_VALUE[1]
 EOD;
-<?php echo "?>";?>
-&lt;?php
-$content=$data;
+
 if ($REX['REDAXO']) {
 	$content=str_replace('src="files/','src="../files/',$content);
 	echo '&lt;link rel="stylesheet" type="text/css" href="../files/tinymce/content.css" /&gt;';
