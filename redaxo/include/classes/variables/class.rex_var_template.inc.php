b26b8372fbaf66181cb5c97add04c11a0b0979ed
staabm <markus.staab@redaxo.de>
2006-10-17 12:25:32 +0000

    rexVariable API erweitert: - REX_TEMPLATE[xx]

---
+++ redaxo/include/classes/variables/class.rex_var_template.inc.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * REX_TEMPLATE[2]
+ * 
+ * @package redaxo3
+ * @version $Id$
+ */
+
+class rex_var_template extends rex_var
+{
+  // --------------------------------- Output
+  
+  function getBEOutput(& $sql, $content)
+  {
+    return $this->matchTemplate(&$sql, $content);
+  }
+
+  function getInputParams($content, $varname)
+  {
+    $matches = array ();
+    $id = '';
+    $clang = '';
+
+    $match = $this->matchVar($content, $varname);
+    foreach ($match as $param_str)
+    {
+      $params = $this->splitString($param_str);
+
+      foreach ($params as $name => $value)
+      {
+        switch ($name)
+        {
+          case '0' :
+          case 'id' :
+            $id = (int) $value;
+            break;
+        }
+      }
+
+      $matches[] = array (
+        $param_str,
+        $id,
+      );
+    }
+
+    return $matches;
+  }
+
+  /**
+   * Wert für die Ausgabe
+   */
+  function matchTemplate(& $sql, $content)
+  {
+    $var = 'REX_TEMPLATE';
+    $matches = $this->getInputParams($content, $var);
+
+    foreach ($matches as $match)
+    {
+      list ($param_str, $template_id) = $match;
+      
+      $template = new rex_template($template_id);
+      $content = str_replace($var . '[' . $param_str . ']', $template->getTemplate(), $content);
+    }
+
+    return $content;
+  }
+}
+?>
