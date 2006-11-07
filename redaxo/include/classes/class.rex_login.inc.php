a2ebdac8893e882e9f1bb648abf27c7e4cbb5395
staabm <markus.staab@redaxo.de>
2006-11-07 11:10:09 +0000

    - kommentiert

--- redaxo/include/classes/class.rex_login.inc.php
+++ redaxo/include/classes/class.rex_login.inc.php
@@ -1,42 +1,38 @@
 <?php
+
+
 /** 
- *  
+ * Klasse zum handling des Login/Logout-Mechanismuses
+ * 
  * @package redaxo3
  * @version $Id$
- */ 
+ */
 
-// class login 1.0
-// 
-// erstellt 01.12.2003
-// pergopa kristinus gbr
-// lange strasse 31
-// 60311 Frankfurt/M.
-// www.pergopa.de
-// ersteller: j.kristinus
-
-class rex_login_sql extends rex_sql{
-
+class rex_login_sql extends rex_sql
+{
   function isValueOf($feld, $prop)
   {
     if ($prop == "")
     {
-      return TRUE;
+      return true;
     }
-    else 
+    else
     {
-      if ($feld == "rights") return strpos($this->getValue( $feld), "#".$prop."#") !== false;
-      else return strpos($this->getValue( $feld), $prop) !== false;
+      if ($feld == "rights")
+        return strpos($this->getValue($feld), "#" . $prop . "#") !== false;
+      else
+        return strpos($this->getValue($feld), $prop) !== false;
     }
   }
-  
+
   function hasPerm($perm)
   {
-    return $this->isValueOf("rights",$perm);
+    return $this->isValueOf("rights", $perm);
   }
-  
 }
 
-class rex_login{
+class rex_login
+{
 
   var $DB;
   var $error_page;
@@ -54,7 +50,7 @@ class rex_login{
   var $passwordfunction;
   var $cache;
   var $login_status;
-  
+
   function rex_login()
   {
     $this->DB = 1;
@@ -63,203 +59,276 @@ class rex_login{
     $this->system_id = "default";
     $this->cryptFunction = false;
     $this->setLanguage();
-	$this->cache = false;
-	$this->login_status = 0; // 0 = nochchecken, 1 = ok, -1 = notok
-    
+    $this->cache = false;
+    $this->login_status = 0; // 0 = nochchecken, 1 = ok, -1 = notok
+
   }
 
-  
+  /**
+   * Setzt die Sprache, die für die versch. Meldungen verwendet wird
+   */
   function setLanguage($lang = "en")
   {
-  	global $REX;
+    global $REX;
     if ($lang == "de")
     {
       $this->text[10] = "Session beendet.";
       $this->text[20] = "ID nicht gefunden.";
-      $this->text[30] = "Fehler beim Login. Bitte vor dem nächsten Versuch ".$REX['RELOGINDELAY']." Sekunden warten.";
+      $this->text[30] = "Fehler beim Login. Bitte vor dem nächsten Versuch " . $REX['RELOGINDELAY'] . " Sekunden warten.";
       $this->text[40] = "Bitte einloggen.";
       $this->text[50] = "Ausgeloggt.";
-    }else
+    }
+    else
     {
       $this->text[10] = "your session is expired!";
       $this->text[20] = "uid not found!";
-      $this->text[30] = "login wrong. please wait ".$REX['RELOGINDELAY']." seconds before you try again.";
+      $this->text[30] = "login wrong. please wait " . $REX['RELOGINDELAY'] . " seconds before you try again.";
       $this->text[40] = "login please.";
       $this->text[50] = "You logged out.";
-    }   
+    }
   }
 
-  
+  /**
+   * Setzt, ob die Ergebnisse der Login-Abfrage 
+   * pro Seitenaufruf gecached werden sollen
+   */
   function setCache($status = true)
   {
     $this->cache = $status;
   }
- 
+
+  /**
+   * Setzt die Id der zu verwendenden SQL Connection
+   */
   function setSqlDb($DB)
   {
     $this->DB = $DB;
   }
-  
+
+  /**
+   * Setzt eine eindeutige System Id, damit mehrere 
+   * Sessions auf der gleichen Domain unterschieden werden können
+   */
   function setSysID($system_id)
   {
     $this->system_id = $system_id;
   }
-  
+
+  /**
+   * Setzt das Session Timeout
+   */
   function setSessiontime($session_duration)
   {
-    $this->session_duration = $session_duration;  
+    $this->session_duration = $session_duration;
   }
-  
-  function setLogin($usr_login,$usr_psw)
+
+  /**
+   * Setzt den Login und das Password
+   */
+  function setLogin($usr_login, $usr_psw)
   {
     $this->usr_login = $usr_login;
     $this->usr_psw = $this->encryptPassword($usr_psw);
   }
-  
+
+  /**
+   * Markiert die aktuelle Session als ausgeloggt
+   */
   function setLogout($logout)
   {
-    $this->logout = $logout;  
+    $this->logout = $logout;
   }
-  
+
+  /**
+   * Setzt den UserQuery
+   * 
+   * Dieser wird benutzt, um einen bereits eingeloggten User 
+   * im Verlauf seines Aufenthaltes auf der Webseite zu verifizieren
+   */
   function setUserquery($login_query)
   {
     $this->user_query = $login_query;
-  } 
-  
+  }
+
+  /**
+   * Setzt den LoginQuery
+   * 
+   * Dieser wird benutzt, um den eigentlichne Loginvorgang durchzuführen.
+   * Hier wird das eingegebene Password und der Login eingesetzt.
+   */
   function setLoginquery($user_query)
   {
     $this->login_query = $user_query;
   }
-  
+
+  /**
+   * Setzt den Namen der Spalte, der die User-Id enthält
+   */
   function setUserID($uid)
   {
     $this->uid = $uid;
   }
-  
+
+  /**
+   * Setzt einen Meldungstext
+   */
   function setMessage($message)
   {
-    $this->message = $message;  
+    $this->message = $message;
   }
-  
+
+  /**
+   * Prüft die mit setLogin() und setPassword() gesetzten Werte 
+   * anhand des LoginQueries/UserQueries und gibt den Status zurück
+   * 
+   * Gibt true zurück bei erfolg, sonst false
+   */
   function checkLogin()
   {
     global $REX;
     // wenn logout dann header schreiben und auf error seite verweisen
     // message schreiben
-    
+
     $ok = false;
-    
+
     if (!$this->logout)
     {
-      
+
       // checkLogin schonmal ausgeführt ? gecachte ausgabe erlaubt ?
-      if ($this->cache && $this->login_status>0) return true;
-      elseif ($this->cache && $this->login_status<0) return false;
+      if ($this->cache && $this->login_status > 0)
+        return true;
+      elseif ($this->cache && $this->login_status < 0) return false;
 
       if ($this->usr_login != "")
       {
         // wenn login daten eingegeben dann checken
         // auf error seite verweisen und message schreiben
-        
+
         $this->USER = new rex_login_sql($this->DB);
         $USR_LOGIN = $this->usr_login;
         $USR_PSW = $this->usr_psw;
 
-        $query = str_replace("USR_LOGIN",$this->usr_login,$this->login_query);
-        $query = str_replace("USR_PSW",$this->usr_psw,$query);
-        
+        $query = str_replace("USR_LOGIN", $this->usr_login, $this->login_query);
+        $query = str_replace("USR_PSW", $this->usr_psw, $query);
+
         $this->USER->setQuery($query);
         if ($this->USER->getRows() == 1)
         {
           $ok = true;
-		  $this->setSessionVar('UID',$this->USER->getValue($this->uid));
-        }else
+          $this->setSessionVar('UID', $this->USER->getValue($this->uid));
+        }
+        else
         {
           $this->message = $this->text[30];
-          $this->setSessionVar('UID','');
+          $this->setSessionVar('UID', '');
         }
-                
-      } elseif ($this->getSessionVar('UID') != '')
+
+      }
+      elseif ($this->getSessionVar('UID') != '')
       {
         // wenn kein login und kein logout dann nach sessiontime checken
         // message schreiben und falls falsch auf error verweisen
-                
+
         $this->USER = new rex_login_sql($this->DB);
-        $query = str_replace("USR_UID",$this->getSessionVar('UID'),$this->user_query);
-        
+        $query = str_replace("USR_UID", $this->getSessionVar('UID'), $this->user_query);
+
         $this->USER->setQuery($query);
         if ($this->USER->getRows() == 1)
         {
-          if ( ($this->getSessionVar('STAMP')+$this->session_duration) > time() )
+          if (($this->getSessionVar('STAMP') + $this->session_duration) > time())
           {
             $ok = true;
-			$this->setSessionVar('UID',$this->USER->getValue($this->uid));
-          }else
+            $this->setSessionVar('UID', $this->USER->getValue($this->uid));
+          }
+          else
           {
-            $this->message = $this->text[10]; 
-          }         
-        }else
+            $this->message = $this->text[10];
+          }
+        }
+        else
         {
           $this->message = $this->text[20];
         }
-      }else
+      }
+      else
       {
         $this->message = $this->text[40];
         $ok = false;
       }
-    }else
+    }
+    else
     {
       $this->message = $this->text[50];
-      $this->setSessionVar('UID','');
+      $this->setSessionVar('UID', '');
       // session_unregister("REX_SESSION");
-      
+
     }
 
     if ($ok)
     {
       // wenn alles ok dann REX[UID][system_id) schreiben
-      $this->setSessionVar('STAMP',time());
+      $this->setSessionVar('STAMP', time());
 
-    }else
+    }
+    else
     {
       // wenn nicht, dann UID loeschen und error seite
-      $this->setSessionVar('STAMP','');
-      $this->setSessionVar('UID','');
+      $this->setSessionVar('STAMP', '');
+      $this->setSessionVar('UID', '');
     }
-    
-    if ($ok) $this->login_status = 1;
-    else $this->login_status = -1;
 
-    return $ok;       
+    if ($ok)
+      $this->login_status = 1;
+    else
+      $this->login_status = -1;
+
+    return $ok;
   }
-  
+
+  /**
+   * Gibt einen Benutzer-Spezifischen Wert zurück 
+   */
   function getValue($value)
   {
     return $this->USER->getValue($value);
   }
 
+  /**
+   * Setzt eine Password-Funktion
+   */
   function setPasswordFunction($pswfunc)
   {
-  	$this->passwordfunction = $pswfunc;
+    $this->passwordfunction = $pswfunc;
   }
 
+  /**
+   * Verschlüsselt den übergebnen String, falls eine Password-Funktion gesetzt ist. 
+   */
   function encryptPassword($psw)
   {
-  	if ($this->passwordfunction == "") return $psw;
-  	return call_user_func($this->passwordfunction,$psw);
+    if ($this->passwordfunction == "")
+      return $psw;
+
+    return call_user_func($this->passwordfunction, $psw);
   }
-  
+
+  /**
+   * Setzte eine Session-Variable
+   */
   function setSessionVar($varname, $value)
   {
-  	$_SESSION[$this->system_id][$varname] = $value;
+    $_SESSION[$this->system_id][$varname] = $value;
   }
 
+  /**
+   * Gibt den Wert einer Session-Variable zurück
+   */
   function getSessionVar($varname, $default = '')
   {
-  	if(isset($_SESSION[$this->system_id][$varname])) return $_SESSION[$this->system_id][$varname];
-	return $default;
-  }
-  
-}
+    if (isset ($_SESSION[$this->system_id][$varname]))
+      return $_SESSION[$this->system_id][$varname];
 
+    return $default;
+  }
+}
 ?>
