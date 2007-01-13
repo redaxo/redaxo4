8472375849ac644e6220dd560b35e10d8a105f74
staabm <markus.staab@redaxo.de>
2007-01-13 17:48:52 +0000

    bugfix beim rowcount

--- redaxo/include/classes/class.rex_list.inc.php
+++ redaxo/include/classes/class.rex_list.inc.php
@@ -28,7 +28,7 @@ class rex_list
 {
 	var $query;
 	var $sql;
-	var $debugsql;
+	var $debug;
 	
 	var $name;
 	var $caption;
@@ -62,8 +62,8 @@ class rex_list
 		// --------- List Attributes
 		$this->query = $query;
 		$this->sql =& new rex_sql();
-		$this->sql->debugsql =& $this->debugsql;
-		$this->debugsql = $debug;
+		$this->debug = $debug;
+		$this->sql->debugsql =& $this->debug;
 		$this->name = $listName;
 		$this->caption = '';
 		$this->params = array();
@@ -408,9 +408,9 @@ class rex_list
 		if(!$this->rows)
 		{
 			$sql = rex_sql::getInstance();
-			$sql->debugsql = $this->debugsql;
-			$sql->setQuery(preg_replace('/SELECT(.*)FROM/iU', 'SELECT count(*) as rowCount FROM	', $this->query, 1));
-			$this->rows = $sql->getValue('rowCount'); 
+			$sql->debugsql = $this->debug;
+			$sql->setQuery($this->query);
+			$this->rows = $sql->getRows();
 		}
 		
 		return $this->rows;
