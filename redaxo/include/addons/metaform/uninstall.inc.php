8db3c800bd12a4b4c546c919c4d195836fbe5fe2
staabm <markus.staab@redaxo.de>
2007-02-23 13:20:04 +0000

    addon umbenannt

--- redaxo/include/addons/metaform/uninstall.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$REX['ADDON']['install']['metaform'] = 0;
-// ERRMSG IN CASE: $REX['ADDON']['installmsg']['metaform'] = "Deinstallation fehlgeschlagen weil...";
-
-?>
--- redaxo/include/addons/metaform/uninstall.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS `rex_62_field`;
-DROP TABLE IF EXISTS `rex_62_section`;
-DROP TABLE IF EXISTS `rex_62_type`;
-DROP TABLE IF EXISTS `rex_62_value`;
