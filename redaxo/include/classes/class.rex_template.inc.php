cda434cf85f961ab5f4f8288c314fe46430a6a7f
staabm <markus.staab@redaxo.de>
2007-03-25 17:55:59 +0000

    im constructor jetzt auch mit setId() neu formatiert

--- redaxo/include/classes/class.rex_template.inc.php
+++ redaxo/include/classes/class.rex_template.inc.php
@@ -7,36 +7,36 @@
  * @package redaxo3
  * @version $Id$
  */
- 
+
 class rex_template
 {
-   var $id;
-   
-   function rex_template($template_id)
-   {
-      $this->id = $template_id;
-   }
-   
-   function getId()
-   {
-      return $this->id;
-   }
-   
-   function setId($id)
-   {
-      $this->id = (int) $id;
-   }
-   
-   function getTemplate()
-   {
-      global $REX;
-      
-      $file = $REX['INCLUDE_PATH']. '/generated/templates/'. $this->getId() .'.template';
-      $handle = fopen($file, 'r');
-      $content = fread($handle, filesize($file));
-      fclose($handle);
-      
-      return $content;
-   }
+  var $id;
+
+  function rex_template($template_id)
+  {
+    $this->setId($template_id);
+  }
+
+  function getId()
+  {
+    return $this->id;
+  }
+
+  function setId($id)
+  {
+    $this->id = (int) $id;
+  }
+
+  function getTemplate()
+  {
+    global $REX;
+
+    $file = $REX['INCLUDE_PATH'] . '/generated/templates/' . $this->getId() . '.template';
+    $handle = fopen($file, 'r');
+    $content = fread($handle, filesize($file));
+    fclose($handle);
+
+    return $content;
+  }
 }
 ?>
