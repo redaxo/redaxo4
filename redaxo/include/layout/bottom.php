3ecc4da77114bb0e81788ed75e075cee6951c1f9
staabm <markus.staab@redaxo.de>
2007-04-03 19:30:15 +0000

    tabindex eingebaut

--- redaxo/include/layout/bottom.php
+++ redaxo/include/layout/bottom.php
@@ -16,9 +16,9 @@ if (!(isset( $open_header_only) && $open_header_only == true)):
 	
 	<div id="rex-ftr">
 		<ul>
-			<li><a href="http://www.pergopa.de" target="_blank" class="black">pergopa kristinus gbr</a> | </li>
-      <li><a href="http://www.redaxo.de" target="_blank" class="black">redaxo.de</a> | </li>
-      <li><a href="http://forum.redaxo.de">?</a></li>
+			<li><a href="http://www.pergopa.de" target="_blank" class="black"<?php echo rex_tabindex() ?>>pergopa kristinus gbr</a> | </li>
+      <li><a href="http://www.redaxo.de" target="_blank" class="black"<?php echo rex_tabindex() ?>>redaxo.de</a> | </li>
+      <li><a href="http://forum.redaxo.de"<?php echo rex_tabindex() ?>>?</a></li>
 		</ul>
 		<p><?php echo showScripttime() ?> sec | <?php echo rex_formatter :: format(time(), 'strftime', 'date'); ?></p>
 	</div>
