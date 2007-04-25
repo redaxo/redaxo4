fcb00306a9d7d23927c4e4cbf838b854a64e6bf9
staabm <markus.staab@redaxo.de>
2007-04-25 16:45:13 +0000

    variablen im template werden wieder ersetzt

--- redaxo/include/classes/class.rex_article.inc.php
+++ redaxo/include/classes/class.rex_article.inc.php
@@ -500,7 +500,8 @@ class rex_article
     if ($this->getTemplateId() == 0 and $this->article_id != 0)
     {
       echo $this->getArticle();
-    }elseif ($this->getTemplateId() != 0 and $this->article_id != 0)
+    }
+    elseif ($this->getTemplateId() != 0 and $this->article_id != 0)
     {
       $template_name = $REX['INCLUDE_PATH']."/generated/templates/".$this->getTemplateId().".template";
       if ($fd = fopen ($template_name, "r"))
@@ -508,12 +509,15 @@ class rex_article
       	$fs = filesize ($template_name);
         if ($fs>0) $template_content = fread ($fd, $fs);
         fclose ($fd);
-      }else
+      }
+      else
       {
         $template_content = $this->getTemplateId()." not found";
       }
+      $template_content = $this->replaceCommonVars($template_content);
       eval("?>".$template_content);
-    }else
+    }
+    else
     {
       echo "no template";
     }
