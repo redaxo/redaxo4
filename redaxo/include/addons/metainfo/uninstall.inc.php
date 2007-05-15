d1707f552c7077bb29dc7a65d55eaf57a183784a
staabm <markus.staab@redaxo.de>
2007-05-15 16:31:05 +0000

    cleanup der rex_article tabelle bei uninstall

--- redaxo/include/addons/metainfo/uninstall.inc.php
+++ redaxo/include/addons/metainfo/uninstall.inc.php
@@ -3,4 +3,15 @@
 $REX['ADDON']['install']['metainfo'] = 0;
 // ERRMSG IN CASE: $REX['ADDON']['installmsg']['metainfo'] = "Deinstallation fehlgeschlagen weil...";
 
+$sql = new rex_sql();
+$sql->setQuery('SELECT name FROM '.$REX['TABLE_PREFIX']. '62_params');
+
+$del = new rex_sql();
+
+for($i = 0; $i < $sql->getRows(); $i++)
+{
+	$del->setQuery('ALTER TABLE '.$REX['TABLE_PREFIX']. 'article DROP '. $sql->getValue('name'));	
+	$sql->next();
+}
+
 ?>
