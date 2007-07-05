dbb233dbfe9d6de6c8cda976b6531d9faf01d64f
staabm <markus.staab@redaxo.de>
2007-07-05 11:47:16 +0000

    cleanup

--- redaxo/include/addons/import_export/config.inc.php
+++ redaxo/include/addons/import_export/config.inc.php
@@ -2,7 +2,8 @@
 
 $mypage = 'import_export';        // only for this file
 
-if ($REX['REDAXO']) $I18N_IM_EXPORT = new i18n($REX['LANG'],$REX['INCLUDE_PATH'].'/addons/'. $mypage .'/lang');   // CREATE LANG OBJ FOR THIS ADDON
+if ($REX['REDAXO'])
+  $I18N_IM_EXPORT = new i18n($REX['LANG'],$REX['INCLUDE_PATH'].'/addons/'. $mypage .'/lang');   // CREATE LANG OBJ FOR THIS ADDON
 
 $REX['ADDON']['rxid'][$mypage] = '1';     // unique id /
 $REX['ADDON']['page'][$mypage] = $mypage;     // pagename/foldername
@@ -10,9 +11,5 @@ $REX['ADDON']['name'][$mypage] = 'Import/Export';   // name
 $REX['ADDON']['perm'][$mypage] = 'import[]';    // permission
 
 $REX['PERM'][] = 'import[]';
-
-// IF NECESSARY INCLUDE FUNC/CLASSES ETC
-// INCLUDE IN FRONTEND --- if ($REX[GG]) 
-// INCLUDE IN BACKEND --- if (!$REX[GG]) 
 
 ?>
