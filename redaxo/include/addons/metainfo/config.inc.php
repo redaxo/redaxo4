ae9acef0c02113335c2153ea1c096fd5c8a6b586
staabm <markus.staab@redaxo.de>
2007-07-05 19:48:05 +0000

    setup bug gefixed

--- redaxo/include/addons/metainfo/config.inc.php
+++ redaxo/include/addons/metainfo/config.inc.php
@@ -27,27 +27,27 @@ if ($REX['REDAXO'])
   // Include Extensions
   if (isset ($page))
   {
-    include ($REX['INCLUDE_PATH'] . '/addons/' . $mypage . '/extensions/extension_common.inc.php');
+    require_once ($REX['INCLUDE_PATH'] . '/addons/' . $mypage . '/extensions/extension_common.inc.php');
 
     if ($page == 'content' && isset ($mode) && $mode == 'meta')
     {
-      include ($REX['INCLUDE_PATH'] . '/addons/' . $mypage . '/extensions/extension_art_metainfo.inc.php');
+      require_once ($REX['INCLUDE_PATH'] . '/addons/' . $mypage . '/extensions/extension_art_metainfo.inc.php');
     }
     elseif ($page == 'structure')
     {
-      include ($REX['INCLUDE_PATH'] . '/addons/' . $mypage . '/extensions/extension_cat_metainfo.inc.php');
+      require_once ($REX['INCLUDE_PATH'] . '/addons/' . $mypage . '/extensions/extension_cat_metainfo.inc.php');
     }
     elseif ($page == 'medienpool' && isset ($subpage) && $subpage == 'detail')
     {
-      include ($REX['INCLUDE_PATH'] . '/addons/' . $mypage . '/extensions/extension_med_metainfo.inc.php');
+      require_once ($REX['INCLUDE_PATH'] . '/addons/' . $mypage . '/extensions/extension_med_metainfo.inc.php');
     }
     elseif ($page == 'import_export')
     {
-      include ($REX['INCLUDE_PATH'] . '/addons/' . $mypage . '/extensions/extension_cleanup.inc.php');
+      require_once ($REX['INCLUDE_PATH'] . '/addons/' . $mypage . '/extensions/extension_cleanup.inc.php');
     }
   }
 }
 
-include ($REX['INCLUDE_PATH'] . '/addons/' . $mypage . '/extensions/extension_oof_metainfo.inc.php');
+require_once ($REX['INCLUDE_PATH'] . '/addons/' . $mypage . '/extensions/extension_oof_metainfo.inc.php');
 
 ?>
