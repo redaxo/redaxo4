cb7287102792cf9bd5482151666a0b633e32f2c0
staabm <markus.staab@redaxo.de>
2007-08-01 16:10:29 +0000

    Bugfix: http://forum.redaxo.de/ftopic6303.html

--- redaxo/include/addons/tinymce/help.inc.php
+++ redaxo/include/addons/tinymce/help.inc.php
@@ -1,16 +1,20 @@
 <?php
 
 /**
- * Textile Addon
- *  
+ * TinyMCE Addon
+ *
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
+ *
+ * @author Dave Holloway
+ * @author <a href="http://www.GN2-Netwerk.de">www.GN2-Netwerk.de</a>s
+ *
  * @package redaxo3
  * @version $Id$
  */
 
 ?>
-<b>Textile Addon</b>
+<b>TinyMCE Addon</b>
 
 <br /><br />
-Bringt die Möglichkeit in Modulen Textile Markup zu verwenden
+Erweitert REDAXO um den WYSIWYG-Editor, TinyMCE
