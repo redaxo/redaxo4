ba4600e9eaacc7a9a26a837c4390f64999f0ed2f
staabm <markus.staab@redaxo.de>
2007-08-03 15:17:54 +0000

    installationsprozess verschoenert

--- redaxo/include/addons/tinymce/functions/function_pclzip.inc.php
+++ redaxo/include/addons/tinymce/functions/function_pclzip.inc.php
@@ -2,35 +2,38 @@
 
 /**
  * TinyMCE Addon
- *  
+ *
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
- * 
+ *
  * @author Dave Holloway
  * @author <a href="http://www.GN2-Netwerk.de">www.GN2-Netwerk.de</a>s
- * 
+ *
  * @package redaxo3
  * @version $Id$
  */
 
 include_once $REX['INCLUDE_PATH'] . '/addons/tinymce/classes/class.pclzip.inc.php';
 
-function rex_a52_extract_archive($file)
+function rex_a52_extract_archive($file, $msg = '' )
 {
 	$archive = new PclZip($file);
 	if ($archive->extract(PCLZIP_OPT_PATH, '../files') == 0)
 	{
 	  die("Error : " . $archive->errorInfo(true));
 	}
-	
+
 	if (($list = $archive->listContent()) == 0)
 	{
 	  die("Error : " . $archive->errorInfo(true));
 	}
-	
-	echo '<div style="height:200px;width:770px;overflow:auto;margin-bottom:10px;text-align:center;">';
-	echo '<h3>Archiv wird extrahiert...</h3>';
-	echo '<table border="1" style="margin:0 auto 0 auto;">';
+
+	echo '<div style="height:150px;width:770px;overflow:auto;margin-bottom:10px;text-align:center;">';
+
+  if($msg != '')
+	  echo '<h3>'. $msg .'</h3>';
+
+	echo '<table border="1" style="margin:0 auto 0 auto; width: 600px">';
 	echo '<tr><th>Datei</th><th>Gr&ouml;&szlig;e</th>';
 	for ($i = 0; $i < count($list); $i++)
 	{
--- redaxo/include/addons/tinymce/install.inc.php
+++ redaxo/include/addons/tinymce/install.inc.php
@@ -2,23 +2,25 @@
 
 /**
  * TinyMCE Addon
- *  
+ *
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
- * 
+ *
  * @author Dave Holloway
  * @author <a href="http://www.GN2-Netwerk.de">www.GN2-Netwerk.de</a>s
- * 
+ *
  * @package redaxo3
  * @version $Id$
  */
 
 require_once $REX['INCLUDE_PATH'] . '/addons/tinymce/functions/function_pclzip.inc.php';
 
+$I18N_A52 = new i18n($REX['LANG'], $REX['INCLUDE_PATH'].'/addons/tinymce/lang/');
+
 // Install Tiny Core
-rex_a52_extract_archive('include/addons/tinymce/js/tinymce.zip');
+rex_a52_extract_archive('include/addons/tinymce/js/tinymce.zip', $I18N_A52->msg('install_core'));
 // Install German Language Pack
-rex_a52_extract_archive('include/addons/tinymce/js/tinymce_lang_de.zip');
+rex_a52_extract_archive('include/addons/tinymce/js/tinymce_lang_de.zip', $I18N_A52->msg('install_lang_pakage'));
 
 $REX['ADDON']['install']['tinymce'] = true;
 
