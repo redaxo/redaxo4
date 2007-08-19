76fd00247b80334743655510287e101277cd1c4a
staabm <markus.staab@redaxo.de>
2007-08-19 16:21:27 +0000

    root artikel konnten nicht bearbeitet werden

--- redaxo/include/functions/function_rex_category.inc.php
+++ redaxo/include/functions/function_rex_category.inc.php
@@ -1,12 +1,12 @@
 <?php
 
-/** 
+/**
  * Regelt die Rechte an den einzelnen Kategorien und gibt den Pfad aus
- * Kategorien = Startartikel und Bezüge  
- * @package redaxo3 
- * @version $Id$ 
+ * Kategorien = Startartikel und Bezüge
+ * @package redaxo3
+ * @version $Id$
  */
-  
+
 $KATebene = 0; // aktuelle Ebene: default
 $KATPATH = '|'; // Standard für path Eintragungen in DB
 if (!isset($KATout)) $KATout = ''; // Variable definiert und vorbelegt wenn nicht existent
@@ -15,26 +15,30 @@ $KATPERM = false;
 if ($REX_USER->hasPerm('csw[0]') || $REX_USER->hasPerm('admin[]')) $KATPERM = true;
 
 $KAT = new rex_sql;
+$KAT->debugsql = true;
 $KAT->setQuery("SELECT * FROM ".$REX['TABLE_PREFIX']."article WHERE id=$category_id AND startpage=1 AND clang=$clang");
 
 if ($KAT->getRows()!=1)
 {
 	// kategorie existiert nicht
-	$category_id = 0;
-	$article_id = 0;
+  if($category_id != 0)
+  {
+  	$category_id = 0;
+  	$article_id = 0;
+  }
 }
 else
 {
 	// kategorie existiert
-	
+
 	$KPATH = explode('|',$KAT->getValue('path'));
-		
+
 	$KATebene = count($KPATH)-1;
 	for ($ii=1;$ii<$KATebene;$ii++)
 	{
 		$SKAT = new rex_sql;
 		$SKAT->setQuery('SELECT * FROM '. $REX['TABLE_PREFIX'] .'article WHERE id='. $KPATH[$ii] .' AND startpage=1 AND clang='. $clang);
-      
+
       $catname = str_replace(' ', '&nbsp;', $SKAT->getValue('catname'));
       $catid = $SKAT->getValue('id');
 
@@ -45,7 +49,7 @@ else
 			{
 				$KATout .= '<li>: <a href="index.php?page=structure&amp;category_id='. $catid .'&amp;clang='. $clang .'"'. rex_tabindex() .'>' .$catname. '</a> </li>';
 				$KATPATH .= $KPATH[$ii]."|";
-            
+
 	      if($REX_USER->hasPerm('csw['.$catid.']'))
 	      {
 	         $KATPERM = true;
@@ -53,22 +57,22 @@ else
 			}
 		}
 	}
-	
+
 	if ($KATPERM || $REX_USER->hasPerm('csw['. $category_id .']') || $REX_USER->hasPerm('csr['. $category_id .']'))
 	{
       $catname = str_replace(' ', '&nbsp;', $KAT->getValue('catname'));
-      
+
 		$KATout .= '<li>: <a href="index.php?page=structure&amp;category_id='. $category_id .'&amp;clang='. $clang .'"'. rex_tabindex() .'>'.$catname.'</a> </li>';
 		$KATPATH .= $category_id .'|';
-      
-		if ($REX_USER->hasPerm('csw['. $category_id .']')) 
+
+		if ($REX_USER->hasPerm('csw['. $category_id .']'))
     {
      $KATPERM = true;
     }
 	}
    else
 	{
-		$category_id = 0;	
+		$category_id = 0;
 		$article_id = 0;
 	}
 }
