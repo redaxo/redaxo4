4963fbc5f65bde0994748bc324653a3bef43b1aa
staabm <markus.staab@redaxo.de>
2007-09-04 20:23:52 +0000

    Wenn 2 Add Aktionen auf einer Seite sind

--- redaxo/include/master.inc.php
+++ redaxo/include/master.inc.php
@@ -93,8 +93,9 @@ $REX['DB']['2']['PERSISTENT'] = false;
 $REX['ACKEY']['SAVE'] = 's';
 $REX['ACKEY']['APPLY'] = 'x';
 $REX['ACKEY']['DELETE'] = 'd';
-$REX['ACKEY']['ART_CREATE'] = 'a';
-$REX['ACKEY']['CAT_CREATE'] = 'c';
+$REX['ACKEY']['ADD'] = 'a';
+// Wenn 2 Add Aktionen auf einer Seite sind (z.b. Struktur)
+$REX['ACKEY']['ADD_2'] = 'y';
 $REX['ACKEY']['LOGOUT'] = 'l';
 
 // ----------------- default values
