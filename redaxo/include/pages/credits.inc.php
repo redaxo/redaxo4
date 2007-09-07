ed7b58b840a5ab63a7a39f92e45ea3ee789ca6ea
staabm <markus.staab@redaxo.de>
2007-09-07 16:31:54 +0000

    typos gefixed

--- redaxo/include/pages/credits.inc.php
+++ redaxo/include/pages/credits.inc.php
@@ -21,16 +21,17 @@ include_once $REX['INCLUDE_PATH']."/functions/function_rex_addons.inc.php";
 <b>REDAXO:</b>
 
 <b>Jan Kristinus</b>, jan.kristinus@redaxo.de
-Erfinder und Coreentwickler
-Yakamara Media GmbH & Co KG, <a href="http://www.yakamara.de">www.yakamara.de</a>
+Erfinder und Kernentwickler
+Yakamara Media GmbH &amp; Co KG, <a href="http://www.yakamara.de">www.yakamara.de</a>
 
 <b>Markus Staab</b>, markus.staab@redaxo.de
-Coreentwickler
-???
+Kernentwickler
+Public-4u e.K <a href="http://www.public-4u.de">http://www.public-4u.de</a>
 
 <b>Thomas Blum</b>, thomas.blum@redaxo.de
-CSS Entwickler
+Layout/Design Entwickler
 blumbeet - web.studio, <a href="http://www.blumbeet.com">www.blumbeet.com</a>
+
 ???
 
 
@@ -43,15 +44,17 @@ $ADDONS = rex_read_addons_folder();
 
 foreach ($ADDONS as $cur)
 {
-  	if (isset($REX['ADDON']['page'][$cur])) $cl = 'rex-clr-grn';
-		else $cl = 'rex-clr-red';
-  	echo '<tr><td class="'.$cl.'">'.$cur.'</td><td class="'.$cl.'">';
-  	if (isset($REX['ADDON']['version'][$cur])) echo '['.$REX['ADDON']['version'][$cur].']';
-  	echo '</td><td class="'.$cl.'">';
-  	if (isset($REX['ADDON']['author'][$cur])) echo $REX['ADDON']['author'][$cur];
-  	if (!isset($REX['ADDON']['page'][$cur])) echo 'AddOn inaktiv';
-  	echo '</td><td class="'.$cl.'">';
-  	if (isset($REX['ADDON']['supportpage'][$cur])) echo '<a href="'.$REX['ADDON']['supportpage'][$cur].'">'.$REX['ADDON']['supportpage'][$cur].'</a>';
+	if (isset($REX['ADDON']['page'][$cur])) $cl = 'rex-clr-grn';
+	else $cl = 'rex-clr-red';
+	echo '<tr><td class="'.$cl.'">'.$cur.'</td><td class="'.$cl.'">';
+
+	if (isset($REX['ADDON']['version'][$cur])) echo '['.$REX['ADDON']['version'][$cur].']';
+	echo '</td><td class="'.$cl.'">';
+
+	if (isset($REX['ADDON']['author'][$cur])) echo $REX['ADDON']['author'][$cur];
+	if (!isset($REX['ADDON']['page'][$cur])) echo 'AddOn inaktiv';
+	echo '</td><td class="'.$cl.'">';
+	if (isset($REX['ADDON']['supportpage'][$cur])) echo '<a href="'.$REX['ADDON']['supportpage'][$cur].'">'.$REX['ADDON']['supportpage'][$cur].'</a>';
 	echo '</td></tr>';
 }
 ?></table>
