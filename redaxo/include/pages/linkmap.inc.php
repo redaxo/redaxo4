24182616ea14e02695cad5f95d1a97dffb587cf7
staabm <markus.staab@redaxo.de>
2007-09-10 16:11:10 +0000

    &nbsp; bei leerem artikel/kategorie namen

--- redaxo/include/pages/linkmap.inc.php
+++ redaxo/include/pages/linkmap.inc.php
@@ -23,19 +23,30 @@ function rex_linkmap_backlink($id, $name)
   return 'javascript:insertLink(\'redaxo://'.$id.'\',\''.$name.'\');';
 }
 
+function rex_linkmap_format_label($OOobject)
+{
+  global $REX_USER;
+
+  $label = $OOobject->getName();
+
+  if(trim($label) == '')
+    $label = '&nbsp;';
+
+  if ($REX_USER->hasPerm('advancedMode[]'))
+    $label .= ' ['. $OOobject->getId() .']';
+
+  return $label;
+}
+
 function rex_linkmap_format_li($OOobject, $current_category_id, $GlobalParams, $liAttr = '', $linkAttr = '')
 {
-	global $REX_USER;
-
 	$liAttr .= $OOobject->getId() == $current_category_id ? ' id="rex-lmp-active"' : '';
 	$linkAttr .= ' class="'. ($OOobject->isOnline() ? 'rex-online' : 'rex-offine'). '"';
 
 	if(strpos($linkAttr, ' href=') === false)
 		$linkAttr .= ' href="'. rex_linkmap_url(array('category_id' => $OOobject->getId()), $GlobalParams) .'"';
 
-	$label = $OOobject->getName();
-	if ($REX_USER->hasPerm('advancedMode[]'))
-	  $label .= ' ['. $OOobject->getId() .']';
+	$label = rex_linkmap_format_label($OOobject);
 
 	return '<li'. $liAttr .'><a'. $linkAttr .'>'. $label .'</a>';
 }
@@ -74,8 +85,10 @@ function rex_linkmap_tree($tree, $category_id, $children, $GlobalParams)
       if($linkclasses != '')
         $linkclasses = ' class="'. rtrim($linkclasses) .'"';
 
+      $label = rex_linkmap_format_label($cat);
+
 			$li .= '      <li'.$liclasses.'>';
-			$li .= '<a'.$linkclasses.' href="'. rex_linkmap_url(array('category_id' => $cat_id), $GlobalParams).'">'.$cat->getName().'</a>';
+			$li .= '<a'.$linkclasses.' href="'. rex_linkmap_url(array('category_id' => $cat_id), $GlobalParams).'">'.$label.'</a>';
 			//$li .= ' '. $liclasses . $linkclasses;
 			$li .= $sub_li;
 			$li .= '</li>'. "\n";
