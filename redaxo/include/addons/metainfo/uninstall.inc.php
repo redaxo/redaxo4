3f7313fc419920d1d7bb5fc8b5e16f54b7a7cbf7
staabm <markus.staab@redaxo.de>
2007-09-10 17:51:24 +0000

    typo gefixed

--- redaxo/include/addons/metainfo/uninstall.inc.php
+++ redaxo/include/addons/metainfo/uninstall.inc.php
@@ -3,7 +3,7 @@
 $REX['ADDON']['install']['metainfo'] = 0;
 // ERRMSG IN CASE: $REX['ADDON']['installmsg']['metainfo'] = "Deinstallation fehlgeschlagen weil...";
 
-require_once ($REX['INCLUDE_PATH'] . '/addons/' . $mypage . '/extensions/extension_cleanup.inc.php');
+require_once ($REX['INCLUDE_PATH'] . '/addons/metainfo/extensions/extension_cleanup.inc.php');
 
 rex_a62_metainfo_cleanup(array());
 
