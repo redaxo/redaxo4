661c177336e0492cd352bb2e90309290852e36ea
staabm <markus.staab@redaxo.de>
2007-09-10 17:50:15 +0000

    cleanup methode gekapselt

--- redaxo/include/addons/metainfo/extensions/extension_cleanup.inc.php
+++ redaxo/include/addons/metainfo/extensions/extension_cleanup.inc.php
@@ -19,8 +19,43 @@ function rex_a62_metainfo_cleanup($params)
 {
 	global $REX;
 
-	$sql = new rex_sql();
-	$sql->setQuery('DELETE FROM '. $REX['TABLE_PREFIX'] .'62_params');
+  require_once $REX['INCLUDE_PATH'].'/addons/metainfo/classes/class.rex_tableExpander.inc.php';
+
+  $sql = new rex_sql();
+  $sql->setQuery('SELECT name FROM ' . $REX['TABLE_PREFIX'] . '62_params');
+
+  for ($i = 0; $i < $sql->getRows(); $i++)
+  {
+    if (substr($sql->getValue('name'), 0, 4) == 'med_')
+      $tableManager = new rex_a62_tableManager($REX['TABLE_PREFIX'] . 'file');
+    else
+      $tableManager = new rex_a62_tableManager($REX['TABLE_PREFIX'] . 'article');
+
+    $tableManager->deleteColumn($sql->getValue('name'));
+
+    $sql->next();
+  }
+
+
+  // evtl reste aufräumen
+  $tablePrefixes = array('article' => array('art_', 'cat_'), 'file' => array('med_'));
+  foreach($tablePrefixes as $table => $prefixes)
+  {
+    $table = $REX['TABLE_PREFIX'] .$table;
+    $tableManager = new rex_a62_tableManager($table);
+
+    foreach(rex_sql::showColumns($table) as $column)
+    {
+      $column = $column['name'];
+      if(in_array(substr($column, 0, 4), $prefixes))
+      {
+        $tableManager->deleteColumn($column);
+      }
+    }
+  }
+
+  $sql = new rex_sql();
+  $sql->setQuery('DELETE FROM '. $REX['TABLE_PREFIX'] .'62_params');
 }
 
 ?>
--- redaxo/include/addons/metainfo/uninstall.inc.php
+++ redaxo/include/addons/metainfo/uninstall.inc.php
@@ -1,40 +1,10 @@
 <?php
-require_once $REX['INCLUDE_PATH'].'/addons/metainfo/classes/class.rex_tableExpander.inc.php';
 
 $REX['ADDON']['install']['metainfo'] = 0;
 // ERRMSG IN CASE: $REX['ADDON']['installmsg']['metainfo'] = "Deinstallation fehlgeschlagen weil...";
 
-$sql = new rex_sql();
-$sql->setQuery('SELECT name FROM ' . $REX['TABLE_PREFIX'] . '62_params');
+require_once ($REX['INCLUDE_PATH'] . '/addons/' . $mypage . '/extensions/extension_cleanup.inc.php');
 
-for ($i = 0; $i < $sql->getRows(); $i++)
-{
-  if (substr($sql->getValue('name'), 0, 4) == 'med_')
-    $tableManager = new rex_a62_tableManager($REX['TABLE_PREFIX'] . 'file');
-  else
-    $tableManager = new rex_a62_tableManager($REX['TABLE_PREFIX'] . 'article');
-
-  $tableManager->deleteColumn($sql->getValue('name'));
-
-  $sql->next();
-}
-
-
-// evtl reste aufräumen
-$tablePrefixes = array('article' => array('art_', 'cat_'), 'file' => array('med_'));
-foreach($tablePrefixes as $table => $prefixes)
-{
-  $table = $REX['TABLE_PREFIX'] .$table;
-  $tableManager = new rex_a62_tableManager($table);
-
-  foreach(rex_sql::showColumns($table) as $column)
-  {
-    $column = $column['name'];
-    if(in_array(substr($column, 0, 4), $prefixes))
-    {
-      $tableManager->deleteColumn($column);
-    }
-  }
-}
+rex_a62_metainfo_cleanup(array());
 
 ?>
