c733c5f6f8d9baa93adf26e21a543ddd0d4cb152
staabm <markus.staab@redaxo.de>
2007-09-10 17:47:52 +0000

    cleanup routine ausgebaut

--- redaxo/include/addons/metainfo/uninstall.inc.php
+++ redaxo/include/addons/metainfo/uninstall.inc.php
@@ -1,22 +1,40 @@
 <?php
+require_once $REX['INCLUDE_PATH'].'/addons/metainfo/classes/class.rex_tableExpander.inc.php';
+
 $REX['ADDON']['install']['metainfo'] = 0;
 // ERRMSG IN CASE: $REX['ADDON']['installmsg']['metainfo'] = "Deinstallation fehlgeschlagen weil...";
 
 $sql = new rex_sql();
 $sql->setQuery('SELECT name FROM ' . $REX['TABLE_PREFIX'] . '62_params');
 
-$del = new rex_sql();
-
 for ($i = 0; $i < $sql->getRows(); $i++)
 {
   if (substr($sql->getValue('name'), 0, 4) == 'med_')
-  {
-    $del->setQuery('ALTER TABLE ' . $REX['TABLE_PREFIX'] . 'file DROP ' . $sql->getValue('name'));
-  }
+    $tableManager = new rex_a62_tableManager($REX['TABLE_PREFIX'] . 'file');
   else
-  {
-    $del->setQuery('ALTER TABLE ' . $REX['TABLE_PREFIX'] . 'article DROP ' . $sql->getValue('name'));
-  }
+    $tableManager = new rex_a62_tableManager($REX['TABLE_PREFIX'] . 'article');
+
+  $tableManager->deleteColumn($sql->getValue('name'));
+
   $sql->next();
 }
+
+
+// evtl reste aufräumen
+$tablePrefixes = array('article' => array('art_', 'cat_'), 'file' => array('med_'));
+foreach($tablePrefixes as $table => $prefixes)
+{
+  $table = $REX['TABLE_PREFIX'] .$table;
+  $tableManager = new rex_a62_tableManager($table);
+
+  foreach(rex_sql::showColumns($table) as $column)
+  {
+    $column = $column['name'];
+    if(in_array(substr($column, 0, 4), $prefixes))
+    {
+      $tableManager->deleteColumn($column);
+    }
+  }
+}
+
 ?>
