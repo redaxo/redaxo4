6aeeab639b776ab71fba8f585102eeaaf649b8eb
dergel <jan.kristinus@yakamara.de>
2007-10-09 17:43:45 +0000

    " " gefehlt

--- redaxo/include/classes/class.rex_article.inc.php
+++ redaxo/include/classes/class.rex_article.inc.php
@@ -713,8 +713,8 @@ class rex_article
           '.$RE_MODUL_IN.'
 		  </div></div>
           <p class="rex-sbmt">
-            <input type="submit" value="'.$I18N->msg('save_block').'" name="btn_save"'. rex_accesskey($I18N->msg('save_block'), $REX['ACKEY']['SAVE']) .' />
-            <input type="submit" value="'.$I18N->msg('update_block').'" name="btn_update"'. rex_accesskey($I18N->msg('update_block'), $REX['ACKEY']['APPLY']) .' />
+            <input type="submit" value="'.$I18N->msg('save_block').'" name="btn_save" '. rex_accesskey($I18N->msg('save_block'), $REX['ACKEY']['SAVE']) .' />
+            <input type="submit" value="'.$I18N->msg('update_block').'" name="btn_update" '. rex_accesskey($I18N->msg('update_block'), $REX['ACKEY']['APPLY']) .' />
           </p>
         </fieldset>
       </form>';
