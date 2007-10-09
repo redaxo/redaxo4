cd339b8630a5d8dc3ce23d58dbfb7b69c111990e
staabm <markus.staab@redaxo.de>
2007-10-09 14:54:42 +0000

    aktueller menupunkt mit rex-active klasse http://forum.redaxo.de/ftopic6832.html

--- redaxo/include/layout/top.php
+++ redaxo/include/layout/top.php
@@ -15,15 +15,15 @@ if ($page_name != '')
   $page_title .= ' - ' . $page_name;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $I18N->msg("htmllang"); ?>" lang="<?php echo $I18N->msg("htmllang"); ?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $I18N->msg('htmllang'); ?>" lang="<?php echo $I18N->msg('htmllang'); ?>">
 <head>
   <title><?php echo $page_title ?></title>
 <?php
   // ----- EXTENSION POINT
   echo rex_register_extension_point('PAGE_HEADER', '');
 ?>
-  <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $I18N->msg("htmlcharset"); ?>" />
-  <meta http-equiv="Content-Language" content="<?php echo $I18N->msg("htmllang"); ?>" />
+  <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $I18N->msg('htmlcharset'); ?>" />
+  <meta http-equiv="Content-Language" content="<?php echo $I18N->msg('htmllang'); ?>" />
   <meta http-equiv="Cache-Control" content="no-cache" />
   <meta http-equiv="Pragma" content="no-cache" />
   <link rel="stylesheet" type="text/css" href="media/css_backend.css" media="screen, projection, print" />
@@ -47,9 +47,9 @@ if (!isset($open_header_only)) echo 'onunload="closeAll();"';
 
 <div id="rex-hdr">
 
-	<p class="rex-hdr-top"><?php echo $REX['SERVERNAME']; ?></p>
+  <p class="rex-hdr-top"><?php echo $REX['SERVERNAME']; ?></p>
 
-	<div>
+  <div>
 <?php
 
 if (isset ($LOGIN) AND $LOGIN AND !isset($open_header_only))
@@ -59,36 +59,45 @@ if (isset ($LOGIN) AND $LOGIN AND !isset($open_header_only))
   $user_name = $REX_USER->getValue('name') != '' ? $REX_USER->getValue('name') : $REX_USER->getValue('login');
   echo '<p>' . $I18N->msg('name') . ' : <strong>' . $user_name . '</strong> [<a href="index.php?FORM[logout]=1"'. rex_accesskey($I18N->msg('logout'), $REX['ACKEY']['LOGOUT']) .'>' . $I18N->msg('logout') . '</a>]</p>' . "\n";
   echo '<ul>';
-  echo '<li><a href="index.php?page=structure"'. rex_tabindex() . rex_accesskey($I18N->msg('structure'), $accesskey++) .'>' . $I18N->msg("structure") . '</a></li>' . "\n";
 
-  if ($REX_USER->hasPerm("mediapool[]") || $REX_USER->hasPerm("admin[]") || ($REX_USER->hasPerm("clang[") AND ($REX_USER->hasPerm("csw[") || $REX_USER->hasPerm("csr["))))
+  $activeClass = ' class="rex-active"';
+  $liClass = $page == 'structure' || $page == 'content' ? $activeClass : '';
+  echo '<li'. $liClass .'><a href="index.php?page=structure"'. rex_tabindex() . rex_accesskey($I18N->msg('structure'), $accesskey++) .'>' . $I18N->msg("structure") . '</a></li>' . "\n";
+
+  if ($REX_USER->hasPerm('mediapool[]') || $REX_USER->hasPerm('admin[]') || ($REX_USER->hasPerm('clang[') AND ($REX_USER->hasPerm('csw[') || $REX_USER->hasPerm('csr['))))
   {
+    $liClass = $page == 'medienpool' ? $activeClass : '';
     echo '<li> | <a href="#" onclick="openMediaPool();"'. rex_tabindex() . rex_accesskey($I18N->msg('pool_name'), $accesskey++) .'>' . $I18N->msg("pool_name") . '</a></li>' . "\n";
   }
 
-  if ($REX_USER->hasPerm("template[]") || $REX_USER->hasPerm("admin[]"))
+  if ($REX_USER->hasPerm('template[]') || $REX_USER->hasPerm('admin[]'))
   {
-    echo '<li> | <a href="index.php?page=template"'. rex_tabindex() . rex_accesskey($I18N->msg('template'), $accesskey++) .'>' . $I18N->msg("template") . '</a></li>' . "\n";
+    $liClass = $page == 'template' ? $activeClass : '';
+    echo '<li'. $liClass .'> | <a href="index.php?page=template"'. rex_tabindex() . rex_accesskey($I18N->msg('template'), $accesskey++) .'>' . $I18N->msg("template") . '</a></li>' . "\n";
   }
 
-  if ($REX_USER->hasPerm("module[]") || $REX_USER->hasPerm("admin[]"))
+  if ($REX_USER->hasPerm('module[]') || $REX_USER->hasPerm('admin[]'))
   {
-    echo '<li> | <a href="index.php?page=module"'. rex_tabindex() . rex_accesskey($I18N->msg('modules'), $accesskey++) .'>' . $I18N->msg("modules") . '</a></li>' . "\n";
+    $liClass = $page == 'module' ? $activeClass : '';
+    echo '<li'. $liClass .'> | <a href="index.php?page=module"'. rex_tabindex() . rex_accesskey($I18N->msg('modules'), $accesskey++) .'>' . $I18N->msg("modules") . '</a></li>' . "\n";
   }
 
-  if ($REX_USER->hasPerm("user[]") || $REX_USER->hasPerm("admin[]"))
+  if ($REX_USER->hasPerm('user[]') || $REX_USER->hasPerm('admin[]'))
   {
-    echo '<li> | <a href="index.php?page=user"'. rex_tabindex() . rex_accesskey($I18N->msg('user'), $accesskey++) .'>' . $I18N->msg("user") . '</a></li>' . "\n";
+    $liClass = $page == 'user' ? $activeClass : '';
+    echo '<li'. $liClass .'> | <a href="index.php?page=user"'. rex_tabindex() . rex_accesskey($I18N->msg('user'), $accesskey++) .'>' . $I18N->msg("user") . '</a></li>' . "\n";
   }
 
-  if ($REX_USER->hasPerm("addon[]") || $REX_USER->hasPerm("admin[]"))
+  if ($REX_USER->hasPerm('addon[]') || $REX_USER->hasPerm('admin[]'))
   {
-    echo '<li> | <a href="index.php?page=addon"'. rex_tabindex() . rex_accesskey($I18N->msg('addon'), $accesskey++) .'>' . $I18N->msg("addon") . '</a></li>' . "\n";
+    $liClass = $page == 'addon' ? $activeClass : '';
+    echo '<li'. $liClass .'> | <a href="index.php?page=addon"'. rex_tabindex() . rex_accesskey($I18N->msg('addon'), $accesskey++) .'>' . $I18N->msg("addon") . '</a></li>' . "\n";
   }
 
-  if ($REX_USER->hasPerm("specials[]") || $REX_USER->hasPerm("admin[]"))
+  if ($REX_USER->hasPerm('specials[]') || $REX_USER->hasPerm('admin[]'))
   {
-    echo '<li> | <a href="index.php?page=specials"'. rex_tabindex() . rex_accesskey($I18N->msg('specials'), $accesskey++) .'>' . $I18N->msg("specials") . '</a></li>' . "\n";
+    $liClass = $page == 'specials' ? $activeClass : '';
+    echo '<li'. $liClass .'> | <a href="index.php?page=specials"'. rex_tabindex() . rex_accesskey($I18N->msg('specials'), $accesskey++) .'>' . $I18N->msg("specials") . '</a></li>' . "\n";
   }
 
   if (is_array($REX['ADDON']['status']))
@@ -127,25 +136,26 @@ if (isset ($LOGIN) AND $LOGIN AND !isset($open_header_only))
       // Leerzeichen durch &nbsp; ersetzen, damit Addonnamen immer in einer Zeile stehen
       $name = str_replace(' ', '&nbsp;', $name);
 
+      $liClass = $page == $apage ? $activeClass : '';
       if (current($REX['ADDON']['status']) == 1 && $name != '' && ($perm == '' || $REX_USER->hasPerm($perm) || $REX_USER->hasPerm("admin[]")))
       {
-      	$separator = ' | ';
-      	if($first)
-      	{
-      		$separator = '';
-  	    	$first = false;
-      	}
+        $separator = ' | ';
+        if($first)
+        {
+          $separator = '';
+          $first = false;
+        }
         if ($popup == 1)
         {
-          echo '<li>' . $separator . '<a href="javascript:newPoolWindow(\'index.php?page=' . $apage . '\');"'. rex_tabindex() . $accesskey .'>' . $name . '</a></li>' . "\n";
+          echo '<li'. $liClass .'>' . $separator . '<a href="javascript:newPoolWindow(\'index.php?page=' . $apage . '\');"'. rex_tabindex() . $accesskey .'>' . $name . '</a></li>' . "\n";
         }
-        elseif ($popup == "" or $popup == 0)
+        elseif ($popup == '' or $popup == 0)
         {
-          echo '<li>' . $separator . '<a href="index.php?page=' . $apage . '"'. rex_tabindex() . $accesskey .'>' . $name . '</a></li>' . "\n";
+          echo '<li'. $liClass .'>' . $separator . '<a href="index.php?page=' . $apage . '"'. rex_tabindex() . $accesskey .'>' . $name . '</a></li>' . "\n";
         }
         else
         {
-          echo '<li>' . $separator . '<a href="' . $popup . '"'. rex_tabindex() . $accesskey .'>' . $name . '</a></li>' . "\n";
+          echo '<li'. $liClass .'>' . $separator . '<a href="' . $popup . '"'. rex_tabindex() . $accesskey .'>' . $name . '</a></li>' . "\n";
         }
       }
       next($REX['ADDON']['status']);
@@ -159,10 +169,10 @@ else if(!isset($open_header_only))
   echo '<p>' . $I18N->msg('logged_out') . '</p>';
 }else
 {
-	echo '<p>&nbsp;</p>';
+  echo '<p>&nbsp;</p>';
 }
 ?>
-	</div>
+  </div>
 
 </div>
 <?php
