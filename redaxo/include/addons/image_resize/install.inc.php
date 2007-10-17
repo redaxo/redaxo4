1439eadd98fae440a464f56fabc39e75730cf9e2
staabm <markus.staab@redaxo.de>
2007-10-17 07:53:15 +0000

    cleanup

--- redaxo/include/addons/image_resize/install.inc.php
+++ redaxo/include/addons/image_resize/install.inc.php
@@ -17,12 +17,17 @@ $error = '';
 if (!extension_loaded('gd'))
 {
   if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
+  {
     $ext_loaded = @ dl('gd.dll');
+  }
   else
+  {
     $ext_loaded = @ dl('gd.so');
-
+  }
   if (!$ext_loaded)
+  {
     $error = 'GD-LIB-extension not available! See <a href="http://www.php.net/gd">http://www.php.net/gd</a>';
+  }
 }
 
 if ($error != '')
