b3e5a45823514b300896f97c626c3a854ba335da
staabm <markus.staab@redaxo.de>
2008-01-17 13:45:40 +0000

    typo fix in fehlermeldung

--- redaxo/include/functions/function_rex_generate.inc.php
+++ redaxo/include/functions/function_rex_generate.inc.php
@@ -1171,7 +1171,7 @@ function rex_generateTemplate($template_id)
     }
     else
     {
-      trigger_error('unable to generate template '. $template_id .'!', E_USER_ERROR);
+      trigger_error('Unable to generate template '. $template_id .'!', E_USER_ERROR);
 
       if(!is_writable())
         trigger_error('directory "'. $templatesDir .'" is not writable!', E_USER_ERROR);
