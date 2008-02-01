0bacd8c30302ec8de6aa6ab92764dfaaa8d37105
staabm <markus.staab@redaxo.de>
2008-02-01 10:07:02 +0000

    tiny3 anpassungen

--- redaxo/include/addons/tinymce/classes/class.tiny.inc.php
+++ redaxo/include/addons/tinymce/classes/class.tiny.inc.php
@@ -416,8 +416,8 @@ class rexTiny2Editor
     // Hier Fehler behandeln
     // siehe http://trac.symfony-project.com/ticket/2625
     echo ' try {'."\n";
-    echo '   win.resetImageData();'."\n";
-    echo '   win.showPreviewImage(imageUrl, false);'."\n";
+    echo '   win.ImageDialog.resetImageData();'."\n";
+    echo '   win.ImageDialog.showPreviewImage(imageUrl, false);'."\n";
     echo ' } catch (ex) {}'."\n";
     echo '}' . "\n";
 
