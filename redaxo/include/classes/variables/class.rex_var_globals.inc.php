6bdffcef4f8d5ca00b98cf078b5476ea5f9dc70f
staabm <markus.staab@redaxo.de>
2008-02-14 16:00:44 +0000

    * Neue Variablen in den Actions/Aktionen : [kills]     - $REX['ARTICLE_ID']     - $REX['EVENT']

--- redaxo/include/classes/variables/class.rex_var_globals.inc.php
+++ redaxo/include/classes/variables/class.rex_var_globals.inc.php
@@ -15,13 +15,28 @@ class rex_var_globals extends rex_var
 
   function getACRequestValues($REX_ACTION)
   {
-    // SLICE ID nur im Update Mode setzen
+    // SLICE ID im Update Mode setzen
     if($this->isEditEvent())
+    {
+      $REX_ACTION['EVENT'] = 'EDIT';
       $REX_ACTION['SLICE_ID'] = rex_request('slice_id', 'int');
+    }
+    // SLICE ID im Delete Mode setzen
+    elseif ($this->isDeleteEvent())
+    {
+      $REX_ACTION['EVENT'] = 'DELETE';
+      $REX_ACTION['SLICE_ID'] = rex_request('slice_id', 'int');
+    }
     // Im Add Mode 0 setze wg auto-increment
     else
+    {
+      $REX_ACTION['EVENT'] = 'ADD';
       $REX_ACTION['SLICE_ID'] = 0;
-       
+    }
+
+    // ArtikelId hier nur einfuegen, damit in einer Aktion abgefragt
+    // werden kann in welchem Artikel gerade gearbeitet wird
+    $REX_ACTION['ARTICLE_ID'] = rex_request('article_id', 'int');
     $REX_ACTION['CTYPE_ID'] = rex_request('ctype', 'int');
     $REX_ACTION['MODULE_ID'] = rex_request('module_id', 'int');
 
@@ -30,6 +45,9 @@ class rex_var_globals extends rex_var
 
   function getACDatabaseValues($REX_ACTION, & $sql)
   {
+    // ArtikelId hier nur einfuegen, damit in einer Aktion abgefragt
+    // werden kann in welchem Artikel gerade gearbeitet wird
+    $REX_ACTION['ARTICLE_ID'] = $this->getValue($sql, 'article_id');
     $REX_ACTION['SLICE_ID'] = $this->getValue($sql, 'id');
     $REX_ACTION['CTYPE_ID'] = $this->getValue($sql, 'ctype');
     $REX_ACTION['MODULE_ID'] = $this->getValue($sql, 'modultyp_id');
