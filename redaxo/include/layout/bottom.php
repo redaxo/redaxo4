77829e2fd85dc4e993eda46c7ceeec6f43337ce8
staabm <markus.staab@redaxo.de>
2008-02-29 15:51:28 +0000

    forum in neuem fenster oeffnen

--- redaxo/include/layout/bottom.php
+++ redaxo/include/layout/bottom.php
@@ -18,7 +18,7 @@
 			<li><a href="http://www.yakamara.de" onclick="window.open(this.href); return false;" class="black"<?php echo rex_tabindex() ?>>yakamara.de</a> | </li>
       <li><a href="http://www.redaxo.de" onclick="window.open(this.href); return false;" class="black"<?php echo rex_tabindex() ?>>redaxo.de</a> | </li>
 			<?php if(isset($REX_USER)) echo '<li><a href="index.php?page=credits" class="black">'.$I18N->msg('credits').'</a> | </li>'; ?>
-      <li><a href="http://forum.redaxo.de"<?php echo rex_tabindex() ?>>?</a></li>
+      <li><a href="http://forum.redaxo.de" onclick="window.open(this.href); return false;"<?php echo rex_tabindex() ?>>?</a></li>
 		</ul>
 		<p><?php echo showScripttime() ?> sec | <?php echo rex_formatter :: format(time(), 'strftime', 'date'); ?></p>
 	</div>
