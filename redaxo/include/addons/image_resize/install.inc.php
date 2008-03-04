8e76453711bcbfacef78c9e6a83cdef3a09fc29f
staabm <markus.staab@redaxo.de>
2008-03-04 14:52:51 +0000

    pruefen auf schreibrechte des config.inc damit settings geschrieben werden koennen

--- redaxo/include/addons/image_resize/install.inc.php
+++ redaxo/include/addons/image_resize/install.inc.php
@@ -19,8 +19,17 @@ if (!extension_loaded('gd'))
   $error = 'GD-LIB-extension not available! See <a href="http://www.php.net/gd">http://www.php.net/gd</a>';
 }
 
+if($error == '')
+{
+  $settings_file = $REX['INCLUDE_PATH'] .'/addons/image_resize/config.inc.php';
+
+  if(($state = rex_is_writable($settings_file)) !== true)
+    $error = $state;
+}
+
 if ($error != '')
   $REX['ADDON']['installmsg']['image_resize'] = $error;
 else
   $REX['ADDON']['install']['image_resize'] = true;
+
 ?>
