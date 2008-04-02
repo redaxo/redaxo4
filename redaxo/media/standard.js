dbf2a3e8f6dddb378c497e0890c77c636eaa9f8c
staabm <markus.staab@redaxo.de>
2008-04-02 20:45:50 +0000

    refactored accesskeys to jquery code

--- redaxo/media/standard.js
+++ redaxo/media/standard.js
@@ -526,139 +526,31 @@ function getElementsByClass(searchClass,node,tag) {
 	return classElements;
 }
 
-// written by Dean Edwards, 2005
-// with input from Tino Zijdel - crisp@xs4all.nl
-// http://dean.edwards.name/weblog/2005/10/add-event/
-function addEvent(element, type, handler)
-{
-  if (element.addEventListener)
-    element.addEventListener(type, handler, false);
-  else
-  {
-    if (!handler.$$guid) handler.$$guid = addEvent.guid++;
-    if (!element.events) element.events = {};
-    var handlers = element.events[type];
-    if (!handlers)
-    {
-      handlers = element.events[type] = {};
-      if (element['on' + type]) handlers[0] = element['on' + type];
-      element['on' + type] = handleEvent;
-    }
-  
-    handlers[handler.$$guid] = handler;
-  }
-}
-addEvent.guid = 1;
-
-function removeEvent(element, type, handler)
-{
-  if (element.removeEventListener)
-    element.removeEventListener(type, handler, false);
-  else if (element.events && element.events[type] && handler.$$guid)
-    delete element.events[type][handler.$$guid];
-}
-
-function handleEvent(event)
-{
-  event = event || fixEvent(window.event);
-  var returnValue = true;
-  var handlers = this.events[event.type];
-
-  for (var i in handlers)
-  {
-    if (!Object.prototype[i])
-    {
-      this.$$handler = handlers[i];
-      if (this.$$handler(event) === false) returnValue = false;
-    }
-  }
-
-  if (this.$$handler) this.$$handler = null;
-
-  return returnValue;
-}
-
-function fixEvent(event)
-{
-  event.preventDefault = fixEvent.preventDefault;
-  event.stopPropagation = fixEvent.stopPropagation;
-  return event;
-}
-fixEvent.preventDefault = function()
-{
-  this.returnValue = false;
-}
-fixEvent.stopPropagation = function()
-{
-  this.cancelBubble = true;
-}
-
-// This little snippet fixes the problem that the onload attribute on the body-element will overwrite
-// previous attached events on the window object for the onload event
-// see http://therealcrisp.xs4all.nl/upload/addEvent_dean.html
-if (!window.addEventListener)
-{
-  document.onreadystatechange = function()
-  {
-    if (window.onload && window.onload != handleEvent)
-    {
-      addEvent(window, 'load', window.onload);
-      window.onload = handleEvent;
-    }
-  }
-}
-
-// --------------- /AddEvent
-
-// Beim Drücken eines Access-Keys weiterleiten
-addEvent(document, 'keypress', function(e)
-{
-  var activeElement = document.activeElement || e.explicitOriginalTarget;
-  
-  if(!activeElement)
-    return;
-    
-  var sTagName = activeElement.tagName.toLowerCase();
-  if(sTagName == 'input' || sTagName == 'textarea' || sTagName == 'select' || sTagName == 'option')
-    return;
-    
-  var key = String.fromCharCode(e.keyCode || e.which);
-  
-  var buttons = document.getElementsByTagName('input');
-  for (var i = 0; i < buttons.length; i++)
-  {
-    if (buttons[i].getAttribute('accesskey') == key)
-    {
-      buttons[i].click();
-      stopEvent(e);
-      return;
-    }
-  }
-  
-  var anchors = document.getElementsByTagName('a');
-  for (var i = 0; i < anchors.length; i++)
-  {
-    if (anchors[i].getAttribute('accesskey') == key)
-    {
-      // href des links als location setzen 
-      if(anchors[i].onclick && anchors[i].onclick != "")
-        anchors[i].onclick();
+jQuery(function($){
+  $("*:not(a,input,textarea,select,option)")
+    .keypress(function(event) {
+      var key = String.fromCharCode(event.which);
+      var haystack = $("input[accesskey="+ key +"]");
+      
+      if(haystack.size() > 0)
+      {
+        $(haystack.get(0)).click();
+        return false;
+      }
+      else
+      {
+        haystack = $("a[accesskey="+ key +"]");
         
-      // onclick event auslösen, falls vorhanden
-      if(anchors[i].href && anchors[i].href != "" && anchors[i].href != "#")
-        document.location = anchors[i].href;
-        
-      stopEvent(e);
-      return;
-    }
-  }
+        if(haystack.size() > 0)
+        {
+          var hit = $(haystack.get(0));
+          if(hit.attr("onclick") != undefined)
+            hit.click();
+          else if(hit.attr("href") != undefined && hit.attr("href") != "#")
+            document.location = hit.attr("href");
+            
+          return false;
+        }
+      }    
+    });
 });
-
-
-function stopEvent(e)
-{
-  if (e.stopPropagation) e.stopPropagation();
-  else e.cancelBubble = true;
-  
-  return e.preventDefault();
-}
