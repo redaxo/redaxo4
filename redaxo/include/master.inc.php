019b2075c74fd20f099bce72eee813c70b85b97f
staabm <markus.staab@redaxo.de>
2008-12-30 11:27:24 +0000

    dirperm...

--- redaxo/include/master.inc.php
+++ redaxo/include/master.inc.php
@@ -21,6 +21,7 @@ $REX['SUBVERSION'] = "2";
 $REX['MINORVERSION'] = "0";
 $REX['ERROR_EMAIL'] = "jan.kristinus@yakamara.de";
 $REX['FILEPERM'] = octdec(660); // oktaler wert
+$REX['DIRPERM'] = octdec(770); // oktaler wert
 $REX['INSTNAME'] = "rex20081101010101";
 $REX['SESSION_DURATION'] = 3000;
 
