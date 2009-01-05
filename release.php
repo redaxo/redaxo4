201f3d882b91acf7ab9ee41d1b7ca7ec91576629
staabm <markus.staab@redaxo.de>
2009-01-05 21:03:36 +0000

    version update

--- release.php
+++ release.php
@@ -31,7 +31,7 @@ else
  * Verwendung in der Console:
  *
  *  Erstelles eines Release mit Versionsnummer:
- *  "php release.php 3.3"
+ *  "php release.php 4.2"
  *
  *
  * Vorgehensweise des release-scripts:
