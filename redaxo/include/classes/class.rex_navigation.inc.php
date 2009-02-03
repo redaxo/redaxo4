6b96a5921db59164bb7f27f236dca9cf163b807d
staabm <markus.staab@redaxo.de>
2009-02-03 17:04:55 +0000

    formatiert

--- redaxo/include/classes/class.rex_navigation.inc.php
+++ redaxo/include/classes/class.rex_navigation.inc.php
@@ -9,64 +9,64 @@
 
 /*
  * TODOS:
- * 
+ *
  * - Breadcrump
- * - CSS Klassen nach Ebene ($class = array('lev1', 'lev2', 'lev3')) 
+ * - CSS Klassen nach Ebene ($class = array('lev1', 'lev2', 'lev3'))
  */
 
 /*
  * Beispiel:
- * 
- * UL, LI Navigation von der Rootebene aus, 
- * 2 Ebenen durchgehen, Alle unternavis offen 
+ *
+ * UL, LI Navigation von der Rootebene aus,
+ * 2 Ebenen durchgehen, Alle unternavis offen
  * und offline categorien nicht beachten
- * 
+ *
  * $nav = rex_navigation::factory(0,2,TRUE,TRUE);
- * 
+ *
  * Sitemap
- * 
+ *
  * $nav = rex_navigation::factory(0,1000,TRUE,TRUE);
- * 
-*/
+ *
+ */
 
 class rex_navigation
 {
 
-  var $category_id = 0; // Startpunkt
-  var $deep = 3; // Wieviele Ebene tief, ab der Startebene
-  var $open = FALSE; // alles aufgeklappt, z.b. Sitemap
-  var $ignore_offlines = false;
+	var $category_id = 0; // Startpunkt
+	var $deep = 3; // Wieviele Ebene tief, ab der Startebene
+	var $open = FALSE; // alles aufgeklappt, z.b. Sitemap
+	var $ignore_offlines = false;
 	var $paths = array();
 
 	var $current_article_id = -1; // Aktueller Artikel
 	var $current_category_id = -1; // Aktuelle Katgorie
 
-  function rex_navigation($category_id = 0,$deep = 3,$open = FALSE, $ignore_offlines = false)
-  {
-    global $REX, $I18N;
-    $this->category_id = $category_id;
-    $this->deep = $deep;
-    $this->open = $open;
-    $this->ignore_offlines = $ignore_offlines;
-  }
+	function rex_navigation($category_id = 0,$deep = 3,$open = FALSE, $ignore_offlines = false)
+	{
+		global $REX, $I18N;
+		$this->category_id = $category_id;
+		$this->deep = $deep;
+		$this->open = $open;
+		$this->ignore_offlines = $ignore_offlines;
+	}
 
-  public function get()
-  {
-    global $REX,$I18N;
+	public function get()
+	{
+		global $REX,$I18N;
 		if(!$this->_setActivePaths()) return FALSE;
-    return $this->_getNavigation($this->category_id,0,$this->ignore_offlines);
-  }
+		return $this->_getNavigation($this->category_id,0,$this->ignore_offlines);
+	}
 
-  public function show()
-  {
-    echo $this->get();
-  }
+	public function show()
+	{
+		echo $this->get();
+	}
 
 	private function _setActivePaths()
 	{
 		global $REX;
 		$this->current_article_id = $REX["ARTICLE_ID"];
-		
+
 		if($a = OOArticle::getArticleById($REX["ARTICLE_ID"]))
 		{
 			$this->paths = explode("|",$a->getValue("path"));
@@ -81,43 +81,43 @@ class rex_navigation
 	private function _getNavigation($category_id,$cur_deep = 0,$ignore_offlines = TRUE)
 	{
 
-		if($category_id < 1) 
-    	$nav_obj = OOCategory::getRootCategories($ignore_offlines);
-    else 
-    	$nav_obj = OOCategory::getChildrenById($category_id, $ignore_offlines);
-		
+		if($category_id < 1)
+		$nav_obj = OOCategory::getRootCategories($ignore_offlines);
+		else
+		$nav_obj = OOCategory::getChildrenById($category_id, $ignore_offlines);
+
 		$return = "";
 
 		if(count($nav_obj)>0)
-			$return .= '<ul class="navi'.$cur_deep.'">';
+		$return .= '<ul class="navi'.$cur_deep.'">';
 
-    foreach($nav_obj as $nav)
+		foreach($nav_obj as $nav)
 		{
 			$path1 = 1;
-			
+				
 			if($nav->getId() == $this->current_category_id)
-				$return .= '<li class="active"><a href="'.$nav->getUrl().'">'.$nav->getName().'</a>';
+			$return .= '<li class="active"><a href="'.$nav->getUrl().'">'.$nav->getName().'</a>';
 			elseif (in_array($nav->getId(),$this->paths))
-					$return .= '<li class="active"><a class="current" href="'.$nav->getUrl().'">'.$nav->getName().'</a>';
-      else
-         $return .= '<li class="normal"><a href="'.$nav->getUrl().'">'.$nav->getName().'</a>';
-      
-	    $cur_deep++;
-      if(	
-      		($this->open && $this->deep > $cur_deep) || 
-      		($nav->getId() == $this->current_category_id && $this->deep > $cur_deep ) ||
-      		(in_array($nav->getId(),$this->paths) && $this->deep > $cur_deep)
-      	)
-      {
-      	$return .= $this->_getNavigation($nav->getId(),$cur_deep,$ignore_offlines);
-      }
-	    $cur_deep--;
-      
-      $return .= '</li>';
-		}		
+			$return .= '<li class="active"><a class="current" href="'.$nav->getUrl().'">'.$nav->getName().'</a>';
+			else
+			$return .= '<li class="normal"><a href="'.$nav->getUrl().'">'.$nav->getName().'</a>';
+
+			$cur_deep++;
+			if(
+			($this->open && $this->deep > $cur_deep) ||
+			($nav->getId() == $this->current_category_id && $this->deep > $cur_deep ) ||
+			(in_array($nav->getId(),$this->paths) && $this->deep > $cur_deep)
+			)
+			{
+				$return .= $this->_getNavigation($nav->getId(),$cur_deep,$ignore_offlines);
+			}
+			$cur_deep--;
+
+			$return .= '</li>';
+		}
 
 		if(count($nav_obj)>0)
-			$return .= '</ul>';
+		$return .= '</ul>';
 
 		return $return;
 	}
