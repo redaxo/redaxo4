0dc284d3eb768aba1ceae255bffc239f7356bae0
staabm <markus.staab@redaxo.de>
2009-02-04 10:54:19 +0000

    formatiert

--- redaxo/include/addons/metainfo/js/metainfo.js
+++ redaxo/include/addons/metainfo/js/metainfo.js
@@ -1,68 +1,57 @@
 var visibleNotice;
 
-function checkConditionalFields(selectEl, activeIds)
-{
+function checkConditionalFields(selectEl, activeIds) {
   var needle = new getObj('rex_62_params_Feld_bearbeiten_erstellen_params');
   var params = needle.obj;
   var toggle = false;
-  
-  for(var i = 0; i < activeIds.length; i++)
-  {
-    if(selectEl.value == activeIds[i])
-    {
+
+  for ( var i = 0; i < activeIds.length; i++) {
+    if (selectEl.value == activeIds[i]) {
       toggle = activeIds[i];
       break;
     }
   }
-  
-  if(toggle)
-  {
+
+  if (toggle) {
     // show params field
     toggleElement(params.parentNode, '');
-    
-    if(visibleNotice)
-    {
+
+    if (visibleNotice) {
       toggleElement(visibleNotice, 'none');
     }
-    
-    needle = new getObj('a62_field_params_notice_'+ toggle);
-    if(needle.obj)
-    {
+
+    needle = new getObj('a62_field_params_notice_' + toggle);
+    if (needle.obj) {
       toggleElement(needle.obj, '');
       visibleNotice = needle.obj;
     }
-  }
-  else
-  {
+  } else {
     // hide params field
     toggleElement(params.parentNode, 'none');
 
-    if(visibleNotice)
-    {
+    if (visibleNotice) {
       toggleElement(visibleNotice, 'none');
     }
   }
 };
 
-jQuery(function($) {
-  function disableSelect(chkbox)
-  {
-	  var disabled = $(chkbox).is(":checked") ? '' : 'disabled';
+jQuery( function($) {
+  function disableSelect(chkbox) {
+    var disabled = $(chkbox).is(":checked") ? '' : 'disabled';
     var sibling = chkbox;
-    while(sibling != null)
-    {
-      if(sibling.nodeType == 1 && sibling.tagName.toLowerCase() == "select")
-      {
+    while (sibling != null) {
+      if (sibling.nodeType == 1 && sibling.tagName.toLowerCase() == "select") {
         $(sibling).attr('disabled', disabled);
       }
       sibling = sibling.previousSibling;
     }
-  };
-  
-	$("input[type=checkbox].rex-metainfo-checkbox").click(function() {
-	   disableSelect(this);
-	});
-	$("input[type=checkbox].rex-metainfo-checkbox").each(function() {
-     disableSelect(this);
-	});
+  }
+  ;
+
+  $("input[type=checkbox].rex-metainfo-checkbox").click( function() {
+    disableSelect(this);
+  });
+  $("input[type=checkbox].rex-metainfo-checkbox").each( function() {
+    disableSelect(this);
+  });
 });
