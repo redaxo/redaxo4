0aa94e6b8aceaaedf6505f31b491bf3dbb1347a4
dergel <jan.kristinus@yakamara.de>
2009-02-19 17:42:23 +0000

    erste anpassungen..

--- redaxo/include/addons/be_style/config.inc.php
+++ redaxo/include/addons/be_style/config.inc.php
@@ -16,10 +16,24 @@
 $mypage = 'be_style';
 
 /* Addon Parameter */
-$REX['ADDON']['rxid'][$mypage] = 'xxYYxx';
+$REX['ADDON']['rxid'][$mypage] = '467';
 $REX['ADDON']['page'][$mypage] = $mypage;
-//$REX['ADDON']['name'][$mypage] = 'Backend Style';
+$REX['ADDON']['name'][$mypage] = 'Backend Style';
 //$REX['ADDON']['perm'][$mypage] = 'be_style[]';
 $REX['ADDON']['version'][$mypage] = '1.0';
 $REX['ADDON']['author'][$mypage] = 'Jan Kristinus, Markus Staab';
 $REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
+
+if($REX["REDAXO"])
+{
+	rex_register_extension('PAGE_HEADER', 'rex_be_style_css_add');
+	function rex_be_style_css_add($params)
+	{
+		$addon = "be_style";
+		foreach(rex_read_plugins_folder($addon) as $plugin)
+		{
+			if(OOPlugin::isActivated($addon, $plugin) && OOPlugin::isInstalled($addon, $plugin))
+				echo "\n".'<link rel="stylesheet" type="text/css" href="files/addons/'.$addon.'/plugins/'.$plugin.'/main.css" media="screen, projection, print" />';
+		}
+	}
+}
