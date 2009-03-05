b1b696c4e4ebced3b29c32c06a30ad0b28481b55
staabm <markus.staab@redaxo.de>
2009-03-05 19:24:39 +0000

    position des brandings einfacher konfigurierbar

--- redaxo/include/addons/image_resize/filters/filter.brand.inc.php
+++ redaxo/include/addons/image_resize/filters/filter.brand.inc.php
@@ -13,11 +13,48 @@ function image_resize_brand(& $src_im)
   $brandImage = $files[0];
   $brand = new rex_thumbnail($brandImage);
 
-  $paddX = 10;
-  $paddY = 10;
-
+  // -------------------------------------- CONFIG
+  
+  // Abstand vom Rand
+  $paddX = -10;
+  $paddY = -10;
+  // horizontale ausrichtung: left/center/right
+  $hpos = 'right'; 
+  // vertikale ausrichtung:   top/center/bottom
+  $vpos = 'bottom';
+  
+  // -------------------------------------- /CONFIG
+  
+  switch($hpos)
+  {
+    case 'left':
+      $dstX = 0;
+      break;
+    case 'center':
+      $dstX = (int)((imagesx($src_im) - $brand->getImageWidth()) / 2);
+      break;
+    case 'right':
+      $dstX = imagesx($src_im) - $brand->getImageWidth();
+      break;
+    default: trigger_error('Unexpected value for "hpos"!', E_USER_ERROR);
+  }
+  
+  switch($vpos)
+  {
+    case 'top':
+      $dstY = 0;
+      break;
+    case 'center':
+      $dstY = (int)((imagesy($src_im) - $brand->getImageHeight()) / 2);
+      break;
+    case 'bottom':
+      $dstY = imagesy($src_im) - $brand->getImageHeight();
+      break;
+    default: trigger_error('Unexpected value for "vpos"!', E_USER_ERROR);
+  }
+  
   imagealphablending($src_im, true);
-  imagecopy($src_im, $brand->getImage(), imagesx($src_im) - $brand->getImageWidth() - $paddX,  imagesy($src_im) - $brand->getImageHeight() - $paddY, 0, 0, $brand->getImageWidth(), $brand->getImageHeight());
+  imagecopy($src_im, $brand->getImage(), $dstX + $paddX, $dstY + $paddY, 0, 0, $brand->getImageWidth(), $brand->getImageHeight());
 
   $brand->destroyImage();
 }
