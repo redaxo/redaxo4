8325105827b01fc9e5972394321769a49e6a0cbf
staabm <markus.staab@redaxo.de>
2009-03-06 18:44:21 +0000

    compat: rex_medien* -> rex_media*

--- redaxo/include/classes/class.compat.inc.php
+++ redaxo/include/classes/class.compat.inc.php
@@ -184,6 +184,54 @@ function rex_parseArticleName($name)
   return rex_parse_article_name($name);
 }
 
+// rex_medien* -> rex_media*
+// Für < R4.2
+function rex_medienpool_filename($FILENAME, $doSubindexing = true)
+{
+  return rex_mediapool_filename($FILENAME, $doSubindexing);  
+}
+
+function rex_medienpool_saveMedia($FILE, $rex_file_category, $FILEINFOS, $userlogin = null, $doSubindexing = TRUE)
+{
+  return rex_mediapool_saveMedia($FILE, $rex_file_category, $FILEINFOS, $userlogin, $doSubindexing);
+}
+
+function rex_medienpool_updateMedia($FILE, &$FILEINFOS, $userlogin = null)
+{
+  return rex_mediapool_updateMedia($FILE, $FILEINFOS, $userlogin);  
+}
+
+function rex_medienpool_syncFile($physical_filename,$category_id,$title,$filesize = null, $filetype = null, $doSubindexing = FALSE)
+{
+  return rex_mediapool_syncFile($physical_filename,$category_id,$title,$filesize, $filetype, $doSubindexing);
+}
+
+function rex_medienpool_addMediacatOptions( &$select, &$mediacat, &$mediacat_ids, $groupName = '')
+{
+  return rex_mediapool_addMediacatOptions( $select, $mediacat, $mediacat_ids, $groupName);
+}
+
+function rex_medienpool_addMediacatOptionsWPerm( &$select, &$mediacat, &$mediacat_ids, $groupName = '')
+{
+  return rex_mediapool_addMediacatOptionsWPerm( $select, $mediacat, $mediacat_ids, $groupName);
+}
+
+function rex_medienpool_Mediaform($form_title, $button_title, $rex_file_category, $file_chooser, $close_form)
+{
+  return rex_mediapool_Mediaform($form_title, $button_title, $rex_file_category, $file_chooser, $close_form);
+}
+
+function rex_medienpool_Uploadform($rex_file_category)
+{
+  return rex_mediapool_Uploadform($rex_file_category);
+}
+
+function rex_medienpool_Syncform($rex_file_category)
+{
+  return rex_mediapool_Syncform($rex_file_category);
+}
+
+
 // ----------------------------------------- Variables
 
 // ---- since R4.2
