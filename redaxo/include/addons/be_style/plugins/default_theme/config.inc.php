058b7ebbc14355e4e28880e2fabe637387557c65
tbaddade <thomas@redaxo.de>
2009-03-07 12:30:52 +0000

    Zusatz CSS-Dateien eingebunden

--- redaxo/include/addons/be_style/plugins/default_theme/config.inc.php
+++ redaxo/include/addons/be_style/plugins/default_theme/config.inc.php
@@ -21,4 +21,30 @@ $REX['ADDON']['version'][$mypage] = '1.2';
 $REX['ADDON']['author'][$mypage] = 'Design: Ralph Zumkeller; Umsetzung: Thomas Blum';
 $REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
 
+
+
+if($REX["REDAXO"])
+{
+  
+  
+  /**
+   * Fügt die zusätzlichen zu css_main.css benötigten Stylesheets ein
+   * 
+   * @param $params Extension-Point Parameter
+   */
+  function rex_be_style_default_theme_css_add($params)
+  {
+    echo '      
+      <!--[if lte IE 7]>
+        <link rel="stylesheet" href="../files/addons/be_style/plugins/default_theme/css_ie_lte_7.css" type="text/css" media="screen, projection, print" />
+      <![endif]-->
+    
+      <!--[if lte IE 6]>
+        <link rel="stylesheet" href="../files/addons/be_style/plugins/default_theme/css_ie_lte_6.css" type="text/css" media="screen, projection, print" />
+      <![endif]-->';
+
+  }
+  
+  rex_register_extension('PAGE_HEADER', 'rex_be_style_default_theme_css_add');
+}
 ?>
