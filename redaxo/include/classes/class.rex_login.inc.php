932373b35b4cb3daaac55af5bffe7f9b05542565
dergel <jan.kristinus@yakamara.de>
2009-03-12 00:01:25 +0000

    rechte startpage problem gefixt

--- redaxo/include/classes/class.rex_login.inc.php
+++ redaxo/include/classes/class.rex_login.inc.php
@@ -440,7 +440,7 @@ class rex_backend_login extends rex_login
 	{
 	  global $REX;
 	  
-  	if (preg_match_all('@startpage\[([^\]]*)\]@' , $this->getValue("rights"), $matches))
+  	if (preg_match_all('@#startpage\[([^\]]*)\]#@' , $this->getValue("rights"), $matches))
   	{
     	foreach ($matches[1] as $match)
     	{
