8d1a0291ff477e580658ae98a323e8ef2fba9cda
dergel <jan.kristinus@yakamara.de>
2009-04-09 16:54:01 +0000

    firlefanz raus

--- redaxo/include/addons/version/install.inc.php
+++ redaxo/include/addons/version/install.inc.php
@@ -9,18 +9,6 @@
  * @version svn:$Id$
  */
 
-/*
-$create_sql = new rex_sql();
-$create_sql->setQuery('CREATE TABLE `rex_version_article` (
-`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`article_id` INT NOT NULL ,
-`version_id` INT NOT NULL ,
-`createdate` INT NOT NULL ,
-`createuser` VARCHAR( 255 ) NOT NULL ,
-`liveversion` INT NOT NULL ,
-) ENGINE = MYISAM ;');
-*/
-
 $create_sql = new rex_sql();
 $create_sql->setQuery('UPDATE rex_article_slice set revision=0 where revision<1 or revision IS NULL');
 $create_sql->setQuery('UPDATE rex_article set revision=0 where revision<1 or revision IS NULL');
