ffe5ae02e0a790b1a901069751b10f5e718ec4fa
staabm <markus.staab@redaxo.de>
2009-11-06 19:29:54 +0000

    ctrl notice; useless class entfernt

--- redaxo/include/addons/metainfo/classes/class.rex_restrictions_element.php
+++ redaxo/include/addons/metainfo/classes/class.rex_restrictions_element.php
@@ -12,7 +12,6 @@ class rex_form_restrictons_element extends rex_form_select_element
     
     parent::rex_form_select_element('', $table, $attributes);
     
-//    $this->chkbox_element = $table->addCheckboxField('restrictions');
     $this->chkbox_element = new rex_form_checkbox_element('', $dummy = null);
     $this->chkbox_element->setAttribute('name', 'enable_restrictions');
     $this->chkbox_element->setAttribute('id', 'enable_restrictions_chkbx');
@@ -21,6 +20,7 @@ class rex_form_restrictons_element extends rex_form_select_element
     $categorySelect = new rex_category_select();
     $categorySelect->setMultiple(true);
     $this->setSelect($categorySelect);
+    $this->setNotice($I18N->msg('ctrl'));
   }
 
   function get()
