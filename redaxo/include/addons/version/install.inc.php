a81018706c01af4ddd34d3f002441381602ac281
gharlan <gharlan@web.de>
2009-11-26 23:24:39 +0000

    rex_ -> $REX['TABLE_PREFIX']

--- redaxo/include/addons/version/install.inc.php
+++ redaxo/include/addons/version/install.inc.php
@@ -10,8 +10,8 @@
  */
 
 $create_sql = rex_sql::factory();
-$create_sql->setQuery('UPDATE rex_article_slice set revision=0 where revision<1 or revision IS NULL');
-$create_sql->setQuery('UPDATE rex_article set revision=0 where revision<1 or revision IS NULL');
+$create_sql->setQuery('UPDATE '.$REX['TABLE_PREFIX'].'article_slice set revision=0 where revision<1 or revision IS NULL');
+$create_sql->setQuery('UPDATE '.$REX['TABLE_PREFIX'].'article set revision=0 where revision<1 or revision IS NULL');
 // $create_sql->setQuery("ALTER TABLE `rex_template` ADD `revision` INT NOT NULL DEFAULT '0'");
 // $create_sql->setQuery("ALTER TABLE `rex_template` CHANGE `revision` `revision` INT( 11 ) NOT NULL DEFAULT '0'");
 
