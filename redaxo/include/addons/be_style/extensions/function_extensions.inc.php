a81f32d47697e1d4e392be5fd347b22de6589559
staabm <markus.staab@redaxo.de>
2010-01-05 21:52:05 +0000

    ggf auch mehrere plugin css includen

--- redaxo/include/addons/be_style/extensions/function_extensions.inc.php
+++ redaxo/include/addons/be_style/extensions/function_extensions.inc.php
@@ -28,6 +28,6 @@ function rex_be_style_css_add($params)
   foreach(OOPlugin::getAvailablePlugins($addon) as $plugin)
   {
   	$params["subject"] .= "\n".'  <link rel="stylesheet" type="text/css" href="../files/addons/'.$addon.'/plugins/'.$plugin.'/css_main.css" media="screen, projection, print" />';
-    return $params["subject"];
   }
+  return $params["subject"];
 }
