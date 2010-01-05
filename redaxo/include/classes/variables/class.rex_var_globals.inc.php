086557326e66239a503461feb9ce1ab1358b4478
staabm <markus.staab@redaxo.de>
2010-01-05 21:59:28 +0000

    REX_ACTION[EVENT] fix fuer preview action

--- redaxo/include/classes/variables/class.rex_var_globals.inc.php
+++ redaxo/include/classes/variables/class.rex_var_globals.inc.php
@@ -13,7 +13,45 @@ class rex_var_globals extends rex_var
 {
   // --------------------------------- Actions
 
-  function getACRequestValues($REX_ACTION)
+  /*public*/ function getACRequestValues($REX_ACTION)
+  {
+    // determine event-type...
+    $REX_ACTION = $this->getEventData($REX_ACTION);
+    
+    // Variablen hier einfuegen, damit sie in einer
+    // Aktion abgefragt werden können
+    $REX_ACTION['ARTICLE_ID'] = rex_request('article_id', 'int');
+    $REX_ACTION['CLANG_ID']   = rex_request('clang', 'int');
+    $REX_ACTION['CTYPE_ID']   = rex_request('ctype', 'int');
+    $REX_ACTION['MODULE_ID']  = rex_request('module_id', 'int');
+
+    return $REX_ACTION;
+  }
+
+  /*public*/ function getACDatabaseValues($REX_ACTION, & $sql)
+  {
+    // determine event-type...
+    $REX_ACTION = $this->getEventData($REX_ACTION);
+    
+    // Variablen hier einfuegen, damit sie in einer
+    // Aktion abgefragt werden können
+    $REX_ACTION['ARTICLE_ID'] = $this->getValue($sql, 'article_id');
+    $REX_ACTION['CLANG_ID']   = $this->getValue($sql, 'clang');
+    $REX_ACTION['CTYPE_ID']   = $this->getValue($sql, 'ctype');
+    $REX_ACTION['MODULE_ID']  = $this->getValue($sql, 'modultyp_id');
+    $REX_ACTION['SLICE_ID']   = $this->getValue($sql, 'id');
+
+    return $REX_ACTION;
+  }
+
+  /*public*/ function setACValues(& $sql, $REX_ACTION, $escape = false)
+  {
+    $this->setValue($sql, 'id', $REX_ACTION['SLICE_ID'], $escape);
+    $this->setValue($sql, 'ctype', $REX_ACTION['CTYPE_ID'], $escape);
+    $this->setValue($sql, 'modultyp_id', $REX_ACTION['MODULE_ID'], $escape);
+  }
+  
+  /*private*/ function getEventData($REX_ACTION)
   {
     // SLICE ID im Update Mode setzen
     if($this->isEditEvent())
@@ -33,40 +71,12 @@ class rex_var_globals extends rex_var
       $REX_ACTION['EVENT'] = 'ADD';
       $REX_ACTION['SLICE_ID'] = 0;
     }
-
-    // Variablen hier einfuegen, damit sie in einer
-    // Aktion abgefragt werden können
-    $REX_ACTION['ARTICLE_ID'] = rex_request('article_id', 'int');
-    $REX_ACTION['CLANG_ID'] = rex_request('clang', 'int');
-    $REX_ACTION['CTYPE_ID'] = rex_request('ctype', 'int');
-    $REX_ACTION['MODULE_ID'] = rex_request('module_id', 'int');
-
     return $REX_ACTION;
-  }
-
-  function getACDatabaseValues($REX_ACTION, & $sql)
-  {
-    // Variablen hier einfuegen, damit sie in einer
-    // Aktion abgefragt werden können
-    $REX_ACTION['ARTICLE_ID'] = $this->getValue($sql, 'article_id');
-    $REX_ACTION['CLANG_ID'] = $this->getValue($sql, 'clang');
-    $REX_ACTION['CTYPE_ID'] = $this->getValue($sql, 'ctype');
-    $REX_ACTION['MODULE_ID'] = $this->getValue($sql, 'modultyp_id');
-    $REX_ACTION['SLICE_ID'] = $this->getValue($sql, 'id');
-
-    return $REX_ACTION;
-  }
-
-  function setACValues(& $sql, $REX_ACTION, $escape = false)
-  {
-    $this->setValue($sql, 'id', $REX_ACTION['SLICE_ID'], $escape);
-    $this->setValue($sql, 'ctype', $REX_ACTION['CTYPE_ID'], $escape);
-    $this->setValue($sql, 'modultyp_id', $REX_ACTION['MODULE_ID'], $escape);
   }
 
   // --------------------------------- Output
 
-  function getBEOutput(& $sql, $content)
+  /*public*/ function getBEOutput(& $sql, $content)
   {
     // Modulabhängige Globale Variablen ersetzen
     $content = str_replace('REX_MODULE_ID', $this->getValue($sql, 'modultyp_id'), $content);
