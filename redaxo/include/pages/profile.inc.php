be9c99453bb80f728da9c6bc7714acf4a3879600
dergel <jan.kristinus@yakamara.de>
2010-01-25 08:36:17 +0000

    Profil: Passwort ändern verbessert - altes passwort muss eingegeben werden

--- redaxo/include/pages/profile.inc.php
+++ redaxo/include/pages/profile.inc.php
@@ -10,7 +10,10 @@ $warning = '';
 $user_id = $REX['USER']->getValue('user_id');
 
 // Allgemeine Infos
-$userpsw  = trim(rex_request('userpsw', 'string'));
+$userpsw = trim(rex_request('userpsw', 'string'));
+$userpsw_new_1 = trim(rex_request('userpsw_new_1', 'string'));
+$userpsw_new_2 = trim(rex_request('userpsw_new_2', 'string'));
+
 $username = rex_request('username', 'string');
 $userdesc = rex_request('userdesc', 'string');
 
@@ -71,20 +74,14 @@ if (rex_post('upd_profile_button', 'string'))
   $updateuser->setValue('name',$username);
   $updateuser->setValue('description',$userdesc);
 
-  if($userpsw != '')
-  {
-    if ($REX['PSWFUNC']!='' && $userpsw != $REX['USER']->getValue($REX['TABLE_PREFIX'].'user.psw')) 
-  	  $userpsw = call_user_func($REX['PSWFUNC'],$userpsw);
-    $updateuser->setValue('psw',$userpsw);
-  }
-
-	// set be langauage
-	$userperm_be_sprache = rex_request("userperm_be_sprache","string");
-	if(!isset($langs[$userperm_be_sprache])) $userperm_be_sprache = "default";
-	$userperm_be_sprache_selected = $userperm_be_sprache;
+  // set be langauage
+  $userperm_be_sprache = rex_request("userperm_be_sprache","string");
+  if(!isset($langs[$userperm_be_sprache])) 
+    $userperm_be_sprache = "default";
+  $userperm_be_sprache_selected = $userperm_be_sprache;
 	
-	$rights = preg_replace('@#be_lang\[([^\]]*)\]@' , '', $REX['LOGIN']->getValue("rights"));
-	$rights .= 'be_lang['.$userperm_be_sprache.']#';
+  $rights = preg_replace('@#be_lang\[([^\]]*)\]@' , '', $REX['LOGIN']->getValue("rights"));
+  $rights .= 'be_lang['.$userperm_be_sprache.']#';
   $updateuser->setValue('rights',$rights);
 	
   $updateuser->addGlobalUpdateFields();
@@ -94,6 +91,37 @@ if (rex_post('upd_profile_button', 'string'))
   else
     $warning = $updateuser->getError();
 }
+
+
+if (rex_post('upd_psw_button', 'string'))
+{
+  $updateuser = rex_sql::factory();
+  $updateuser->setTable($REX['TABLE_PREFIX'].'user');
+  $updateuser->setWhere('user_id='. $user_id);
+
+  if ($REX['PSWFUNC'] != '') 
+    $userpsw = call_user_func($REX['PSWFUNC'],$userpsw);
+    
+  if($userpsw != '' && $REX['USER']->getValue('psw') == $userpsw && $userpsw_new_1 != '' && $userpsw_new_1 == $userpsw_new_2)
+  {
+    if ($REX['PSWFUNC'] != '') 
+      $userpsw_new_1 = call_user_func($REX['PSWFUNC'],$userpsw_new_1);
+  
+    $updateuser->setValue('psw',$userpsw_new_1);
+    $updateuser->addGlobalUpdateFields();
+
+    if($updateuser->update())
+      $info = $I18N->msg('user_psw_updated');
+    else
+      $warning = $updateuser->getError();
+    
+  }else
+  {
+  	$warning = $I18N->msg('user_psw_error');
+  }
+
+}
+
 
 $sel_be_sprache->setSelected($userperm_be_sprache_selected);
 
@@ -117,7 +145,7 @@ if ($sql->getRows()!=1)
 }
 else
 {
-  $userpsw = $sql->getValue($REX['TABLE_PREFIX'].'user.psw');
+  // $userpsw = $sql->getValue($REX['TABLE_PREFIX'].'user.psw');
   $username = $sql->getValue($REX['TABLE_PREFIX'].'user.name');
   $userdesc = $sql->getValue($REX['TABLE_PREFIX'].'user.description');
 
@@ -135,11 +163,11 @@ else
               <label for="userlogin">'. htmlspecialchars($I18N->msg('login_name')).'</label>
               <span class="rex-form-read" id="userlogin">'. htmlspecialchars($sql->getValue($REX['TABLE_PREFIX'].'user.login')) .'</span>
 						</p>
-						<p class="rex-form-col-b rex-form-text">
-              <label for="userpsw">'.$I18N->msg('new_password').'</label>
-							<input class="rex-form-text" type="password" id="userpsw" name="userpsw" />
-							'. ($REX['PSWFUNC']!='' ? '<span class="rex-form-notice">'. $I18N->msg('psw_encrypted') .'</span>' : '') .'
-						</p>
+
+	          <p class="rex-form-col-b rex-form-select">
+	            <label for="userperm-mylang">'.$I18N->msg('backend_language').'</label>
+	            '.$sel_be_sprache->get().'
+	          </p>
 					</div>
 
 					<div class="rex-form-row">
@@ -153,13 +181,6 @@ else
             </p>
       		</div>
 
-					<div class="rex-form-row">
-          <p class="rex-form-col-a rex-form-select">
-            <label for="userperm-mylang">'.$I18N->msg('backend_language').'</label>
-            '.$sel_be_sprache->get().'
-          </p>
-			    </div>
-
       	</div>
       </fieldset>
 
@@ -168,6 +189,50 @@ else
           <div class="rex-form-row">
 						<p class="rex-form-col-a rex-form-submit">
             	<input class="rex-form-submit" type="submit" name="upd_profile_button" value="'.$I18N->msg('profile_save').'" '. rex_accesskey($I18N->msg('user_save'), $REX['ACKEY']['SAVE']) .' />
+            </p>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+    </div>
+  ';
+
+  echo '<p>&nbsp;</p>
+    <div class="rex-form" id="rex-form-profile-psw">
+    <form action="index.php" method="post">
+      <fieldset class="rex-form-col-2">
+        <legend>'.$I18N->msg('profile_changepsw').'</legend>
+
+        <div class="rex-form-wrapper">
+          <input type="hidden" name="page" value="profile" />
+
+					<div class="rex-form-row">
+			    	<p class="rex-form-col-a rex-form-text">
+              			<label for="userpsw">'.$I18N->msg('old_password').'</label>
+						<input class="rex-form-text" type="password" id="userpsw" name="userpsw" />
+						</p>
+					</div>
+					
+					
+					<div class="rex-form-row">
+			    	<p class="rex-form-col-a rex-form-text">
+             				 <label for="userpsw">'.$I18N->msg('new_password').'</label>
+							<input class="rex-form-text" type="password" id="userpsw_new_1" name="userpsw_new_1" />
+						</p>
+			    	<p class="rex-form-col-b rex-form-text">
+              				<label for="userpsw">'.$I18N->msg('new_password_repeat').'</label>
+							<input class="rex-form-text" type="password" id="userpsw_new_2" name="userpsw_new_2" />
+						</p>
+					</div>
+
+      	</div>
+      </fieldset>
+
+      <fieldset class="rex-form-col-1">
+        <div class="rex-form-wrapper">
+          <div class="rex-form-row">
+						<p class="rex-form-col-a rex-form-submit">
+            	<input class="rex-form-submit" type="submit" name="upd_psw_button" value="'.$I18N->msg('profile_save_psw').'" '. rex_accesskey($I18N->msg('user_save'), $REX['ACKEY']['SAVE']) .' />
             </p>
           </div>
         </div>
