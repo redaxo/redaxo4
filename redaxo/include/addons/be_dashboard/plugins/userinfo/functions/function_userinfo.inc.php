bc44dee25e6654c021c364ab5cab5d5555892503
tbaddade <thomas@redaxo.de>
2010-01-31 10:40:04 +0000

    Query - Anpassungen

--- redaxo/include/addons/be_dashboard/plugins/userinfo/functions/function_userinfo.inc.php
+++ redaxo/include/addons/be_dashboard/plugins/userinfo/functions/function_userinfo.inc.php
@@ -20,7 +20,7 @@ function rex_a659_statistics()
   
   $sql = rex_sql::factory();
 //  $sql->debugsql = true;
-  $result = $sql->getArray('SELECT COUNT(*) as count, updatedate FROM '. $REX['TABLE_PREFIX'] .'article WHERE clang=0 ORDER BY updatedate DESC LIMIT 1');
+  $result = $sql->getArray('SELECT COUNT(*) as count, updatedate FROM '. $REX['TABLE_PREFIX'] .'article WHERE clang=0 GROUP BY clang ORDER BY updatedate DESC');
   if(count($result) > 0)
   {
     $stats['total_articles'] = $result[0]['count'];
@@ -31,7 +31,7 @@ function rex_a659_statistics()
     $stats['total_articles'] = 0;
   }
   
-  $result = $sql->getArray('SELECT COUNT(*) as count, updatedate FROM '. $REX['TABLE_PREFIX'] .'article_slice ORDER BY updatedate DESC LIMIT 1');
+  $result = $sql->getArray('SELECT COUNT(*) as count, updatedate FROM '. $REX['TABLE_PREFIX'] .'article_slice GROUP BY revision ORDER BY updatedate DESC LIMIT 1');
   if(count($result) > 0)
   {
     $stats['total_slices'] = $result[0]['count'];
@@ -52,10 +52,11 @@ function rex_a659_statistics()
     $stats['total_clangs'] = 0;
   }
   
-  $result = $sql->getArray('SELECT COUNT(*) as count, updatedate FROM '. $REX['TABLE_PREFIX'] .'template ORDER BY updatedate DESC LIMIT 1');
+  $result = $sql->getArray('SELECT COUNT(*) as count FROM '. $REX['TABLE_PREFIX'] .'template');
   if(count($result) > 0)
   {
     $stats['total_templates'] = $result[0]['count'];
+	  $result = $sql->getArray('SELECT updatedate FROM '. $REX['TABLE_PREFIX'] .'template ORDER BY updatedate DESC LIMIT 1');
     $stats['last_update'] = $result[0]['updatedate'] > $stats['last_update'] ? $result[0]['updatedate'] : $stats['last_update'];
   }
   else
@@ -63,7 +64,7 @@ function rex_a659_statistics()
     $stats['total_templates'] = 0;
   }
   
-  $result = $sql->getArray('SELECT COUNT(*) as count, updatedate FROM '. $REX['TABLE_PREFIX'] .'module ORDER BY updatedate DESC LIMIT 1');
+  $result = $sql->getArray('SELECT COUNT(*) as count, updatedate FROM '. $REX['TABLE_PREFIX'] .'module GROUP BY revision ORDER BY updatedate DESC LIMIT 1');
   if(count($result) > 0)
   {
     $stats['total_modules'] = $result[0]['count'];
@@ -74,7 +75,7 @@ function rex_a659_statistics()
     $stats['total_modules'] = 0;
   }
   
-  $result = $sql->getArray('SELECT COUNT(*) as count, updatedate FROM '. $REX['TABLE_PREFIX'] .'action ORDER BY updatedate DESC LIMIT 1');
+  $result = $sql->getArray('SELECT COUNT(*) as count, updatedate FROM '. $REX['TABLE_PREFIX'] .'action GROUP BY revision ORDER BY updatedate DESC LIMIT 1');
   if(count($result) > 0)
   {
     $stats['total_actions'] = $result[0]['count'];
@@ -85,7 +86,7 @@ function rex_a659_statistics()
     $stats['total_actions'] = 0;
   }
   
-  $result = $sql->getArray('SELECT COUNT(*) as count, updatedate FROM '. $REX['TABLE_PREFIX'] .'user ORDER BY updatedate DESC LIMIT 1');
+  $result = $sql->getArray('SELECT COUNT(*) as count, updatedate FROM '. $REX['TABLE_PREFIX'] .'user GROUP BY revision ORDER BY updatedate DESC LIMIT 1');
   if(count($result) > 0)
   {
     $stats['total_users'] = $result[0]['count'];
