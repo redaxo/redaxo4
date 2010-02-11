19fb98aed13454211623301e3102e5ab623e6975
dergel <jan.kristinus@yakamara.de>
2010-02-11 17:20:21 +0000

    community, xform, anpassung com_user

--- redaxo/include/addons/community/xform/value/class.xform.com_user.inc.php
+++ redaxo/include/addons/community/xform/value/class.xform.com_user.inc.php
@@ -5,43 +5,52 @@
 class rex_xform_com_user extends rex_xform_abstract
 {
 
-	// Aufruf des Objektes mit den verschiedenen Zeigern
-	function enterObject(&$email_elements,&$sql_elements,&$warning,&$form_output,$send = 0)
-	{		
-		global $REX;
-	
-		$this->value = -1;
-		$this->user_name = "";
-		if (isset($REX["COM_USER"]) && is_object($REX["COM_USER"]))
-		{
-			$user_field = $REX["COM_USER"]->getValue($this->elements[2]);
-			$this->value = $user_field;
-			$this->user_name = $REX["COM_USER"]->getValue("firstname")." ".$REX["COM_USER"]->getValue("name");
-		}
+  function enterObject(&$email_elements,&$sql_elements,&$warning,&$form_output,$send = 0)
+  {   
+    global $REX;
 
-		$wc = "";
-		if (isset($warning["el_" . $this->getId()])) 
-			$wc = $warning["el_" . $this->getId()];
+    $show_label = "email";
+    $show_value = "";
+    if(isset($this->elements[6]))
+    {
+      $show_label = $this->elements[6];
+    }
+    
+    $this->value = -1;
+    if (isset($REX["COM_USER"]) && is_object($REX["COM_USER"]))
+    {
+      $this->value = $REX["COM_USER"]->getValue($this->elements[2]);
+      $show_value = $REX["COM_USER"]->getValue($show_label);
+    }
 
-		if (!isset($this->elements[4]) || trim($this->elements[4]) != "hidden")
-		{
-			$form_output[] = '
-				<p class="formtext">
-			  	<label class="text ' . $wc . '" for="el_' . $this->id . '" >' . $this->elements[3] . '</label>
-				  <input type="text" class="text inp_disabled" disabled="disabled"  id="el_' . $this->id . '" value="'.htmlspecialchars($this->user_name) . '" />
-				</p>';
-		}
-		
-		$email_elements[$this->elements[1]] = stripslashes($this->value);
-		if (!isset($this->elements[5]) || $this->elements[5] != "no_db") 
-		  $sql_elements[$this->elements[1]] = $this->value;
-		
-	}
-	
-	function getDescription()
-	{
-		return "com_user -> Beispiel: com_user|label|dbfield|Fieldlabel|hidden|[no_db]";
-	}
+    $wc = "";
+    if (isset($warning["el_" . $this->getId()]))
+    {
+      $wc = $warning["el_" . $this->getId()];
+    }
+
+    if (!isset($this->elements[4]) || trim($this->elements[4]) != "hidden")
+    {
+      $form_output[] = '
+        <p class="formtext">
+          <label class="text ' . $wc . '" for="el_' . $this->id . '" >' . $this->elements[3] . '</label>
+          <input type="text" class="text inp_disabled" disabled="disabled"  id="el_' . $this->id . '" value="'.htmlspecialchars($show_value) . '" />
+        </p>';
+    }
+    
+    $email_elements[$this->getLabel()] = stripslashes($this->value);
+    if (!isset($this->elements[5]) || $this->elements[5] != "no_db")
+    {
+      $sql_elements[$this->getLabel()] = $this->value;
+    }
+    
+  }
+  
+  function getDescription()
+  {
+    return "com_user -> Beispiel: com_user|label|dbfield|Fieldlabel|hidden|[no_db]|showlabel";
+  }
+  
 }
 
 ?>
