6b672499944a8fd6aa0a3f0a69b7d22e378bb48b
staabm <markus.staab@redaxo.de>
2010-02-13 13:00:04 +0000

    "unwichtige" komponenten auskommentiert

--- redaxo/include/addons/be_dashboard/plugins/userinfo/config.inc.php
+++ redaxo/include/addons/be_dashboard/plugins/userinfo/config.inc.php
@@ -34,13 +34,13 @@ if($REX["REDAXO"])
     require_once dirname(__FILE__) .'/classes/class.dashboard.inc.php';
     
     $adminComponents = array(
-      'rex_stats_component',
       'rex_articles_component',
-      'rex_templates_component',
-      'rex_modules_component',
-      'rex_actions_component',
-      'rex_users_component',
+//      'rex_templates_component',
+//      'rex_modules_component',
+//      'rex_actions_component',
+//      'rex_users_component',
       'rex_media_component',
+      'rex_stats_component',
     );
     
     foreach($adminComponents as $compClass)
