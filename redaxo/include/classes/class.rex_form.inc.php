d7a594d16c1304a52e2e97dd4b86d0c85ba13de7
gharlan <gharlan@web.de>
2010-02-14 22:04:49 +0000

    rex_form -> Linklist hinzugefügt

--- redaxo/include/classes/class.rex_form.inc.php
+++ redaxo/include/classes/class.rex_form.inc.php
@@ -282,6 +282,13 @@ class rex_form
     $field =& $this->addField('', $name, $value, $attributes, true);
     return $field;
   }
+  
+  /*public*/ function &addLinklistField($name, $value = null, $attributes = array())
+  {
+    $attributes['internal::fieldClass'] = 'rex_form_widget_linklist_element';
+    $field =& $this->addField('', $name, $value, $attributes, true);
+    return $field;
+  }
 
   /*public*/ function &addControlField($saveElement = null, $applyElement = null, $deleteElement = null, $resetElement = null, $abortElement = null)
   {
@@ -1608,6 +1615,28 @@ class rex_form_widget_linkmap_element extends rex_form_element
 
     $html = rex_var_link::getLinkButton($widget_counter, $this->getValue());
     $html = str_replace('LINK['. $widget_counter .']', $this->getAttribute('name'), $html);
+
+    $widget_counter++;
+    return $html;
+  }
+}
+
+class rex_form_widget_linklist_element extends rex_form_element
+{
+  // 1. Parameter nicht genutzt, muss aber hier stehen,
+  // wg einheitlicher Konstrukturparameter
+  function rex_form_widget_linklist_element($tag = '', &$table, $attributes = array())
+  {
+    parent::rex_form_element('', $table, $attributes);
+  }
+
+
+  function formatElement()
+  {
+    static $widget_counter = 1;
+
+    $html = rex_var_link::getLinkListButton($widget_counter, $this->getValue());
+    $html = str_replace('LINKLIST['. $widget_counter .']', $this->getAttribute('name').'[]', $html);
 
     $widget_counter++;
     return $html;
