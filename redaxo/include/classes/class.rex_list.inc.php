881ac6dcb4fa4e12cf9257ad9f5c7ea9f089949e
gharlan <gharlan@web.de>
2010-02-22 11:41:12 +0000

    bugfix rex_list

--- redaxo/include/classes/class.rex_list.inc.php
+++ redaxo/include/classes/class.rex_list.inc.php
@@ -692,7 +692,7 @@ class rex_list
       if(strpos(strtoupper($query), ' ORDER BY ') === false)
         $query .= ' ORDER BY '. $sortColumn .' '. $sortType;
       else
-        $query = preg_replace('/ORDER\sBY\s[^ ]*\s(asc|desc)?/i', 'ORDER BY '. $sortColumn .' '. $sortType, $query);
+        $query = preg_replace('/ORDER\sBY\s[^ ]*(\sasc|\sdesc)?/i', 'ORDER BY '. $sortColumn .' '. $sortType, $query);
     }
 
     if(strpos(strtoupper($query), ' LIMIT ') === false)
