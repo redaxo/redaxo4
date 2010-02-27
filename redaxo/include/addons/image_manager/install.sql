1c1572e6599cbbd6938207787569c694a1d7e53c
staabm <markus.staab@redaxo.de>
2010-02-27 17:55:29 +0000

    typo fix

--- redaxo/include/addons/image_manager/install.sql
+++ redaxo/include/addons/image_manager/install.sql
@@ -16,6 +16,6 @@ CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%679_type_effects` (
   PRIMARY KEY (`id`)
 ) TYPE=MyISAM;
 
-INSERT INTO `%TABLE_PREFIX%679_types` (`id`, `status`, `name`, `description`) VALUES(1, 1, 'rex_mediapool_detail', 'Zur darstellung von Bildern in der Detailansicht im Medienpool');
+INSERT INTO `%TABLE_PREFIX%679_types` (`id`, `status`, `name`, `description`) VALUES(1, 1, 'rex_mediapool_detail', 'Zur Darstellung von Bildern in der Detailansicht im Medienpool');
 INSERT INTO `%TABLE_PREFIX%679_types` (`id`, `status`, `name`, `description`) VALUES(2, 1, 'rex_mediapool_maximized', 'Zur Darstellung von Bildern im Medienpool wenn maximiert');
 INSERT INTO `%TABLE_PREFIX%679_types` (`id`, `status`, `name`, `description`) VALUES(3, 1, 'rex_mediapool_preview', 'Zur Darstellung der Vorschaubilder im Medienpool');
