6af25e4bfbd0e45e6c4aac5447451c86a776a3a8
staabm <markus.staab@redaxo.de>
2010-02-27 14:07:05 +0000

    bilder werden jetzt generiert und gecached

--- redaxo/include/addons/image_manager/classes/class.rex_image_manager.inc.php
+++ redaxo/include/addons/image_manager/classes/class.rex_image_manager.inc.php
@@ -22,10 +22,10 @@ class rex_image_manager
       trigger_error('Given image is not a valid rex_image', E_USER_ERROR);
     }
     
-    $set = $this->effectsFromType($type);
 		
-//    if(!$this->image_cacher->isCached($image, $set))
-//    {
+    if(!$this->image_cacher->isCached($image, $type))
+    {
+      $set = $this->effectsFromType($type);
   		$image->prepare();
   
   		// execute effects on image
@@ -39,27 +39,64 @@ class rex_image_manager
   			$effect->setParams($effect_params['params']);
   			$effect->execute();
   		}
-//    }
+    }
     
     return $image;
 	}
 	
   /*public*/ function effectsFromType($type)
   {
-    return array(
-    array('effect' => 'resize', 'params' => array( 'height' => 200, 'width' => 200, 'style' => 'fit') ),
-//        array('effect' => 'resize', 'params' => array( 'width' => 200, 'height' => 100) ),
-//        array('effect' => 'resize', 'params' => array( 'size' => 200, 'style' => 'auto') ),
-//        array('effect' => 'filter_blur', 'params' => array( 'amount' => '80', 'radius' => 8, 'threshold' => 3) ),
-//        array('effect' => 'filter_sharpen', 'params' => array( 'amount' => '80', 'radius' => 8, 'threshold' => 3) ),
-        array('effect' => 'brand', 'params' => array( 'brandimage' => 'logo.gif', ) ),
-    array('effect' => 'filter_greyscale', 'params' => array() ),
-    //    array('effect' => 'filter_sepia', 'params' => array() ),
-    );    
+    global $REX;
+    
+    $qry = '
+      SELECT e.* 
+      FROM '. $REX['TABLE_PREFIX'].'679_types t, '. $REX['TABLE_PREFIX'].'679_type_effects e
+      WHERE e.type_id = t.id AND t.name="'. $type .'"';
+    
+    $sql = rex_sql::factory();
+//    $sql->debugsql = true;
+    $sql->setQuery($qry);
+    
+    $effects = array();
+    while($sql->hasNext())
+    {
+      $effname = $sql->getValue('effect');
+      $params = unserialize($sql->getValue('parameters'));
+      $effparams = array();
+      
+      // extract parameter out of array
+      if(isset($params['rex_effect_'. $effname]))
+      {
+        foreach($params['rex_effect_'. $effname] as $name => $value)
+        {
+          $effparams[str_replace('rex_effect_'. $effname .'_', '', $name)] = $value;
+          unset($effparams[$name]);
+        }
+      }
+      
+      $effect = array(
+        'effect' => $effname,
+        'params' => $effparams,
+      );
+      
+      $effects[] = $effect;
+      $sql->next();
+    }
+    return $effects;
+    
+//    return array(
+//    array('effect' => 'resize', 'params' => array( 'height' => 200, 'width' => 200, 'style' => 'fit') ),
+////        array('effect' => 'resize', 'params' => array( 'width' => 200, 'height' => 100) ),
+////        array('effect' => 'resize', 'params' => array( 'size' => 200, 'style' => 'auto') ),
+////        array('effect' => 'filter_blur', 'params' => array( 'amount' => '80', 'radius' => 8, 'threshold' => 3) ),
+////        array('effect' => 'filter_sharpen', 'params' => array( 'amount' => '80', 'radius' => 8, 'threshold' => 3) ),
+//        array('effect' => 'brand', 'params' => array( 'brandimage' => 'logo.gif', ) ),
+//    array('effect' => 'filter_greyscale', 'params' => array() ),
+//    //    array('effect' => 'filter_sepia', 'params' => array() ),
+//    );    
   }
   /*public*/ function sendImage(/*rex_image*/ $image, $type)
   {
-    $set = $this->effectsFromType($type);
-    $this->image_cacher->sendImage($image, $set);
+    $this->image_cacher->sendImage($image, $type);
   }
 }
