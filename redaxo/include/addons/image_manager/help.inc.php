05364fa4c48495e7d476177a82ef1acc1dfba767
staabm <markus.staab@redaxo.de>
2010-02-28 18:52:23 +0000

    kurze beschreibung ergaenzt

--- redaxo/include/addons/image_manager/help.inc.php
+++ redaxo/include/addons/image_manager/help.inc.php
@@ -13,8 +13,17 @@
 ?>
 <h3>Funktionen:</h3>
 
-<p>Addon zum generieren von Vorschaugrafiken.</p>
+<p>Addon zum generieren von Grafiken anhand von Bildtypen.</p>
 
 <h3>Benutzung:</h3>
 <p>
+Die Bildtypen werden in der Verwaltung des Addons erstellt und konfiguriert.
+Jeder Bildtyp kann beliebig viele Effekte enthalten, die auf das aktuelle Bild angewendet werden.
+
+Zum einbinden eines Bildes muss dazu der Bildtyp in der Url notiert werden.
+</p>
+
+<h3>Anwendungsbeispiele:</h3>
+<p>
+  <?php echo $REX["FRONTEND_FILE"]; ?>?rex_img_type=ImgTypeName&rex_img_file=ImageFileName
 </p>
