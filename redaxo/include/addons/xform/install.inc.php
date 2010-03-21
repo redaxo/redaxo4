36ce74edd289ce445155034c0ac75f0e7e947f58
staabm <markus.staab@redaxo.de>
2010-03-21 14:28:01 +0000

    syntax error gefixed; keine charset angaben in der installation machen,... es sollten die vorgaben der datenbank uebernommen werden (default mysql verhalten)

--- redaxo/include/addons/xform/install.inc.php
+++ redaxo/include/addons/xform/install.inc.php
@@ -12,13 +12,13 @@
 $sql = rex_sql::factory();
 $sql->setQuery('CREATE TABLE IF NOT EXISTS `'.$REX['TABLE_PREFIX'].'xform_email_template` (
   `id` int(11) NOT NULL auto_increment,
-  `name` varchar(255) NOT NULL default '',
-  `mail_from` varchar(255) NOT NULL default '',
-  `mail_from_name` varchar(255) NOT NULL default '',
-  `subject` varchar(255) NOT NULL default '',
+  `name` varchar(255) NOT NULL default "",
+  `mail_from` varchar(255) NOT NULL default "",
+  `mail_from_name` varchar(255) NOT NULL default "",
+  `subject` varchar(255) NOT NULL default "",
   `body` text NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 ;
+) ENGINE=MyISAM;
 ');
 
 // evtl. Fehler beim Anlegen?
@@ -28,7 +28,6 @@ if ($sql->hasError())
 	$msg .= $sql->getError();
 
 	// Evtl Ausgabe einer Meldung
-	// $rxa_htmlinsert['meldung'] = 'Das Addon wurde nicht installiert, weil...';
 	$REX['ADDON']['install']['xform'] = 0;
 	$REX['ADDON']['installmsg']['xform'] = $msg;
 	
