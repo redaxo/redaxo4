ce4033b64ca03591c7f28be7f73033f2bf2e89fd
aeberhard <andreas.eberhard@gmail.com>
2010-03-31 17:54:43 +0000

    Anpassung für TinyMCE, ismap="ismap" bei Bildern im Frontend und Backend entfernen

--- redaxo/include/addons/image_resize/extensions/extension_wysiwyg.inc.php
+++ redaxo/include/addons/image_resize/extensions/extension_wysiwyg.inc.php
@@ -44,15 +44,13 @@ function rex_resize_wysiwyg_output($params)
           if (($realsize[0] != $width[1]) || ($realsize[1] != $height[1]))
           {
             $newsrc = $REX["FRONTEND_FILE"].'?rex_resize=' . $width[1] . 'w__' . $height[1] . 'h__' . $src[2];
-            $newimage = str_replace(array($src[1], 'ismap="ismap" '), array($newsrc, ''), $var);
-            $content = str_replace($var, $newimage, $content);
           }
-          else if ($REX['REDAXO'])
+          else
           {
             $newsrc = $REX['HTDOCS_PATH'] . 'files/' . $src[2];
-            $newimage = str_replace(array($src[1], 'ismap="ismap" '), array($newsrc, ''), $var);
-            $content = str_replace($var, $newimage, $content);
           }          
+          $newimage = str_replace(array($src[1], 'ismap="ismap" '), array($newsrc, ''), $var);
+          $content = str_replace($var, $newimage, $content);
         }
       }
     }
