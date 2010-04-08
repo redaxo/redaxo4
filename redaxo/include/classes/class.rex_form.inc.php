6d403453c8e6d2caa6cdccfe4660b6ede21c84eb
gharlan <gharlan@web.de>
2010-04-08 11:41:56 +0000

    fixed #73: kein "|" um die values bei medialist und linklist

--- redaxo/include/classes/class.rex_form.inc.php
+++ redaxo/include/classes/class.rex_form.inc.php
@@ -398,7 +398,7 @@ class rex_form
     {
       $value = $this->sql->getValue($name);
     }
-    
+
     if (is_array($value))
     {
       $value = '|' . implode('|', $value) . '|';
@@ -1970,7 +1970,7 @@ class rex_form_widget_medialist_element extends rex_form_element
     static $widget_counter = 1;
 
     $html = rex_var_media::getMediaListButton($widget_counter, $this->getValue(), $this->category_id, $this->args);
-    $html = str_replace('MEDIALIST['. $widget_counter .']', $this->getAttribute('name').'[]', $html);
+    $html = str_replace('MEDIALIST['. $widget_counter .']', $this->getAttribute('name'), $html);
 
     $widget_counter++;
     return $html;
@@ -2027,7 +2027,7 @@ class rex_form_widget_linklist_element extends rex_form_element
     static $widget_counter = 1;
 
     $html = rex_var_link::getLinkListButton($widget_counter, $this->getValue(), $this->category_id);
-    $html = str_replace('LINKLIST['. $widget_counter .']', $this->getAttribute('name').'[]', $html);
+    $html = str_replace('LINKLIST['. $widget_counter .']', $this->getAttribute('name'), $html);
 
     $widget_counter++;
     return $html;
