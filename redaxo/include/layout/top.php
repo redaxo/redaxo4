569f8f2fd301128e644bc6e12e3ab59137288650
staabm <markus.staab@redaxo.de>
2010-04-08 18:56:33 +0000

    fixed #71: remove illegal characters in id's

--- redaxo/include/layout/top.php
+++ redaxo/include/layout/top.php
@@ -112,7 +112,7 @@ if ($REX['USER'] && !$REX["PAGE_NO_NAVI"])
     if(rex_be_main_page::isValid($pageObj))
     {
       $page = $pageObj->getPage();
-      $page->setItemAttr('id', 'rex-navi-page-'.$p);
+      $page->setItemAttr('id', 'rex-navi-page-'.strtolower(preg_replace('/[^a-zA-Z0-9\-_]*/', '', $p)));
       
       if(!$pageObj->getBlock())
         $pageObj->setBlock('addons');
