b485f49618f9d8bd7c42059b9776d3040274cc65
dergel <jan.kristinus@yakamara.de>
2010-04-19 17:47:58 +0000

    addons xform/editme - klassen umgeschoben..

--- redaxo/include/addons/editme/config.inc.php
+++ redaxo/include/addons/editme/config.inc.php
@@ -13,67 +13,66 @@ $mypage = 'editme';
 
 if($REX["REDAXO"] && !$REX['SETUP'])
 {
-  // Sprachdateien anhaengen
-  $I18N->appendFile($REX['INCLUDE_PATH'].'/addons/editme/lang/');
+	// Sprachdateien anhaengen
+	$I18N->appendFile($REX['INCLUDE_PATH'].'/addons/editme/lang/');
 
-  $REX['ADDON']['name'][$mypage] = $I18N->msg("editme");
-  $REX['ADDON']['perm'][$mypage] = 'em[]';
+	$REX['ADDON']['name'][$mypage] = $I18N->msg("editme");
+	$REX['ADDON']['perm'][$mypage] = 'em[]';
 
-  // Credits
-  $REX['ADDON']['version'][$mypage] = '0.9';
-  $REX['ADDON']['author'][$mypage] = 'Jan Kristinus';
-  $REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
-  $REX['ADDON']['navigation'][$mypage] = array(
-    // rootPage nur aktivieren wenn sie direkt ausgewaehlt ist
-    // da alle pages main-pages und daher separate oberpunkte sind
+	// Credits
+	$REX['ADDON']['version'][$mypage] = '1.0';
+	$REX['ADDON']['author'][$mypage] = 'Jan Kristinus';
+	$REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
+	$REX['ADDON']['navigation'][$mypage] = array(
+	// rootPage nur aktivieren wenn sie direkt ausgewaehlt ist
+	// da alle pages main-pages und daher separate oberpunkte sind
     'activateCondition' => array('page' => $mypage, 'subpage' => ''),
-  ); 
-  
+	);
 
-  // Fuer Benutzervewaltung
-  $REX['PERM'][] = 'em[]';
+	// Fuer Benutzervewaltung
+	$REX['PERM'][] = 'em[]';
 
-  include $REX['INCLUDE_PATH'].'/addons/editme/functions/functions.inc.php';
+	include $REX['INCLUDE_PATH'].'/addons/editme/functions/functions.inc.php';
 
-  $REX['ADDON']['tables'][$mypage] = rex_em_getTables();
+	$REX['ADDON']['tables'][$mypage] = rex_em_getTables();
 
-  $subpages = array();
-  if(is_array($REX['ADDON']['tables'][$mypage]))
-  {
-    foreach($REX['ADDON']['tables'][$mypage] as $table)
-    {
-      // Recht um das AddOn ueberhaupt einsehen zu koennen
-      $table_perm = 'em['.$table["name"].']';
-      $REX['EXTPERM'][] = $table_perm;
+	$subpages = array();
+	if(is_array($REX['ADDON']['tables'][$mypage]))
+	{
+		foreach($REX['ADDON']['tables'][$mypage] as $table)
+		{
+			// Recht um das AddOn ueberhaupt einsehen zu koennen
+			$table_perm = 'em['.$table["name"].']';
+			$REX['EXTPERM'][] = $table_perm;
 
-      // check active-state and permissions
-      if($table['status'] == 1 && $table['hidden'] != 1 && 
-         $REX['USER'] && ($REX['USER']->isAdmin() || $REX['USER']->hasPerm($table_perm)))
-      {
-        // include dashbord-components
-        if(rex_request('page', 'string') == 'be_dashboard')
-        {
-          require_once dirname(__FILE__) .'/classes/class.dashboard.inc.php';
-  
-          rex_register_extension (
-            'DASHBOARD_COMPONENT',
-            array(new rex_editme_component($table["name"]), 'registerAsExtension')
-          );
-        }
-        
-        // include page
-        $be_page = new rex_be_page($table['label'], array('page'=>$mypage, 'subpage' => $table['name']));
-        $be_page->setHref('index.php?page=editme&subpage='.$table['name']);
-        $subpages[] = new rex_be_main_page($mypage, $be_page);
-      }
-    }
-  }
-  $REX['ADDON']['pages'][$mypage] = $subpages;
-  
+			// check active-state and permissions
+			if($table['status'] == 1 && $table['hidden'] != 1 &&
+			$REX['USER'] && ($REX['USER']->isAdmin() || $REX['USER']->hasPerm($table_perm)))
+			{
+				 
+				// include page
+				$be_page = new rex_be_page($table['label'], array('page'=>$mypage, 'subpage' => $table['name']));
+				$be_page->setHref('index.php?page=editme&subpage='.$table['name']);
+				$subpages[] = new rex_be_main_page($mypage, $be_page);
+				// $subpages[] = array($table['name'],$table['label']); // für rex 4.2.1
+			}
+		}
+	}
 
-  function rex_editme_assets($params){
-    $params['subject'] .= "\n  ".'<script src="../files/addons/editme/em.js" type="text/javascript"></script>';
-    return $params['subject'];
-  }
-  rex_register_extension('PAGE_HEADER', 'rex_editme_assets');
+	$REX['ADDON']['pages'][$mypage] = $subpages;
+	// $REX['ADDON'][$mypage]['SUBPAGES'] = $subpages; // für rex 4.2.1
+
+	function rex_editme_assets($params){
+		$params['subject'] .= "\n  ".'<script src="../files/addons/editme/em.js" type="text/javascript"></script>';
+		return $params['subject'];
+	}
+	rex_register_extension('PAGE_HEADER', 'rex_editme_assets');
+}
+
+rex_register_extension('ADDONS_INCLUDED', 'rex_com_xform_add');
+function rex_com_xform_add($params){
+	global $REX;
+	$REX['ADDON']['xform']['classpaths']['action'][] = $REX["INCLUDE_PATH"]."/addons/editme/xform/action/";
+	$REX['ADDON']['xform']['classpaths']['value'][] = $REX["INCLUDE_PATH"]."/addons/editme/xform/value/";
+
 }
