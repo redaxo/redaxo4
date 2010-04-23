8df90b8c6b1f015620710243954ae43e4ffadef1
dergel <jan.kristinus@yakamara.de>
2010-04-23 08:10:08 +0000

    addon image_manager . effekt resize, modus minimum eingebaut.

--- redaxo/include/addons/image_manager/classes/effects/class.rex_effect_resize.inc.php
+++ redaxo/include/addons/image_manager/classes/effects/class.rex_effect_resize.inc.php
@@ -1,19 +1,23 @@
 <?php
 
+// Todo:
+// - Vergrš§ern erlauben oder nicht. aber eher als Modul einsetzen, also
+// fit
+
 class rex_effect_resize extends rex_effect_abstract
 {
-  var $options;
-  
-  function rex_effect_resize()
-  {
-    $this->options = array('fit','warp');
-  }
-  
+	var $options;
+
+	function rex_effect_resize()
+	{
+		$this->options = array('fit','warp','minimum');
+	}
+
 	function execute()
 	{
-    $gdimage =& $this->image->getImage();
-    $w = $this->image->getWidth();
-    $h = $this->image->getHeight();
+		$gdimage =& $this->image->getImage();
+		$w = $this->image->getWidth();
+		$h = $this->image->getHeight();
 
 		if(!isset($this->params['style']) || !in_array($this->params['style'],$this->options))
 		{
@@ -22,12 +26,12 @@ class rex_effect_resize extends rex_effect_abstract
 
 		if($this->params['style'] == 'fit')
 		{
-		
+
 			if (!empty($this->params['height']) && !empty($this->params['width']))
 			{
 				$img_ratio  = $w / $h;
 				$resize_ratio = $this->params['width'] / $this->params['height'];
-				
+
 				if ($img_ratio >= $resize_ratio)
 				{
 					// --- width
@@ -50,7 +54,39 @@ class rex_effect_resize extends rex_effect_abstract
 			}
 		}
 
-		// TODO prüfen!
+		// @ WHEEZ - Neuer Modus 'minimum'
+
+		if($this->params['style'] == 'minimum')
+		{
+
+			if (!empty($this->params['height']) && !empty($this->params['width']))
+			{
+				$img_ratio  = $w / $h;
+				$resize_ratio = $this->params['width'] / $this->params['height'];
+
+				if ($img_ratio < $resize_ratio)
+				{
+					// --- width
+					$this->params['height'] = (int) ($this->params['width'] / $w * $h);
+				}else
+				{
+					// --- height
+					$this->params['width']  = (int) ($this->params['height'] / $h * $w);
+				}
+			}
+			elseif (!empty($this->params['height']))
+			{
+				$img_factor  = $h / $this->params['height'];
+				$this->params['width'] = (int) ($w / $img_factor);
+			}
+			elseif (!empty($this->params['width']))
+			{
+				$img_factor  = $w / $this->params['width'];
+				$this->params['height'] = (int) ($h / $img_factor);
+			}
+		}
+
+		// TODO prŸfen!
 		// Originalbild selbst sehr klein und wuerde via resize vergroessert
 		// => Das Originalbild ausliefern
 		if(!isset($this->params["width"]))
@@ -62,8 +98,6 @@ class rex_effect_resize extends rex_effect_abstract
 		{
 			$this->params["height"] = $h;
 		}
-		
-		// TODO allow_enlarge check
 
 		if (function_exists('ImageCreateTrueColor'))
 		{
@@ -81,7 +115,7 @@ class rex_effect_resize extends rex_effect_abstract
 		// Transparenz erhalten
 		$this->keepTransparent($des);
 		imagecopyresampled($des, $gdimage, 0, 0, 0, 0, $this->params['width'], $this->params['height'], $w, $h);
-		
+
 		$gdimage = $des;
 		$this->image->refreshDimensions();
 	}
@@ -89,7 +123,7 @@ class rex_effect_resize extends rex_effect_abstract
 
 	function keepTransparent($des)
 	{
-	  $image = $this->image;
+		$image = $this->image;
 		if ($image->getFormat() == 'PNG')
 		{
 			imagealphablending($des, false);
@@ -97,7 +131,7 @@ class rex_effect_resize extends rex_effect_abstract
 		}
 		else if ($image->getFormat() == 'GIF')
 		{
-		  $gdimage =& $image->getImage();
+			$gdimage =& $image->getImage();
 			$colorTransparent = imagecolortransparent($gdimage);
 			imagepalettecopy($gdimage, $des);
 			if($colorTransparent>0)
@@ -116,30 +150,32 @@ class rex_effect_resize extends rex_effect_abstract
 		global $REX,$I18N;
 
 		return array(
-  		array(
+		array(
         'label'=>$I18N->msg('imanager_effect_resize_width'),
         'name' => 'width',
         'type' => 'int'
-      ),
-      array(
+        ),
+        array(
         'label'=>$I18N->msg('imanager_effect_resize_height'),
         'name' => 'height',
         'type' => 'int'
-      ),
-      array(
+        ),
+        array(
         'label' => $I18N->msg('imanager_effect_resize_style'),
         'name' => 'style',
         'type'  => 'select',
         'options' => $this->options,
         'default' => 'fit',
-      ),
-      array(
-        'label'=>$I18N->msg('imanager_effect_resize_allow_enlarge'),
-        'name' => 'allow_enlarge',
-        'type' => 'select',
-        'options' => array($I18N->msg('yes'), $I18N->msg('no')),
-        'default' => $I18N->msg('no'),
-      ),
-    );
+        ),
+        /*
+         array(
+         'label'=>$I18N->msg('imanager_effect_resize_allow_enlarge'),
+         'name' => 'allow_enlarge',
+         'type' => 'select',
+         'options' => array($I18N->msg('yes'), $I18N->msg('no')),
+         'default' => $I18N->msg('no'),
+         ),
+         */
+        );
 	}
 }
