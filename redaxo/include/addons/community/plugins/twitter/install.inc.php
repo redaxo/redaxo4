f602189611ca7e0bea3a32679539103bc39f1410
dergel <jan.kristinus@yakamara.de>
2010-05-04 15:07:02 +0000

    addon twitter: min. php5 bei intalls

--- redaxo/include/addons/community/plugins/twitter/install.inc.php
+++ redaxo/include/addons/community/plugins/twitter/install.inc.php
@@ -1,5 +1,15 @@
 <?php
 
-$REX['ADDON']['install']['twitter'] = 1;
+$pv = (int) phpversion();
+
+if($pv>4)
+{
+  $REX['ADDON']['install']['twitter'] = 1;
+}else
+{
+  $REX['ADDON']['install']['twitter'] = 0;
+  $REX['ADDON']['installmsg']['twitter'] = 'Es muss mindestens PHP in der Version 5 vorhanden sein';
+
+}
 
 ?>
