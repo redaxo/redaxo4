ee738250d6509c795e790e4ce24983be807429b7
dergel <jan.kristinus@yakamara.de>
2010-12-02 23:54:44 +0000

    addon community auth . info bei meldung, rex_com_navi mit link und name wrap erweitert

--- redaxo/include/addons/community/plugins/auth/xform/value/class.xform.com_auth_form_info.inc.php
+++ redaxo/include/addons/community/plugins/auth/xform/value/class.xform.com_auth_form_info.inc.php
@@ -3,36 +3,38 @@
 class rex_xform_com_auth_form_info extends rex_xform_abstract
 {
 
-	function enterObject(&$email_elements,&$sql_elements,&$warning,&$form_output,$send = 0)
-	{
-		global $REX;
+  function enterObject(&$email_elements,&$sql_elements,&$warning,&$form_output,$send = 0)
+  {
+    global $REX;
 
-		$message = "";
-		$layout = $this->elements[1];
+    $message = "";
+    
+    $info = rex_request('rex_com_auth_info',"string");
+    
+    $messages = array();
+    $messages[0] = $this->getElement(2);
+    $messages[1] = $this->getElement(3);
+    $messages[2] = $this->getElement(4);
+    $messages[3] = $this->getElement(5);
+    
+    $message = @$messages[$info];
+    
+    if($message != "") 
+    {
+      $this->params["form_output"][$this->getId()] = '
+        <p class="formcom_auth_form_info formlabel-'.$this->getName().'" id="'.$this->getHTMLId().'">
+          '.$message.'
+        </p>';
+    }
+  
+    return;
 
-		if(isset($_REQUEST[$REX['ADDON']['editme']['plugin_auth']['request']['name']]))
-		{
-			// loginversuch fehlgeschlagen
-			if (isset($this->elements[2])) $message = $this->elements[2];	
-		}else
-		{
-			// seite zum ersten mal aufgerufen
-			if (isset($this->elements[3])) $message = $this->elements[3];	
-		}
+  }
 
-		if($message == "")
-			return;
-			
-		$form_output[] .= str_replace('###message###',$message,$layout);
-
-		return;
-
-	}
-
-	function getDescription()
-	{
-		return "com_auth_form_info -> Beispiel: com_auth_form_info|<p>###message###</p>|login_failed_info|please_login_info|";
-	}
+  function getDescription()
+  {
+    return "com_auth_form_info [0 - nichts / 1 - logout / 2 - failed login / 3 - logged in] -> Beispiel: com_auth_form_info|label|msg_nothing|msg_logout|msg_failed|msg_logged_out|";
+  }
 
 }
 
