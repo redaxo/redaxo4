6356721e54c2b3751eb0ce360a595efa3bcb9c31
jdlx <github@rexdev.de>
2010-12-10 22:28:49 +0000

    unnötige showstopper queries wieder raus.. ;-)

--- redaxo/include/install/update4_x_to_4_3.sql
+++ redaxo/include/install/update4_x_to_4_3.sql
@@ -1,5 +1,2 @@
 ## Redaxo Database Dump Version 4
 ## Prefix rex_
-
-ALTER TABLE `rex_62_params` CHANGE `validate` `validate` TEXT NULL
-ALTER TABLE `rex_62_params` ADD `restrictions` TEXT NOT NULL AFTER `validate`
