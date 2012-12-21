58a85fcdae3b7ab3a366ee9e2ec6790dd0860de5
Gregor Harlan <gharlan@web.de>
2012-12-21 13:48:35 -0800

    Merge pull request #72 from jdlx/addons/phpmailer/admin_bcc_email_option
    [phpmailer] add settings option: admin bcc
    Conflicts:
    	redaxo/include/addons/image_manager
    	redaxo/include/addons/phpmailer/classes/class.rex_mailer.inc.php
    	redaxo/include/addons/phpmailer/lang/de_de.lang
    	redaxo/include/addons/phpmailer/lang/en_gb.lang
    	redaxo/include/addons/phpmailer/pages/settings.inc.php

---
+++ redaxo/include/addons/phpmailer/classes/class.rex_mailer.inc.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * PHPMailer Addon
+ *
+ * @author markus[dot]staab[at]redaxo[dot]de Markus Staab
+ *
+ *
+ * @package redaxo4
+ * @version svn:$Id$
+ */
+
+class rex_mailer extends PHPMailer
+{
+  public $AdminBcc = '';
+
+  function rex_mailer()
+  {
+    global $REX;
+
+// --- DYN
+$this->From             = 'from@example.com';
+$this->FromName         = 'Mailer';
+$this->ConfirmReadingTo = '';
+$this->AdminBcc         = '';
+$this->Mailer           = 'sendmail';
+$this->Host             = 'localhost';
+$this->CharSet          = 'utf-8';
+$this->WordWrap         = 120;
+$this->Encoding         = '8bit';
+$this->Priority         = 3;
+$this->SMTPAuth         = false;
+$this->Username         = '';
+$this->Password         = '';
+// --- /DYN
+
+    $this->PluginDir = $REX['INCLUDE_PATH'] . '/addons/phpmailer/classes/';
+
+    if($this->AdminBcc !== ''){
+      parent::AddBCC($this->AdminBcc);
+    }
+  }
+
+  function SetLanguage($lang_type, $lang_path = null)
+  {
+    global $REX;
+
+    if ($lang_path == null)
+      $lang_path = $REX['INCLUDE_PATH'] . '/addons/phpmailer/classes/language/';
+
+    parent :: SetLanguage($lang_type, $lang_path);
+  }
+}
