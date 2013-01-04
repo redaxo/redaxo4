bbdd9617e385d18214310be228c97feb1016e6be
Gregor Harlan <gharlan@web.de>
2013-01-04 15:15:25 +0100

    add lang directory to $I18N

--- redaxo/include/addons/textile/install.inc.php
+++ redaxo/include/addons/textile/install.inc.php
@@ -15,7 +15,8 @@ $required_PHP = '5.0.0';
 ////////////////////////////////////////////////////////////////////////////////
 if(version_compare(PHP_VERSION, $required_PHP, '<'))
 {
-  $REX['ADDON']['installmsg']['textile'] = $I18N->msg('textile_install_phpversion',$required_PHP);
+  $I18N->appendFile($REX['INCLUDE_PATH'].'/addons/textile/lang/');
+  $REX['ADDON']['installmsg']['textile'] = $I18N->msg('textile_install_phpversion', $required_PHP);
   $REX['ADDON']['install']['textile']    = 0;
   return;
 }
