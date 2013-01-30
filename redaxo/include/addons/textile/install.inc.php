41ec6876203ccf675de4551d8524a923d4f73b43
Gregor Harlan <gharlan@web.de>
2013-01-30 16:47:21 +0100

    removed obsolete php version checks

--- redaxo/include/addons/textile/install.inc.php
+++ redaxo/include/addons/textile/install.inc.php
@@ -9,16 +9,4 @@
  * @version svn:$Id$
  */
 
-$required_PHP = '5.0.0';
-
-// CHECK PHP VERSION
-////////////////////////////////////////////////////////////////////////////////
-if(version_compare(PHP_VERSION, $required_PHP, '<'))
-{
-  $I18N->appendFile($REX['INCLUDE_PATH'].'/addons/textile/lang/');
-  $REX['ADDON']['installmsg']['textile'] = $I18N->msg('textile_install_phpversion', $required_PHP);
-  $REX['ADDON']['install']['textile']    = 0;
-  return;
-}
-
 $REX['ADDON']['install']['textile'] = 1;
