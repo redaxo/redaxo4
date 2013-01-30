7cf038f12755bd984192831282e83177d9f497fd
Gregor Harlan <gharlan@web.de>
2013-01-30 17:49:21 +0100

    fixed jquery
    refs #77
    thx to @jdlx

--- redaxo/include/pages/user.inc.php
+++ redaxo/include/pages/user.inc.php
@@ -774,9 +774,9 @@ if ($FUNC_ADD != "" || $user_id > 0)
       }
       else
       {
-        $("#userperm-module").attr("disabled", "");
+        $("#userperm-module").attr("disabled", false);
         $("#cats_mcats_box").slideDown("slow");
-        $("#userperm-extra").find("option[value=\'editContentOnly\[\]\']").attr("disabled", "");
+        $("#userperm-extra").find("option[value=\'editContentOnly\[\]\']").attr("disabled", false);
         catsChecked();
       }
     });
@@ -794,12 +794,12 @@ if ($FUNC_ADD != "" || $user_id > 0)
       if(c_checked)
         $("#userperm-cat").attr("disabled", "disabled");
       else
-        $("#userperm-cat").attr("disabled", "");
+        $("#userperm-cat").attr("disabled", false);
 
       if(m_checked)
         $("#userperm-media").attr("disabled", "disabled");
       else
-        $("#userperm-media").attr("disabled", "");
+        $("#userperm-media").attr("disabled", false);
 
       if(animate)
       {
