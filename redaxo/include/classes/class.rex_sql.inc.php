a697cd998bf9b662c6dde1fb9221af06c43b284f
Gregor Harlan <gharlan@web.de>
2013-01-30 01:26:39 +0100

    avoid strict messages
    refs #88

--- redaxo/include/classes/class.rex_sql.inc.php
+++ redaxo/include/classes/class.rex_sql.inc.php
@@ -805,7 +805,7 @@ class rex_sql
    * @param $DBID int Id der Datenbankverbindung
    * @return array Ein Array das die Metadaten enthält
    */
-  /*public*/ function showColumns($table, $DBID=1)
+  static /*public*/ function showColumns($table, $DBID=1)
   {
     $sql = rex_sql::factory($DBID);
     $sql->setQuery('SHOW COLUMNS FROM '.$table);
