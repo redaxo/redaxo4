da31a7e618dcf4b2cbf04a61b38c36eb31b5e98a
RexDude <rexdude@arcor.de>
2013-02-01 20:22:50 +0100

    Removed getMediaAddonFolder, now using [FRONTEND_PATH]

--- redaxo/include/classes/class.rex_manager.inc.php
+++ redaxo/include/classes/class.rex_manager.inc.php
@@ -354,15 +354,6 @@
       return '';
     }
   }
-
-  /**
-   * Gibt den Pfad zum Unterordner 'addons' des 'files' Ordners zurück.
-   */
-  /*protected*/ function getMediaAddonFolder() {
-    global $REX;
-    $lastSlashPos = strrpos($REX['MEDIAFOLDER'], DIRECTORY_SEPARATOR);
-    return substr($REX['MEDIAFOLDER'], 0, $lastSlashPos + 1) . $REX['MEDIA_ADDON_DIR'];
-  }
 }
 
 /**
@@ -429,7 +420,7 @@ class rex_addonManager extends rex_baseManager
   /*protected*/ function mediaFolder($addonName)
   {
     global $REX;
-    return $this->getMediaAddonFolder() . DIRECTORY_SEPARATOR .$addonName;
+    return $REX['FRONTEND_PATH'] . DIRECTORY_SEPARATOR . $REX['MEDIA_ADDON_DIR'] . DIRECTORY_SEPARATOR . $addonName;
   }
 }
 
@@ -535,6 +526,6 @@ class rex_pluginManager extends rex_baseManager
   /*protected*/ function mediaFolder($pluginName)
   {
     global $REX;
-    return $this->getMediaAddonFolder() . DIRECTORY_SEPARATOR. $this->addonName .DIRECTORY_SEPARATOR .'plugins'. DIRECTORY_SEPARATOR. $pluginName;
+    return $REX['FRONTEND_PATH'] . DIRECTORY_SEPARATOR . $REX['MEDIA_ADDON_DIR'] . DIRECTORY_SEPARATOR. $this->addonName .DIRECTORY_SEPARATOR .'plugins'. DIRECTORY_SEPARATOR. $pluginName;
   }
 }
