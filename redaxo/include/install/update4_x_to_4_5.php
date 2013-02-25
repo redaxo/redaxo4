26fa4e0f14ff81819f3c2323bc37b16f0f8b5c9a
tbaddade <thomas@redaxo.de>
2013-02-25 08:18:02 +0100

    Index entfernt, Feld gibt es nicht und Metainfo legt selbst keinen an

--- redaxo/include/install/update4_x_to_4_5.php
+++ redaxo/include/install/update4_x_to_4_5.php
@@ -32,7 +32,6 @@ if ($eventType == REX_A1_IMPORT_EVENT_PRE)
   $update->setQuery("ALTER TABLE ". $REX['TABLE_PREFIX'] ."module DROP PRIMARY KEY, ADD PRIMARY KEY (`id`), ADD INDEX `category_id` (`category_id`);");
   $update->setQuery("ALTER TABLE ". $REX['TABLE_PREFIX'] ."user ADD UNIQUE INDEX `login` (`login`(50));");
 
-  $update->setQuery("ALTER TABLE ". $REX['TABLE_PREFIX'] ."62_type ADD UNIQUE INDEX `login` (`login`(50));");
   $update->setQuery("UPDATE ". $REX['TABLE_PREFIX'] ."62_type set dbtype='text', dblength='0' where label='". $REX['TABLE_PREFIX'] ."MEDIALIST_BUTTON' or label='". $REX['TABLE_PREFIX'] ."LINKLIST_BUTTON'");
 
   $update->setQuery("ALTER TABLE `". $REX['TABLE_PREFIX'] ."62_params` CHANGE `validate` `validate` TEXT DEFAULT NULL");
