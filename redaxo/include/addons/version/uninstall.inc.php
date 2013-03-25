814502218bf1850e15a7bc2d18eef44405a27fc0
Jan Kristinus <jan.kristinus@yakamara.de>
2013-03-25 13:23:43 +0100

    #162 fixed bei deinstallation von version, werden alle revisionsslices entfernt

--- redaxo/include/addons/version/uninstall.inc.php
+++ redaxo/include/addons/version/uninstall.inc.php
@@ -10,4 +10,8 @@
  */
 
 $REX['ADDON']['install']['version'] = 0;
+
+$d = rex_sql::factory();
+$d->setQuery('delete from '.$REX['TABLE_PREFIX'].'article_slice where revision=1');
+
 // ERRMSG IN CASE: $REX['ADDON']['installmsg']['version'] = "Leider konnte nichts installiert werden da.";
