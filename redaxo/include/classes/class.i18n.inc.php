0fca603eb29667192a6f77f746d15ac5c9c93740
jdlx <github@rexdev.de>
2014-10-12 02:03:35 +0200

    add EP "I18N_MISSING_TRANSLATION" ..

--- redaxo/include/classes/class.i18n.inc.php
+++ redaxo/include/classes/class.i18n.inc.php
@@ -104,6 +104,15 @@ class i18n
             $msg = $this->text[$key];
         } else {
             $msg = "[translate:$key]";
+            rex_register_extension_point(
+                'I18N_MISSING_TRANSLATION',
+                $key,
+                array(
+                    'msg'  => $msg,
+                    'i18N' => $this,
+                    'args' => func_get_args()
+                )
+            );
         }
 
         $patterns = array ();
