bb9e58ba82326b40f131e294d86658ddae52372c
Jan Kristinus <jan.kristinus@yakamara.de>
2015-09-11 10:22:44 +0200

    image_manager: grayscale effekt über IMG_FILTER_GRAYSCALE

--- redaxo/include/addons/image_manager/classes/effects/class.rex_effect_filter_greyscale.inc.php
+++ redaxo/include/addons/image_manager/classes/effects/class.rex_effect_filter_greyscale.inc.php
@@ -10,34 +10,11 @@ class rex_effect_filter_greyscale extends rex_effect_abstract
         }
 
         $gdimage = & $this->image->getImage();
-        $w = $this->image->getWidth();
-        $h = $this->image->getHeight();
+        imagesavealpha($gdimage, true);
+		imagealphablending($gdimage, true);
 
-        $src_x = ceil($w);
-        $src_y = ceil($h);
-        $dst_x = $src_x;
-        $dst_y = $src_y;
-        $dst_im = ImageCreateTrueColor($dst_x, $dst_y);
+        imagefilter($gdimage, IMG_FILTER_GRAYSCALE);
 
-        ImageCopyResampled( $dst_im, $gdimage, 0, 0, 0, 0, $dst_x, $dst_y,
-            $src_x, $src_y );
-
-        for ($c = 0; $c < 256; $c++) {
-            $palette[$c] = imagecolorallocate($dst_im, $c, $c, $c);
-        }
-
-        for ($y = 0; $y < $src_y; $y++) {
-            for ($x = 0; $x < $src_x; $x++) {
-                $rgb = imagecolorat($dst_im, $x, $y);
-                $r = ($rgb >> 16) & 0xFF;
-                $g = ($rgb >> 8) & 0xFF;
-                $b = $rgb & 0xFF;
-                $gs = $r * 0.299 + $g * 0.587 + $b * 0.114;
-                imagesetpixel($dst_im, $x, $y, $palette[$gs]);
-            }
-        }
-
-        $gdimage = $dst_im;
     }
 
     function getParams()
