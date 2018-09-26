65c32b8c868ce38df8c420031f1af101a89ec055
Marcel Kuhmann <der_marcell@hotmail.com>
2018-09-26 19:01:32 +0200

    Update mediapool.inc.php

--- redaxo/include/pages/mediapool.inc.php
+++ redaxo/include/pages/mediapool.inc.php
@@ -129,8 +129,8 @@ function selectMedialist(filename)
 {
     <?php
         if (substr($opener_input_field, 0, 14) == 'REX_MEDIALIST_') {
-            $id = substr($opener_input_field, 14, strlen($opener_input_field));
-            echo 'var medialist = "REX_MEDIALIST_SELECT_' . htmlspecialchars($id) . '";
+            $id = (int) substr($opener_input_field, 14, strlen($opener_input_field));
+            echo 'var medialist = "REX_MEDIALIST_SELECT_' . $id . '";
 
                         var source = opener.document.getElementById(medialist);
                         var sourcelength = source.options.length;
@@ -140,7 +140,7 @@ function selectMedialist(filename)
                         option.value = filename;
 
                         source.options.add(option, sourcelength);
-                        opener.writeREXMedialist(' . htmlspecialchars($id) . ');';
+                        opener.writeREXMedialist(' . $id . ');';
 
         }
     ?>
@@ -150,8 +150,8 @@ function selectMediaListArray(files)
 {
     <?php
         if (substr($opener_input_field, 0, 14) == 'REX_MEDIALIST_') {
-            $id = substr($opener_input_field, 14, strlen($opener_input_field));
-            echo 'var medialist = "REX_MEDIALIST_SELECT_' . htmlspecialchars($id) . '";
+            $id = (int) substr($opener_input_field, 14, strlen($opener_input_field));
+            echo 'var medialist = "REX_MEDIALIST_SELECT_' . $id . '";
 
                         var source = opener.document.getElementById(medialist);
                         var sourcelength = source.options.length;
@@ -171,7 +171,7 @@ function selectMediaListArray(files)
                             }
                         }
 
-                        opener.writeREXMedialist(' . htmlspecialchars($id) . ');';
+                        opener.writeREXMedialist(' . $id . ');';
 
         }
     ?>
